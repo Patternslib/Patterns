5b654b989d635752fac6464c5f34325c6c5a8982
Wichert Akkerman <wichert@wiggy.net>
2012-01-08 22:03:16 +0100

    Add initial documentation for tooltip logic.

---
+++ src/patterns/tooltip.js
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Patterns @VERSION@ tooltip - tooltips
+ *
+ * Copyright 2008-2012 Simplon B.V.
+ * Copyright 2011 Humberto Sermeño
+ * Copyright 2011 SYSLAB.COM GmbH
+ */
+(function($) {
+    mapal = mapal || {patterns: {}};
+    $.extend(mapal.patterns, {
+    "tooltip": {
+        options: {
+        },
+
+        init: function() {
+        },
+
+        execute: function(elem, url, sources, params, event) {
+        }
+    }});
+})(jQuery);
