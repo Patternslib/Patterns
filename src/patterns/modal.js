6638a2868f6348d9b46e9dafe541c968b1a0937e
Wichert Akkerman <wichert@wiggy.net>
2012-01-08 14:56:35 +0100

    Split patterns into separate bits.

---
+++ src/patterns/modal.js
@@ -0,0 +1,162 @@
+$.extend( mapal.patterns, {
+	"modal": {
+		options: {
+			"class": "",
+			"loadingText": "Loading...",
+			'showLoading': true,
+			pauseVideo: false
+		},
+		
+		init: function() {
+			$("#panel .closePanel").live('click.mapalModal', function(e) {
+				mapal.patterns.modal._findClose($(this));
+				e.preventDefault();
+				return false;
+			});
+		//	mapal.patterns.registerListener(['selfHealing'], 'onExecuted', mapal.patterns.modal._findClose);
+		},
+		
+		_findClose: function($elem) {
+			if ( $elem.hasClass('closePanel') ) {
+				var $panel = $elem.parents( "#panel" );
+				if ( $panel.length > 0 ) {
+					mapal.patterns.modal.close($panel);
+				}
+			}
+		},
+		
+		execute: function( elem, url, sources, params, event ) {
+            var $trigger = $(event.target),
+	            href = event.target.tagName.toLowerCase()==="a" ? $trigger.attr("href") : $trigger.attr("name"),
+	            parts = href.split("#", 2),
+	            $panel = $("#panel");
+	        var source = (parts[1] == undefined) ? [] : parts[1];
+
+	        var opts = $.extend({}, mapal.patterns.modal.options, params);
+	        
+	        if ($panel.length===0) {
+	            $panel = $("<div/>")
+	                .attr("id", "panel")
+	                .appendTo(document.body);
+	            $("<div/>").attr("id","panel-content").appendTo($panel);
+	        }
+	        
+	        $panel.data('modal', opts);
+	        
+	        mapal.patterns.modal.pauseVideo(opts, true);
+	        
+	        if (opts['showLoading']) {
+	        	var $loading = $("<div>").text(opts['loadingText']).attr("id", "panel-loading");
+	        	$('<span>').appendTo($loading);
+	        	$loading.appendTo($panel);
+	        	
+	        	$panel.addClass('loading');
+	        	
+	        	mapal.patterns.modal.apiInit($panel, opts);
+	        	mapal.patterns.modal.centerOverlay($panel);
+	        }
+	        
+	        mapal.injection.load(elem, parts[0], "panel-content:content", source, {
+	        		'onFinished': function($target) {
+	        			$panel.attr('class', opts['class']);
+	        			
+	        			$target.css("opacity", 1).addClass("panel");
+	        			
+	        			
+			            if (opts['showLoading']) {
+			            	$("#panel-loading").remove();
+			            } else {
+			            	mapal.injection.apiInit($panel, opts);
+			            }
+			            
+			            mapal.patterns.modal.centerOverlay($panel);
+	        		}
+	        });
+		},
+		
+		apiInit: function($panel, opts) {
+			var api;
+			
+            //$target.find("form").ajaxForm({context: $trigger.get(0),
+            //                           success: mapal.patterns.modal.formHandler});
+            api = $panel.overlay({api: true,
+                                  closeOnClick: false,
+                                  onClose: function() {
+                                	  mapal.patterns.modal.pauseVideo(opts, false);
+                                  },
+                                  top: 'center',
+                                  mask: {color: "#ebecff", loadSpeed: 200, opacity: 0.9}});
+            api.load();
+		},
+		
+		'centerOverlay': function( $panel ) {					
+			var win = $(window);
+			var w = $panel.outerWidth({ "margin": true });//get width of overlay
+	        var h = $panel.outerHeight({ "margin": true }); //get height of overlay
+	        var l = Math.max((win.width() - w) / 2, 0);  //calculate left property
+	        var t = Math.max((win.height() - h) / 2, 0);  //calculate top property
+	         
+	        $panel.css({ 'position': 'fixed', 'top': t, 'left': l });
+		},
+		
+		"close": function($panel) {
+			//var opts = $panel.data('modal');
+			//mapal.patterns.modal.pauseVideo(opts, false);
+			$panel.overlay().close();
+			$panel.remove();
+		},
+		
+		pauseVideo: function( opts, which ) {
+			if (typeof Playbox !== 'undefined' && opts && opts.pauseVideo ) {
+				if ( which ) {
+					Playbox.pause();
+				} else {
+					Playbox.resume();
+				}
+			}
+		},
+		
+	    formHandler: function(data, status, xhr, $form) {
+	        // regexp taken from jQuery 1.4.1
+	        var rscript = /<script(.|\s)*?\/script>/gi,
+	            $trigger = $(this.context),
+	            href = this.context.tagName.toLowerCase()==="a" ? $trigger.attr("href") : $trigger.attr("name"),
+	            action = $form.attr("action"),
+	            $panel = $("#panel"),
+	            ct = xhr.getResponseHeader("content-type"),
+	            isJSON = ct.indexOf("application/json") >= 0,
+	            $tree, target;
+
+	        // Error or validation error
+	        if (isJSON || xhr.status !== 202) {
+	            if (isJSON) {
+	                var reply = $.parseJSON(xhr.responseText);
+	                $trigger.trigger("ajaxFormResult", reply);
+	                if (reply.action==="reload") {
+	                    location.reload();
+	                } else if (reply.action==="close" || !reply.action) {
+	                	mapal.patterns.modal.close($panel);
+	                }
+	                return;
+	            } else {
+	                $trigger.trigger("ajaxFormResult");
+	            }
+	            mapal.patterns.modal.close($panel);
+	            return;
+	        }
+
+	        if (action.indexOf("#")>0) {
+	            target = action.split("#", 2)[1];
+	        } else {
+	            target = href.split("#", 2)[1];
+	        }
+
+	        $tree = $("<div/>").append(data.replace(rscript, ""));
+	        $tree = $tree.find("#"+target).attr("id", "panel-content");
+	        mapal.initContent(target);
+	        $("#panel-content").replaceWith($tree);
+	        $panel.find("form").ajaxForm({context: this.context,
+	                                      success: mapal.patterns.modal.formHandler});
+	    }
+	}
+});
