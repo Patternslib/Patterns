4c75af8c36c0f0c4c0d2da40aaf5bc18ffa87d7a
Wichert Akkerman <wichert@wiggy.net>
2012-01-15 22:31:46 +0100

    Move store implementation out to separate file.

---
+++ src/core/store.js
@@ -0,0 +1,62 @@
+/**
+ * @license
+ * Patterns @VERSION@ store - store pattern state locally in the browser
+ *
+ * Copyright 2008-2012 Simplon B.V.
+ * Copyright 2011 Humberto Sermeño
+ * Copyright 2011 SYSLAB.COM GmbH
+ */
+(function($){
+    mapal = mapal || {};
+    $.extend(mapal, {
+    'store': {
+        'getPatternAttributes': function(pattern) {
+            if (!mapal.store.hasStorage()) return [];
+            
+            var count = parseInt(window.sessionStorage.getItem( pattern + '-count' ) || "0");
+            var attrs = [];
+            
+            for (var i = 1; i <= count; i++ ) {
+                attrs.push(window.sessionStorage.getItem( pattern + '-' + i ));
+            }
+            
+            return attrs;
+        },
+        
+        'addPatternAttribute': function(pattern, value) {
+            if (!mapal.store.hasStorage()) return;
+            
+            var count = parseInt(window.sessionStorage.getItem( pattern + '-count' ) || "0") + 1;
+            
+            window.sessionStorage.setItem( pattern + '-count', count );
+            window.sessionStorage.setItem( pattern + '-' + count, value );
+        },
+        
+        'setPatternAttribute': function(pattern, index, value) {
+            if (!mapal.store.hasStorage()) return;
+            
+            var count = parseInt(window.sessionStorage.getItem( pattern + '-count' ) || "0");
+            
+            if (index > 0 && index <= count) {
+                window.sessionStorage.setItem( pattern + '-' + index, value);
+                
+                return true;
+            }
+            
+            return false;
+        },
+        
+        'initPatternStore': function(pattern) {
+            if (!mapal.store.hasStorage()) return;
+            
+            if (window.sessionStorage.getItem( pattern+'-count' ) === null) {
+                window.sessionStorage.setItem( pattern+'-count', '0' );
+            }
+        },
+        
+        'hasStorage': function() {
+            return typeof window.sessionStorage !== 'undefined';
+        }
+    }
+    });
+})(jQuery);
