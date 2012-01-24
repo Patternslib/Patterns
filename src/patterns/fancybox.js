b5576f4f34a51c6db3c37291fa6b9e347abed2cf
Florian Friesdorf <flo@chaoflow.net>
2012-01-24 20:49:23 +0100

    whitespace-cleanup of fancybox.js

--- src/patterns/fancybox.js
+++ src/patterns/fancybox.js
@@ -7,25 +7,25 @@
  * Copyright 2011 SYSLAB.COM GmbH
  */
 $.extend( mapal.patterns, {
-	"fancybox": {
-		execute: function(elem, url, sources, params, event) {
-			//var $this = $(event.target);
-			
-			//var modifier = /![a-zA-Z]+/.exec(params);
-			//var className = /[\.][a-zA-Z\-0-9_]+/.exec(params);
-			var options = {};
-			
-			//if (modifier && modifier.length > 0) {
-				//options.type = modifier[0].slice(1);
-			//} else {
-			if (params['type'])
-				options.type = params['type'];
-			//}
-			
-			options.href = url + (sources.length > 0 ? '#' + sources[0] : '');
-									
-			$.fancybox( options );
-			$.fancybox.resize();
-		}
-	}
+    "fancybox": {
+        execute: function(elem, url, sources, params, event) {
+            //var $this = $(event.target);
+
+            //var modifier = /![a-zA-Z]+/.exec(params);
+            //var className = /[\.][a-zA-Z\-0-9_]+/.exec(params);
+            var options = {};
+
+            //if (modifier && modifier.length > 0) {
+            //options.type = modifier[0].slice(1);
+            //} else {
+            if (params['type'])
+                options.type = params['type'];
+            //}
+
+            options.href = url + (sources.length > 0 ? '#' + sources[0] : '');
+
+            $.fancybox( options );
+            $.fancybox.resize();
+        }
+    }
 });
