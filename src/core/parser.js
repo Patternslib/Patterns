7a490eb862de264681e3d3de393f491821d8b953
Florian Friesdorf <flo@chaoflow.net>
2012-03-05 14:56:41 +0100

    require for parser and jasmine tests of parser

--- src/core/parser.js
+++ src/core/parser.js
@@ -4,82 +4,87 @@
  *
  * Copyright 2012 Simplon B.V.
  */
+define([], function() {
 
-function ArgumentParser() {
-    this.options = {};
-    this.parameters = [];
-    this.named_parameters = {};
-}
-
-
-ArgumentParser.prototype = {
-    log: function(msg) {
-        try {
-            console.log(msg);
-        } catch(e) {
-        }
-    },
-
-    add_argument: function(name, default_value) {
-        var parameter = {"name" : name,
-                         "default": default_value};
-        this.parameters.push(parameter);
-        this.named_parameters[name] = parameter;
-    },
-
-    trim:  function(string) {
-        if (String.prototype.trim) {
-            // Firefox 3.5+, Safari 5+, IE9+ in standards mode, Chrome 5+,
-            // Opera 10.5+
-            return string.trim();
-        } else {
-            // All the rest
-            return string.replace(/^\s+|\s+$/g, "");
-        }
-    },
-
-    parse: function(parameter) {
-        var parts = parameter.split(";"),
-     named_param_pattern = /^\s*([a-zA-z]+)\s*:(.*)/,
-     part, matches, i;
-
-        // Popuplate options with default values
-        for (i=0; i<this.parameters.length; i++) {
-            this.options[this.parameters[i].name] = this.parameters[i]["default"];
-        }
-
-        // Grab all positional parameters
-        i=-1;
-        while (parts.length) {
-            i++;
-            if (i>=this.parameters.length) {
-                break;
-            }
-            part=this.trim(parts.shift());
-            if (!part)
-                continue;
-            if (named_param_pattern.test(part)) {
-                parts.unshift(part);
-                break;
-            }
-            this.options[this.parameters[i].name] = this.trim(part);
-        }
-
-        // Handle all named parameters
-        for (i=0; i<parts.length; i++) {
-            matches = parts[i].match(named_param_pattern);
-            if (!matches) {
-                this.log("Positional parameters not allowed after named parameters");
-                break;
-            }
-            if (this.named_parameters[matches[1]]===undefined) {
-                this.log("Unknown named parameter " + matches[1]);
-                continue;
-            }
-            this.options[matches[1]] = this.trim(matches[2]);
-        }
+    function ArgumentParser() {
+        this.options = {};
+        this.parameters = [];
+        this.named_parameters = {};
     }
-};
+
+
+    ArgumentParser.prototype = {
+        log: function(msg) {
+            try {
+                console.log(msg);
+            } catch(e) {
+            }
+        },
+
+        add_argument: function(name, default_value) {
+            var parameter = {"name" : name,
+                             "default": default_value};
+            this.parameters.push(parameter);
+            this.named_parameters[name] = parameter;
+        },
+
+        trim:  function(string) {
+            if (String.prototype.trim) {
+                // Firefox 3.5+, Safari 5+, IE9+ in standards mode, Chrome 5+,
+                // Opera 10.5+
+                return string.trim();
+            } else {
+                // All the rest
+                return string.replace(/^\s+|\s+$/g, "");
+            }
+        },
+
+        parse: function(parameter) {
+            var parts = parameter.split(";"),
+                named_param_pattern = /^\s*([a-zA-z]+)\s*:(.*)/,
+                part, matches, i;
+
+            // Popuplate options with default values
+            for (i=0; i<this.parameters.length; i++) {
+                this.options[this.parameters[i].name] = this.parameters[i]["default"];
+            }
+
+            // Grab all positional parameters
+            i=-1;
+            while (parts.length) {
+                i++;
+                if (i>=this.parameters.length) {
+                    break;
+                }
+                part=this.trim(parts.shift());
+                if (!part)
+                    continue;
+                if (named_param_pattern.test(part)) {
+                    parts.unshift(part);
+                    break;
+                }
+                this.options[this.parameters[i].name] = this.trim(part);
+            }
+
+            // Handle all named parameters
+            for (i=0; i<parts.length; i++) {
+                matches = parts[i].match(named_param_pattern);
+                if (!matches) {
+                    this.log("Positional parameters not allowed after named parameters");
+                    break;
+                }
+                if (this.named_parameters[matches[1]]===undefined) {
+                    this.log("Unknown named parameter " + matches[1]);
+                    continue;
+                }
+                this.options[matches[1]] = this.trim(matches[2]);
+            }
+        }
+    };
+
+    return ArgumentParser;
+
+});
 
 /* jslint devel: true, browser: true, continue: true, sloppy: true, white: true, plusplus: true, regexp: true, indent: 8
  */
--- tests/spec/parser.js
+++ tests/spec/parser.js
@@ -1,78 +1,85 @@
-describe("ArgumentParser", function() {
-    describe("trim", function() {
-        it("No whitespace", function() {
-            parser=new ArgumentParser();
-            expect(parser.trim("foo")).toEqual("foo");
+define([
+    'require',
+    '../../src/core/parser'
+], function(require) {
+    var ArgumentParser = require('../../src/core/parser');
+
+    describe("ArgumentParser", function() {
+        describe("trim", function() {
+            it("No whitespace", function() {
+                var parser=new ArgumentParser();
+                expect(parser.trim("foo")).toEqual("foo");
+            });
+
+            it("Leading whitespace", function() {
+                var parser=new ArgumentParser();
+                expect(parser.trim("  foo")).toEqual("foo");
+            });
+
+            it("Trailing whitespace", function() {
+                var parser=new ArgumentParser();
+                expect(parser.trim("foo  ")).toEqual("foo");
+            });
+
+            it("Whitespace everywhere", function() {
+                var parser=new ArgumentParser();
+                expect(parser.trim("  f o o  ")).toEqual("f o o");
+            });
         });
 
-        it("Leading whitespace", function() {
-            parser=new ArgumentParser();
-            expect(parser.trim("  foo")).toEqual("foo");
-        });
 
-        it("Trailing whitespace", function() {
-            parser=new ArgumentParser();
-            expect(parser.trim("foo  ")).toEqual("foo");
-        });
+        describe("parse", function() {
+            it("Positional argument", function() {
+                var parser=new ArgumentParser();
+                parser.add_argument("selector");
+                parser.parse(".MyClass");
+                expect(parser.options.selector).toEqual(".MyClass");
+            });
 
-        it("Whitespace everywhere", function() {
-            parser=new ArgumentParser();
-            expect(parser.trim("  f o o  ")).toEqual("f o o");
-        });
-    });
+            it("Default value", function() {
+                var parser=new ArgumentParser();
+                parser.add_argument("selector", "default");
+                parser.parse("");
+                expect(parser.options.selector).toEqual("default");
+            });
 
+            it("Use default for empty value", function() {
+                var parser=new ArgumentParser();
+                parser.add_argument("first", "default");
+                parser.add_argument("second");
+                parser.parse("; bar");
+                expect(parser.options.first).toEqual("default");
+                expect(parser.options.second).toEqual("bar");
+            });
 
-    describe("parse", function() {
-        it("Positional argument", function() {
-            parser=new ArgumentParser();
-            parser.add_argument("selector");
-            parser.parse(".MyClass");
-            expect(parser.options.selector).toEqual(".MyClass");
-        });
+            it("Named argument", function() {
+                var parser=new ArgumentParser();
+                parser.add_argument("selector");
+                parser.add_argument("attr");
+                parser.parse("attr: class");
+                expect(parser.options.attr).toEqual("class");
+            });
 
-        it("Default value", function() {
-            parser=new ArgumentParser();
-            parser.add_argument("selector", "default");
-            parser.parse("");
-            expect(parser.options.selector).toEqual("default");
-        });
+            it("Extra colons in named argument", function() {
+                var parser=new ArgumentParser();
+                parser.add_argument("selector");
+                parser.parse("selector: nav:first");
+                expect(parser.options.selector).toEqual("nav:first");
+            });
 
-        it("Use default for empty value", function() {
-            parser=new ArgumentParser();
-            parser.add_argument("first", "default");
-            parser.add_argument("second");
-            parser.parse("; bar");
-            expect(parser.options.first).toEqual("default");
-            expect(parser.options.second).toEqual("bar");
-        });
+            it("Ignore extra positional parameters", function() {
+                var parser=new ArgumentParser();
+                parser.add_argument("foo");
+                parser.parse("bar; buz");
+                expect(parser.options.foo).toEqual("bar");
+            });
 
-        it("Named argument", function() {
-            parser=new ArgumentParser();
-            parser.add_argument("selector");
-            parser.add_argument("attr");
-            parser.parse("attr: class");
-            expect(parser.options.attr).toEqual("class");
-        });
-
-        it("Extra colons in named argument", function() {
-            parser=new ArgumentParser();
-            parser.add_argument("selector");
-            parser.parse("selector: nav:first");
-            expect(parser.options.selector).toEqual("nav:first");
-        });
-
-        it("Ignore extra positional parameters", function() {
-            parser=new ArgumentParser();
-            parser.add_argument("foo");
-            parser.parse("bar; buz");
-            expect(parser.options.foo).toEqual("bar");
-        });
-
-        it("Ignore unknown named parameter", function() {
-            parser=new ArgumentParser();
-            parser.add_argument("selector");
-            parser.parse("attr: class");
-            expect(parser.options.attr).not.toBeDefined();
+            it("Ignore unknown named parameter", function() {
+                var parser=new ArgumentParser();
+                parser.add_argument("selector");
+                parser.parse("attr: class");
+                expect(parser.options.attr).not.toBeDefined();
+            });
         });
     });
 });
