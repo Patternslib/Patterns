20375100b238084f8afed902d679429784c5278d
Florian Friesdorf <flo@chaoflow.net>
2012-03-05 14:56:41 +0100

    require for parser and jasmine tests of parser

---
+++ tests/main.js
@@ -0,0 +1,28 @@
+require([
+    'require',
+    './spec/parser'
+], function(require) {
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 1000;
+
+    var trivialReporter = new jasmine.TrivialReporter();
+
+    jasmineEnv.addReporter(trivialReporter);
+
+    jasmineEnv.specFilter = function(spec) {
+        return trivialReporter.specFilter(spec);
+    };
+
+    var currentWindowOnload = window.onload;
+
+    window.onload = function() {
+        if (currentWindowOnload) {
+            currentWindowOnload();
+        }
+        execJasmine();
+    };
+
+    function execJasmine() {
+        jasmineEnv.execute();
+    }
+});
