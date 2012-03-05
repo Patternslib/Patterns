7c9110282b4e76a2e9ff650038d78ab4d5a63c98
Florian Friesdorf <flo@chaoflow.net>
2012-03-05 18:40:20 +0100

    return opts, don't keep them as state

--- tests/spec/parser.js
+++ tests/spec/parser.js
@@ -28,53 +28,53 @@ define([
             it("Positional argument", function() {
                 var parser=new ArgumentParser();
                 parser.add_argument("selector");
-                parser.parse(".MyClass");
-                expect(parser.options.selector).toEqual(".MyClass");
+                var opts = parser.parse(".MyClass");
+                expect(opts.selector).toEqual(".MyClass");
             });
 
             it("Default value", function() {
                 var parser=new ArgumentParser();
                 parser.add_argument("selector", "default");
-                parser.parse("");
-                expect(parser.options.selector).toEqual("default");
+                var opts = parser.parse("");
+                expect(opts.selector).toEqual("default");
             });
 
             it("Use default for empty value", function() {
                 var parser=new ArgumentParser();
                 parser.add_argument("first", "default");
                 parser.add_argument("second");
-                parser.parse("; bar");
-                expect(parser.options.first).toEqual("default");
-                expect(parser.options.second).toEqual("bar");
+                var opts = parser.parse("; bar");
+                expect(opts.first).toEqual("default");
+                expect(opts.second).toEqual("bar");
             });
 
             it("Named argument", function() {
                 var parser=new ArgumentParser();
                 parser.add_argument("selector");
                 parser.add_argument("attr");
-                parser.parse("attr: class");
-                expect(parser.options.attr).toEqual("class");
+                var opts = parser.parse("attr: class");
+                expect(opts.attr).toEqual("class");
             });
 
             it("Extra colons in named argument", function() {
                 var parser=new ArgumentParser();
                 parser.add_argument("selector");
-                parser.parse("selector: nav:first");
-                expect(parser.options.selector).toEqual("nav:first");
+                var opts = parser.parse("selector: nav:first");
+                expect(opts.selector).toEqual("nav:first");
             });
 
             it("Ignore extra positional parameters", function() {
                 var parser=new ArgumentParser();
                 parser.add_argument("foo");
-                parser.parse("bar; buz");
-                expect(parser.options.foo).toEqual("bar");
+                var opts = parser.parse("bar; buz");
+                expect(opts.foo).toEqual("bar");
             });
 
             it("Ignore unknown named parameter", function() {
                 var parser=new ArgumentParser();
                 parser.add_argument("selector");
-                parser.parse("attr: class");
-                expect(parser.options.attr).not.toBeDefined();
+                var opts = parser.parse("attr: class");
+                expect(opts.attr).not.toBeDefined();
             });
         });
     });
