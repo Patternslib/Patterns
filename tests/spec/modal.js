991cb8b95b2b2cc608bc2e944ff9a7e190b89141
Markus Maier <markusmaier@mytum.de>
2012-03-15 20:22:09 +0100

    further work on modal
    different structures implemented in .../tests/modal.html for testing
    added tests to .../tests/spec/modal.js
    fixed typo/bug in .../src/patterns/modal.js

--- tests/spec/modal.js
+++ tests/spec/modal.js
@@ -7,20 +7,42 @@ define([
     '../../src/patterns'
 ], function(require) {
     var patterns = require('../../src/patterns');
-    var dom = function() {
-        return $('#jasmine-fixtures');
+    var finds = function(selector) {
+	selector = selector || '';
+        return $('#jasmine-fixtures ' + selector);
     };
 
     describe('modal', function() {
         beforeEach(function() {
             loadFixtures('modal.html');
-            patterns.scan(dom());
+            patterns.scan(finds());
         });
-
-        describe('init', function() {
-            it("creates panel-content", function() {
-                expect(dom()).toContain('.panel-content');
+	for (i=1; i<=3; i++) {
+	    var id = '#modal' + i;
+            describe('init '+id, function() {
+		it("creates body", function() {
+                    expect(finds(id)).toContain('.body');
+		});
+		it("creates header", function() {
+		    expect(finds(id)).toContain('.header');
+		});
+		it("insert closebutton into header", function() {
+		    expect($('.header',finds(id))).toContain('.close-panel');
+		});
+		it("function remove modal", function() {
+		    $('.modal', finds(id)).remove();
+		    expect(finds(id)).not.toContain('.modal');
+		});
+		it("remove modal on button click", function() {
+		    finds(id).find('.close-panel').click();
+        	    expect(finds(id)).not.toContain('.modal');
+		});
+		// last test doesn't work properly yet
+/*		it("remove modal on ESC", function() {
+		    finds(id).find('.modal').keyup(27);
+        	    expect(finds(id)).not.toContain('.modal');
+		});*/
             });
-        });
+	}
     });
 });
