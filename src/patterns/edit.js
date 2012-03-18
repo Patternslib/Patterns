b12bf682fdcff88cee4dbafb282832e2e9e533c8
Florian Friesdorf <flo@chaoflow.net>
2012-03-18 03:28:33 +0100

    hack edit to save to textarea and ajax submit

--- src/patterns/edit.js
+++ src/patterns/edit.js
@@ -17,6 +17,45 @@ define([
             Aloha.execCommand('bold', false, '');
         });
 
+
+        // XXX: just copied from autosubmit - we need a generic form handling
+
+        // prepare ajax request and submit function
+        var params = {
+            error: function(jqXHR, textStatus, errorThrown) {
+                var msg = [jqXHR.status, textStatus,
+                           $form.attr('action')].join(' '),
+                    // XXX: error notification pattern!
+                    $error = $('<div class="modal"><h3>Error</h3><div class="error message">'+msg+'</div></div>');
+                inject.append($error, $('body'));
+                console.error(url, jqXHR, textStatus, errorThrown);
+            },
+            success: function(data, textStatus, jqXHR) {
+                if (!data) return;
+                var $forms = $(data).find('form[id]');
+                $forms.each(function() {
+                    var $form = $(this),
+                        id = $(this).attr('id'),
+                        $ourform = $('#' + id);
+                    if ($ourform.length > 0) {
+                        $ourform.attr({action: $form.attr('action')});
+                    } else {
+                        console.warn(
+                            'Ignored form in respone data: not matching id', $form);
+                    }
+                });
+            }
+        };
+        var submit = function(ev) {
+            ev.preventDefault();
+            $el.html($('.aloha-edit').html());
+            $form.ajaxSubmit(params);
+        };
+
+
+
+        // take over form submit - XXX: should hook into normal submit
+        $form.find('button[name=submit]').on('click', submit);
     };
 
     var pattern = {
