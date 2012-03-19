5e5e51cc3e64baa6b8967c6befe259651f4db728
Florian Friesdorf <flo@chaoflow.net>
2012-03-19 19:27:19 +0100

    whitespace-cleanup

--- tests/spec/modal.js
+++ tests/spec/modal.js
@@ -8,7 +8,7 @@ define([
 ], function(require) {
     var patterns = require('../../src/patterns');
     var finds = function(selector) {
-	selector = selector || '';
+        selector = selector || '';
         return $('#jasmine-fixtures ' + selector);
     };
 
@@ -17,32 +17,32 @@ define([
             loadFixtures('modal.html');
             patterns.scan(finds());
         });
-	for (i=1; i<=3; i++) {
-	    var id = '#modal' + i;
+        for (i=1; i<=3; i++) {
+            var id = '#modal' + i;
             describe('init '+id, function() {
-		it("creates body", function() {
+                it("creates body", function() {
                     expect(finds(id)).toContain('.body');
-		});
-		it("creates header", function() {
-		    expect(finds(id)).toContain('.header');
-		});
-		it("insert closebutton into header", function() {
-		    expect($('.header',finds(id))).toContain('.close-panel');
-		});
-		it("function remove modal", function() {
-		    $('.modal', finds(id)).remove();
-		    expect(finds(id)).not.toContain('.modal');
-		});
-		it("remove modal on button click", function() {
-		    finds(id).find('.close-panel').click();
-        	    expect(finds(id)).not.toContain('.modal');
-		});
-		// last test doesn't work properly yet
-/*		it("remove modal on ESC", function() {
-		    finds(id).find('.modal').keyup(27);
-        	    expect(finds(id)).not.toContain('.modal');
-		});*/
+                });
+                it("creates header", function() {
+                    expect(finds(id)).toContain('.header');
+                });
+                it("insert closebutton into header", function() {
+                    expect($('.header',finds(id))).toContain('.close-panel');
+                });
+                it("function remove modal", function() {
+                    $('.modal', finds(id)).remove();
+                    expect(finds(id)).not.toContain('.modal');
+                });
+                it("remove modal on button click", function() {
+                    finds(id).find('.close-panel').click();
+                    expect(finds(id)).not.toContain('.modal');
+                });
+                // last test doesn't work properly yet
+                /*		it("remove modal on ESC", function() {
+                 finds(id).find('.modal').keyup(27);
+                 expect(finds(id)).not.toContain('.modal');
+                 });*/
             });
-	}
+        }
     });
 });
