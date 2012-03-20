82bc54c294f1ce316378b983aebf8e0717ae4dfd
Florian Friesdorf <flo@chaoflow.net>
2012-03-20 02:20:12 +0100

    merge jasmine-settings into tests/main.js

--- tests/jasmine-settings.js
+++
@@ -1,8 +0,0 @@
-define([
-    '../src/lib/order!../src/lib/jquery',
-    '../src/lib/order!../lib/jasmine/lib/jasmine-core/jasmine',
-    '../src/lib/order!../lib/jasmine/lib/jasmine-core/jasmine-html',
-    '../src/lib/order!../lib/jasmine-jquery/lib/jasmine-jquery'
-], function() {
-    jasmine.getFixtures().fixturesPath = './';
-});
