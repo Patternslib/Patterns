a4dcdba99caab1e4f58b719a34e82be4591b8e65
Florian Friesdorf <flo@chaoflow.net>
2012-03-20 02:00:33 +0100

    move jqueryPlugin to attic - it does not work as planned

--- src/attic/jqueryPlugin.js
+++ src/attic/jqueryPlugin.js
@@ -1,12 +1,12 @@
 // Make sure jquery is loaded before jquery plugins are loaded
 define([
     'require',
-    './lib/jquery'
+    './jquery'
 ], function(require) {
     return {
         load: function (name, req, load, config) {
             // wait for jquery to be loaded
-            require('./lib/jquery');
+            require('./jquery');
             //req has the same API as require().
             req([name], function (value) {
                 load(value);
