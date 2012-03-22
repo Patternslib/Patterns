74c891e3717174bada0b57354aacbc8636daca1c
Florian Friesdorf <flo@chaoflow.net>
2012-03-22 21:49:55 +0100

    edit to use ajaxify

--- src/patterns/edit.js
+++ src/patterns/edit.js
@@ -1,9 +1,10 @@
 define([
     'require',
     '../lib/aloha-loader',
-    '../lib/jquery.form/jquery.form'
+    './ajaxify'
 ], function(require) {
-    var Aloha = window.Aloha;
+    var Aloha = window.Aloha,
+        ajaxify = require('./ajaxify').init;
 
     var init = function($el, opts) {
         // find editor controls
@@ -20,14 +21,7 @@ define([
             Aloha.execCommand('bold', false, '');
         });
 
-        // ajaxify form
-        $form.ajaxForm({
-            // in plone we use this to figure out whether a form is
-            // requested or submitted
-            // XXX: consider setting this via $.ajaxSetup in a
-            // specific application
-            data: {submit: "submit"}
-        });
+        ajaxify($form);
 
         // copy aloha textareas to originals before serialization
         // XXX: is there an aloha function for that?
@@ -37,44 +31,6 @@ define([
                     id = $this.attr('id').slice(0, - '-aloha'.length),
                     $target = $('#' + id);
                 $target.html($this.html());
-            });
-        });
-
-
-        // XXX: not specific to edit
-
-        $form.ajaxError(function(ev, xhr, opts, error) {
-            // ajaxHandlers are global, we are only interested in our form
-            if (!(ev.target === $form[0])) return;
-
-            // XXX: this needs to be solved differently
-            var msg = [xhr.status, xhr.statusText, error, opts.url].join(' '),
-                // XXX: error notification pattern!
-                $error = $('<div class="modal">'
-                           + '<h3>Error</h3>'
-                           + '<div class="error message">'+msg+'</div>'
-                           + '</div>');
-            inject.append($error, $('body'));
-            console.error(msg, xhr, opts);
-        });
-
-        $form.ajaxSuccess(function(ev, xhr, opts, data) {
-            // ajaxHandlers are global, we are only interested in our form
-            if (!(ev.target === $form[0])) return;
-
-            // XXX: this needs to be solved differently
-            if (!data) return;
-            var $forms = $(data).find('form[id]');
-            $forms.each(function() {
-                var $form = $(this),
-                    id = $(this).attr('id'),
-                    $ourform = $('#' + id);
-                if ($ourform.length > 0) {
-                    $ourform.attr({action: $form.attr('action')});
-                } else {
-                    console.warn(
-                        'Ignored form in respone data: not matching id', $form);
-                }
             });
         });
     };
