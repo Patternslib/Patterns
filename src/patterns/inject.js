3241a345c1a26876e672774f9d01f4134417904e
Florian Friesdorf <flo@chaoflow.net>
2012-03-23 23:53:55 +0100

    inject based on ajaxify

--- src/patterns/inject.js
+++ src/patterns/inject.js
@@ -2,10 +2,13 @@ define([
     'require',
     '../core/parser',
     '../lib/jquery',
-    '../lib/jquery.form/jquery.form'
+    '../lib/jquery.form/jquery.form',
+    '../logging',
+    './ajaxify'
 ], function(require) {
     var Parser = require('../core/parser'),
-        parser = new Parser("source; target; replace; pre; post; append; prepend");
+        parser = new Parser("source; target; replace; pre; post; append; prepend"),
+        log = require('../logging').getLogger('inject');
 
     var init = function($el, opts) {
         // XXX: if opts, set them on $el as if defined there
@@ -17,16 +20,54 @@ define([
             return $el.attr({href: hrefnext});
         }
 
-        var trigger = function(ev) {
-            ev.preventDefault();
-            pattern.trigger($el);
-        };
+        var ajaxify = require('./ajaxify').init;
+        ajaxify($el);
 
-        if ($el.is('a')) $el.on('click.inject', trigger);
-        if ($el.is('form')) $el.on('submit.inject', trigger);
+        $el.ajaxSuccess(function(ev, jqxhr, ajaxopts, data) {
+            // retrieve href and split into url and default srcid
+            var href = ($el.is('form')
+                        ? $el.attr('action')
+                        : $el.attr('href')).split('#'),
+                srcid = href[1];
+            if (href.length > 2) {
+                log.warn('Ignoring additional source ids:', href.slice(2), $el);
+            }
+
+            // set default source id and parse opts
+            var defaults = { source: srcid && ('#' + srcid) },
+                opts_str = $el.attr('data-inject') || "",
+                opts = parser.parse(opts_str, defaults),
+                callback;
+
+            // default: replace targets content with sources content
+            var method_name = "content";
+
+            // post-process options
+            if (opts.replace) {
+                opts.target = opts.replace;
+                method_name = "replace";
+            }
+            if (opts.pre) {
+                opts.target = opts.pre;
+                method_name = "pre";
+            }
+            if (opts.post) {
+                opts.target = opts.post;
+                method_name = "post";
+            }
+            if (!opts.source) {
+                opts.source = '#__original_body';
+            }
+
+            injector($el, method_name, opts)(data);
+        });
 
         return $el;
     };
+
+    /*
+     * Injection methods
+     */
 
     var _injectmethod = function(name, method) {
         var injectwrapper = function($sources, $targets, suppress) {
@@ -95,6 +136,8 @@ define([
         return $sources;
     });
 
+
+
     // create an injector to be run on ajax success
     var injector = function($el, method_name, opts) {
         // hack to support modals
@@ -121,7 +164,7 @@ define([
             $('body').append($targets);
         }
 
-        var inject = function(data, textStatus, jqXHR) {
+        var inject = function(data) {
             // just copied from old inject code
             data = data
                 .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "")
@@ -163,57 +206,6 @@ define([
         return inject;
     };
 
-    var trigger = function($el) {
-        // retrieve href and split into url and default srcid
-        var href = ($el.is('form')
-                    ? $el.attr('action')
-                    : $el.attr('href')).split('#'),
-            url = href.shift(),
-            srcid = href.pop();
-        if (href.length > 0) console.log('inject: only one #source in href');
-
-        // set default source id and parse opts
-        var defaults = { source: srcid && ('#' + srcid) },
-            opts_str = $el.attr('data-inject') || "",
-            opts = parser.parse(opts_str, defaults),
-            callback;
-
-        // default: replace targets content with sources content
-        var method_name = "content";
-
-        // post-process options
-        if (opts.replace) {
-            opts.target = opts.replace;
-            method_name = "replace";
-        }
-        if (opts.pre) {
-            opts.target = opts.pre;
-            method_name = "pre";
-        }
-        if (opts.post) {
-            opts.target = opts.post;
-            method_name = "post";
-        }
-        if (!opts.source) {
-            opts.source = '#__original_body';
-        }
-
-        // perform ajax call
-        var params = {
-            url: url,
-            type: $el.is('form') ? 'POST' : 'GET',
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.error(url, jqXHR, textStatus, errorThrown);
-            },
-            success: injector($el, method_name, opts)
-        };
-        if ($el.is('form')) {
-            $el.ajaxSubmit(params);
-        } else {
-            $.ajax(params);
-        }
-    };
-
     var pattern = {
         initialised_class: 'inject',
         markup_trigger: 'a.inject, a[data-inject], form.inject, form[data-inject]',
@@ -224,8 +216,7 @@ define([
         post: post,
         append: append,
         prepend: prepend,
-        replace: replace,
-        trigger: trigger
+        replace: replace
     };
 
     return pattern;
