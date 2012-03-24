fa6345355c082167f139a25b78e4aaeada5741e6
Florian Friesdorf <flo@chaoflow.net>
2012-03-24 02:26:09 +0100

    signal skipped element by returning false

--- src/patterns/ajaxify.js
+++ src/patterns/ajaxify.js
@@ -10,7 +10,7 @@ define([
         // skip elements that are covered by old-style injection
         if ($el.is('.injection,[data-injection]')) {
             log.debug('skipping element claimed by old injection', $el);
-            return;
+            return false;
         }
 
         // ajaxify form
@@ -73,6 +73,8 @@ define([
                 }
             });
         });
+
+        return $el;
     };
 
     return {
