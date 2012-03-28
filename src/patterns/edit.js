df7159f244c09a012cfb128db793e0cb1fabf1a5
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-03-27 19:34:50 -0700

    Prototype of contentEditable editor

--- src/patterns/edit.js
+++ src/patterns/edit.js
@@ -4,65 +4,64 @@ define([
     '../logging',
     '../patterns'
 ], function(require) {
-    var Aloha = window.Aloha,
-        log = require('../logging').getLogger('edit');
-
-    // var buttons = {
-    //     emphasise: "i",
-    //     strong: "b",
-    //     "list-ordered": ["<ol />", "<li />"],
-    //     "list-unordered": ["<ul />", "<li />"],
-    //     paragraph: "p",
-    //     h1: "h1",
-    //     h2: "h2",
-    //     h3: "h3"
-    // };
-
+    log = require('../logging').getLogger('edit');
+    
+    // Grab a element of the editor controls
     var init = function($el, opts) {
-        // find editor controls
-        var $form = $el.parents('form'),
-            $ctrls = $('.editor-controls'),
-            buttons = {};
-
-        buttons.b = $ctrls.find('.strong');
-        buttons.bold = $ctrls.find('.strong');
-        buttons.i = $ctrls.find('.emphasised');
-        buttons.italic = $ctrls.find('.emphasised');
-        buttons.ol = $ctrls.find('.list-ordered');
-        buttons.insertorderedlist = $ctrls.find('.list-ordered');
-        buttons.ul = $ctrls.find('.list-unordered');
+        
+        var $form    = $el.parents('form'),
+            $ctrls   = $('.editor-ctrls'),
+            buttons  = {};
+        
+        buttons.b                   = $ctrls.find('.strong');
+        buttons.bold                = $ctrls.find('.strong');
+        buttons.i                   = $ctrls.find('.emphasised');
+        buttons.italic              = $ctrls.find('.emphasised');
+        buttons.ol                  = $ctrls.find('.list-ordered');
+        buttons.insertorderedlist   = $ctrls.find('.list-ordered');
+        buttons.ul                  = $ctrls.find('.list-unordered');
         buttons.insertunorderedlist = $ctrls.find('.list-unordered');
-        buttons.p = $ctrls.find('.paragraph');
-        buttons.insertparagraph = $ctrls.find('.paragraph');
+        buttons.p                   = $ctrls.find('.paragraph');
+        buttons.insertparagraph     = $ctrls.find('.paragraph');
+        buttons.h1                  = $ctrls.find('.header_1');
+        buttons.inserth1            = $ctrls.find('.header_1');
+        buttons.h2                  = $ctrls.find('.header_2');
+        buttons.inserth2            = $ctrls.find('.header_2');
+        buttons.h3                  = $ctrls.find('.header_3');
+        buttons.inserth3            = $ctrls.find('.header_3');
 
         // ensure form is ajaxified
         var ajaxify = require('../patterns').ajaxify.init;
         ajaxify($form);
-
-        // activate aloha on element
-        $el.aloha();
-        log.debug(Aloha.querySupportedCommands());
-
+        
+        // Enables contentEditable
+        $form.attr('contenteditable','true');
         // log editor control clicks
         $ctrls.find('button').on('click', function(ev) {
             log.debug('clicked', $(ev.target), ev);
         });
 
-        // bind editor controls to aloha commands
+        // execCommand is invoked upon a document
+        // the last selected contenteditable is what
+        // recieves the application of execution.
+       
+        // shit happens
         buttons.b.on('click', function(ev) {
-            Aloha.execCommand('bold', false, '');
+            log.debug('bold', $(ev.target), ev);
+            document.execCommand("Bold");
         });
+        
         buttons.i.on('click', function(ev) {
-            Aloha.execCommand('italic', false, '');
+            document.execCommand('italic', false, '');
         });
         buttons.ol.on('click', function(ev) {
-            Aloha.execCommand('insertorderedlist');
+            document.execCommand('insertorderedlist');
         });
         buttons.ol.on('click', function(ev) {
-            Aloha.execCommand('insertorderedlist');
+            document.execCommand('insertorderedlist');
         });
         buttons.ul.on('click', function(ev) {
-            Aloha.execCommand('insertunorderedlist');
+            document.execCommand('insertunorderedlist');
         });
 
 
@@ -77,30 +76,9 @@ define([
                 log.debug('selected', tag);
             });
         };
-
-        // toggle button according to selection
-        // Aloha.bind('aloha-selection-changed', function(ev, selection) {
-        //     setstate(selection);
-        // });
-
-        // Aloha.bind('aloha-command-executed', function(ev, cmd) {
-        //     log.debug('executed', cmd);
-        //     var $button = buttons[cmd];
-        //     if ($button) $button.toggleClass('selected');
-        // });
-
-        // copy aloha textareas to originals before serialization
-        // XXX: is there an aloha function for that?
-        $form.on('form-pre-serialize', function(ev, $form, opts, veto) {
-            $form.find('.aloha-textarea').each(function() {
-                var $this = $(this),
-                    id = $this.attr('id').slice(0, - '-aloha'.length),
-                    $target = $('#' + id);
-                $target.html($this.html());
-            });
-        });
     };
-
+    
+    
     var pattern = {
         markup_trigger: 'form textarea.edit',
         initialised_class: 'edit',
@@ -108,34 +86,3 @@ define([
     };
     return pattern;
 });
-
-//         Aloha.settings = {
-//             locale: 'en',
-//             plugins: {
-//                 format: {
-//                     config: [  'b', 'i', 'p', 'sub', 'sup', 'del', 'title', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre', 'removeFormat' ]
-//                 }
-//             },
-//             sidebar: {
-//                 disabled: true
-//             }
-//         };
-
-// Aloha.settings.toolbar = {
-//     tabs: {
-//         format : {
-//             inline: [ 'bold', 'italic', 'striketrough',
-//                 'abbreviation', 'spacer', 'metaview' ],
-//             paragraph: [ 'formatparagraph' ]
-//         },
-//         insert: {
-//             general: [ 'insertable', 'charakterpicker', 'insertlink' ],
-//             media: [ 'image', 'video' ]
-//         },
-//         link: {
-//             link: [ 'editlink' ]
-//         }
-//     }
-// };
-
-
