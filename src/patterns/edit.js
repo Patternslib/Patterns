7af72e6944a85d80b136dc05fd7183f83f5df9e2
Florian Friesdorf <flo@chaoflow.net>
2012-03-30 13:56:13 +0200

    whitespace-cleanup

--- src/patterns/edit.js
+++ src/patterns/edit.js
@@ -5,22 +5,22 @@ define([
     '../patterns'
 ], function(require) {
     log = require('../logging').getLogger('edit');
-    
+
     // Grab a element of the editor controls
     var init = function($el, opts) {
         var $form    = $el.parents('form'),
             $ctrls   = $('.editor-ctrls'),
             buttons  = {};
-                
- 
+
+
         // ensure form is ajaxified
         var ajaxify = require('../patterns').ajaxify.init;
         ajaxify($form);
-  
+
         // I have taken the liberty of commenting out buttons
         // that I believe serve no purpose, if they end up being
         // critical, just uncomment.
-        
+
         // buttons.b                   = $ctrls.find('.strong');
         buttons.bold                = $ctrls.find('.strong');
         // buttons.i                   = $ctrls.find('.emphasised');
@@ -39,74 +39,74 @@ define([
         buttons.inserth3            = $ctrls.find('.header_3');
         buttons.upload_image        = $ctrls.find('.upload_image');
         buttons.link_image          = $ctrls.find('.link_image');
-        
+
         var button_handler = {
-                'bold'                  : function(){ document.execCommand('bold'); }, 
-                
-                'italic'                : function(){ document.execCommand('italic'); },
-                
-                'insertparagraph'       : function(){ return 0; },
-                
-                'insertorderedlist'     : function(){ document.execCommand('insertorderedlist'); },
-                
-                'insertunorderedlist'   : function(){ document.execCommand('insertunorderedlist'); },
-                
-                'inserth1'              : function(){ wrap_selection('h1') },
-                
-                'inserth2'              : function(){ wrap_selection('h2') },
-                
-                'inserth3'              : function(){ wrap_selection('h3') },
+            'bold'                  : function(){ document.execCommand('bold'); },
 
-                'clear'                 : function(){ 
-                                                var selection_node = $(window.getSelection().anchorNode);
-                                                document.execCommand('removeformat'); 
-                                                if (is_contenteditable(selection_node)) { 
-                                                    selection_node.unwrap(); 
-                                                    if (!$(selection_node).parent().is("p")) { 
-                                                        $(selection_node).wrap('<p>'); 
-                                                    }
-                                                }
-                                          },
-                
-                'upload_image'          : function(){ document.execCommand(); },
+            'italic'                : function(){ document.execCommand('italic'); },
 
-                'link_image'            : function(){ 
-                                                var source = prompt('URL of Image');
-                                                if(source) { 
-                                                  document.execCommand('insertImage', false, source); 
-                                                }
-                                          }
-            };
-        
+            'insertparagraph'       : function(){ return 0; },
+
+            'insertorderedlist'     : function(){ document.execCommand('insertorderedlist'); },
+
+            'insertunorderedlist'   : function(){ document.execCommand('insertunorderedlist'); },
+
+            'inserth1'              : function(){ wrap_selection('h1') },
+
+            'inserth2'              : function(){ wrap_selection('h2') },
+
+            'inserth3'              : function(){ wrap_selection('h3') },
+
+            'clear'                 : function(){
+                var selection_node = $(window.getSelection().anchorNode);
+                document.execCommand('removeformat');
+                if (is_contenteditable(selection_node)) {
+                    selection_node.unwrap();
+                    if (!$(selection_node).parent().is("p")) {
+                        $(selection_node).wrap('<p>');
+                    }
+                }
+            },
+
+            'upload_image'          : function(){ document.execCommand(); },
+
+            'link_image'            : function(){
+                var source = prompt('URL of Image');
+                if(source) {
+                    document.execCommand('insertImage', false, source);
+                }
+            }
+        };
+
 
         // utility method for determining if something is contenteditable
         var is_contenteditable = function(element) {
             mapped_elements = $(element).parents().map(function() {
-                            if ($(this).is("[contenteditable='true']")) {
-                                        return true;
-                            }
-                              }).get();
-            
+                if ($(this).is("[contenteditable='true']")) {
+                    return true;
+                }
+            }).get();
+
             // mapped_element[0] is the closest parent
             // in the document tree -- If it's true
             // the element will inherit contentEditable
             if (mapped_elements[0]) { return true; }
             return false;
-         }
-        
+        }
+
         // simply replaces
-        // rather than toggles, but 
+        // rather than toggles, but
         // theres no reason you couldn't have
         // it do both!
         var wrap_selection = function(wrap_html){
             var selection_node = $(window.getSelection().anchorNode);
-            if(is_contenteditable(selection_node)) { 
+            if(is_contenteditable(selection_node)) {
                 // You just want to unwrap if your
                 // parent is already selected
-                if(selection_node.parent().is(wrap_html)) { 
+                if(selection_node.parent().is(wrap_html)) {
                     selection_node.unwrap();
-                } 
-                else { 
+                }
+                else {
                     selection_node.wrap("<" + wrap_html + ">");
                 }
             }
@@ -114,42 +114,42 @@ define([
             // this is a hacky replacement
             selection_node.attr('contenteditable', true);
         };
-        
-        // lame helper that 
+
+        // lame helper that
         // toggles the class,
         // triggers the handler
         var button_click = function(element) {
-            buttons[element].toggleClass('selected'); 
+            buttons[element].toggleClass('selected');
             button_handler[element]();
         }
- 
+
         // bind click to button_click()/1
         for (var key in buttons) {
-                buttons[key].click(function(element){
-                 return function() {
-                        log.debug('clicked', element); 
-                        button_click(element);
-                        };
+            buttons[key].click(function(element){
+                return function() {
+                    log.debug('clicked', element);
+                    button_click(element);
+                };
             }(key));
         }
 
         // Enables contentEditable
         $('form').attr('contenteditable','true');
-       
-         var setstate = function(selection) {
-             var markup = selection.markupEffectiveAtStart;
-          if (!markup) return;
-             $ctrls.find('*').removeClass('selected');
-             $.each(markup, function(idx, el) {
-                 var tag = el.nodeName.toLowerCase(),
-                 $button = buttons[tag];
-                 if ($button) $button.addClass('selected');
-                 log.debug('selected', tag);
-             });
-         };
-     };
-    
-     var pattern = {
+
+        var setstate = function(selection) {
+            var markup = selection.markupEffectiveAtStart;
+            if (!markup) return;
+            $ctrls.find('*').removeClass('selected');
+            $.each(markup, function(idx, el) {
+                var tag = el.nodeName.toLowerCase(),
+                    $button = buttons[tag];
+                if ($button) $button.addClass('selected');
+                log.debug('selected', tag);
+            });
+        };
+    };
+
+    var pattern = {
         markup_trigger: 'form textarea.edit',
         initialised_class: 'edit',
         init: init
