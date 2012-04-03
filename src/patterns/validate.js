50f8f0e9dc6f9da7aece456eee682b89cf7b2516
Florian Friesdorf <flo@chaoflow.net>
2012-04-03 20:30:07 +0200

    validate: data-required-if

--- src/patterns/validate.js
+++ src/patterns/validate.js
@@ -1,9 +1,28 @@
 define([
     'require',
-    '../../lib/jquery.validate'
+    '../lib/dist/underscore',
+    '../../lib/jquery.validate',
+    '../logging'
 ], function(require) {
+    var log = require('../logging').getLogger('validate');
+
     var init = function($el, opts) {
-        $el.validate();
+        var rules = _.reduce(
+            $el.find('[data-required-if]').toArray(),
+            function(acc, el) {
+                var $el = $(el),
+                    id = $el.attr('id');
+                if (!id) {
+                    log.error('Element needs id, skipping:', $el);
+                    return acc;
+                }
+                acc[id] = {required: $el.data('required-if')};
+                return acc;
+            },
+            {}
+        );
+        log.debug('rules:', rules);
+        $el.validate({rules: rules, debug: true});
         return $el;
     };
 
