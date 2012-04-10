84ec125d9882fc8b0990bae05b9ce088b13819e1
Florian Friesdorf <flo@chaoflow.net>
2012-04-10 14:41:29 +0800

    modals are not ajaxified anymore

--- src/patterns/modal.js
+++ src/patterns/modal.js
@@ -1,8 +1,11 @@
 define([
     'require',
     '../lib/jquery',
-    '../lib/jquery.form/jquery.form'
+    '../lib/jquery.form/jquery.form',
+    '../logging',
+    '../patterns'
 ], function(require) {
+    var log = require('../logging').getLogger('modal');
 
     var init = function($el, opts) {
         var $first = $el.children(':first'),
@@ -40,37 +43,43 @@ define([
         // remove on close-panel button click
         $el.find('.close-panel').on('click.remove.modal', remove);
 
+
+        // ensure element is ajaxified
+        //var ajaxify = require('../patterns').ajaxify.init;
+        //ajaxify($el.find('form'));
+
+
         // close forms that are successfully submitted or show error
-        if ($form) {
-            // prepare ajax request and submit function
-            var params = {
-                data: { submit: "submit" },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    var msg = [jqXHR.status, textStatus,
-                               $form.attr('action')].join(' '),
-                        $errdiv = $el.find('.message.error');
-                    if ($errdiv.length === 0) {
-                        $errdiv = $('<div class="message error"/>');
-                        $errdiv.prependTo($el.find('.body'));
-                    }
-                    $el.removeClass('ajax-in-progress');
-                    $errdiv.html(msg);
-                    console.error(jqXHR, textStatus, errorThrown);
-                },
-                success: function(data, textStatus, jqXHR) {
-                    $el.remove();
-                }
-            };
-            var submit = function(ev) {
-                $el.addClass('ajax-in-progress');
-                ev.preventDefault();
-                $form.ajaxSubmit(params);
-            };
-            $form.find('[type=submit]').on('click', submit);
-            $(document).on('keyup.submit.modal', function(ev) {
-                if (ev.which == 13) submit(ev);
-            });
-        }
+        // if ($form) {
+        //     // prepare ajax request and submit function
+        //     var params = {
+        //         data: { submit: "submit" },
+        //         error: function(jqXHR, textStatus, errorThrown) {
+        //             var msg = [jqXHR.status, textStatus,
+        //                        $form.attr('action')].join(' '),
+        //                 $errdiv = $el.find('.message.error');
+        //             if ($errdiv.length === 0) {
+        //                 $errdiv = $('<div class="message error"/>');
+        //                 $errdiv.prependTo($el.find('.body'));
+        //             }
+        //             $el.removeClass('ajax-in-progress');
+        //             $errdiv.html(msg);
+        //             console.error(jqXHR, textStatus, errorThrown);
+        //         },
+        //         success: function(data, textStatus, jqXHR) {
+        //             $el.remove();
+        //         }
+        //     };
+        //     var submit = function(ev) {
+        //         $el.addClass('ajax-in-progress');
+        //         ev.preventDefault();
+        //         $form.ajaxSubmit(params);
+        //     };
+        //     $form.find('[type=submit]').on('click', submit);
+        //     $(document).on('keyup.submit.modal', function(ev) {
+        //         if (ev.which == 13) submit(ev);
+        //     });
+        // }
 
         return $el;
     };
