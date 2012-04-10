8cc0d0b020c3adb02c4316a8d9b2cbde4bf8860c
Florian Friesdorf <flo@chaoflow.net>
2012-04-10 14:28:18 +0800

    you can't click the triggering element of a modal

--- src/patterns/modal.js
+++ src/patterns/modal.js
@@ -39,10 +39,6 @@ define([
         });
         // remove on close-panel button click
         $el.find('.close-panel').on('click.remove.modal', remove);
-        // remove on click of triggering element
-        if (opts && opts.$trigger_el) {
-            opts.$trigger_el.on('click.remove.modal', remove);
-        }
 
         // close forms that are successfully submitted or show error
         if ($form) {
