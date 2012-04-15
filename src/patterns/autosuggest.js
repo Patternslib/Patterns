3fc69b4ce6be68836391b6e254a12d1ef3d1322d
Florian Friesdorf <flo@chaoflow.net>
2012-04-16 05:35:39 +0800

    fetch autosuggest words from a parents data-auto-suggest-config

--- src/patterns/autosuggest.js
+++ src/patterns/autosuggest.js
@@ -5,17 +5,19 @@ define([
 ], function(require) {
 
     var init = function($el, opts) {
-        $el.autoSuggest([
-            {value: "test1", name: "test1"},
-            {value: "test2", name: "test2"},
-            {value: "test3", name: "test3"},
-            {value: "fest1", name: "fest1"},
-            {value: "fest2", name: "fest2"},
-            {value: "fest3", name: "fest3"}
-        ], {
-            selectedValueProp: "name",
-            searchObjProp: "name"
+        // fetch words for auto-suggestion
+        var $cfg = $el.parents('[data-auto-suggest-config]:first');
+        var words = $cfg.data('auto-suggest-config').split(':')[1].split(/\s*,\s*/);
+        var data = words.map(function(word) {
+            return {value: word, name: word};
         });
+        $el.autoSuggest(
+            data,
+            {
+                selectedValueProp: "name",
+                searchObjProp: "name"
+            }
+        );
     };
 
     var pattern = {
