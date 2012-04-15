85d5a16f5801537ad8fda7d5992e152e500b3c26
Florian Friesdorf <flo@chaoflow.net>
2012-04-16 00:49:07 +0800

    expandable pattern - weird margin-left: -32767px showed up

---
+++ src/patterns/expandable.js
@@ -0,0 +1,34 @@
+define([
+    'require',
+    '../lib/jquery'
+], function(require) {
+
+    var init = function($el, opts) {
+        // find all folders
+        var $folders = $el.find('li.folder');
+
+        // inject span.toggle as first child of each folder
+        $folders.prepend('<span class="toggle"></span>');
+
+        // all folders are implicitly closed
+        $folders.filter(':not(.open,.closed)').addClass('closed');
+
+        // wire spans as control elements
+        var $ctrls = $el.find('span.toggle');
+        $ctrls.each(function() {
+            var $ctrl = $(this),
+                $folder = $ctrl.parent();
+            $ctrl.on('click', function(ev) {
+                $folder.toggleClass('open closed');
+            });
+        });
+    };
+
+    var pattern = {
+        markup_trigger: "ul.expandable",
+        initialised_class: "expandable",
+        init: init
+    };
+
+    return pattern;
+});
