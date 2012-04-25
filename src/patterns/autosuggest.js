24dedfd3563f16af7e4642e72f6a427621b2ac1b
Florian Friesdorf <flo@chaoflow.net>
2012-04-26 00:53:09 +0800

    autosuggest submits on select and remove, if in autosubmit

--- src/patterns/autosuggest.js
+++ src/patterns/autosuggest.js
@@ -1,8 +1,12 @@
+// auto-suggest docs:
+// http://code.drewwilson.com/entry/autosuggest-jquery-plugin
 define([
     'require',
     '../lib/jquery',
+    '../logging',
     '../../lib/jquery.autoSuggest'
 ], function(require) {
+    var log = require('../logging').getLogger('autosuggest');
 
     var init = function($el, opts) {
         // fetch words for auto-suggestion
@@ -16,19 +20,39 @@ define([
         });
         var $prefillcfg = $el.parents('[data-auto-suggest-prefill]:first');
         var prefill = $prefillcfg.data('auto-suggest-prefill') || '';
-        
+
         var $asHtmlIDcfg = $el.parents('[data-auto-suggest-ashtmlid]:first');
         var asHtmlID = $asHtmlIDcfg.data('auto-suggest-ashtmlid') || false;
 
-        $el.autoSuggest(
-            data,
-            {
-                asHtmlID: asHtmlID,
-                preFill: prefill,
-                selectedValueProp: "name",
-                searchObjProp: "name"
-            }
-        );
+        // are we autosubmit?
+        var autosubmit = $el.is('.auto-submit') ||
+                ($el.parents('.auto-submit').length > 0);
+        log.debug('auto-submit', autosubmit, $el);
+
+        var cfg = {
+            asHtmlID: asHtmlID,
+            preFill: prefill,
+            selectedValueProp: "name",
+            searchObjProp: "name",
+            startText: "Enter name here"
+        };
+        var $form;
+        if (autosubmit) {
+            $form = $el.parents('form');
+            cfg.selectionAdded = function($el) {
+                log.debug('submit because selection was added', $el);
+                // trigger the form
+                $form.submit();
+            };
+            cfg.selectionRemoved = function($el) {
+                log.debug('submit because selection was removed', $el);
+                // trigger the form
+                $el.remove();
+                $form.submit();
+            };
+        }
+
+        $el.autoSuggest(data, cfg);
     };
 
     var pattern = {
