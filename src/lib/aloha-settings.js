85d284ebd9d7d73ec73c2d261c516c47cad57a78
Florian Friesdorf <flo@chaoflow.net>
2012-04-26 21:42:52 +0800

    removed aloha

--- src/lib/aloha-settings.js
+++
@@ -1,9 +0,0 @@
-define([
-    'require'
-], function(require) {
-    var Aloha = window.Aloha || (window.Aloha = {});
-    Aloha.settings = {
-        // aloha uses our jquery
-        jQuery: $
-    };
-});
