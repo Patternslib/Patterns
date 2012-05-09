8cc955d73eb50cc138b0a82572b2e2f998252fa5
Florian Friesdorf <flo@chaoflow.net>
2012-05-09 06:30:48 +0200

    inject2 handles anchor injection

--- src/patterns/inject2.js
+++ src/patterns/inject2.js
@@ -1,13 +1,32 @@
 define(function(require) {
     var log = require('../logging').getLogger('inject2'),
-        inject = require('../lib/inject').inject,
+        inject = require('../lib/inject'),
         ajax = require('../lib/ajax');
 
     var init = function($el, opts) {
-        var injecthandler = function() {
+        var injecthandler = function(ev) {
+            if (ev) ev.preventDefault();
             triggerinject($el);
         };
-        $el.on('patterns-collapsible-open', injecthandler);
+
+        // if the element referenced by href-next exists already,
+        // point to it and disable injection
+        // XXX: probably only makes sense for anchors
+        var hrefnext = $el.data('href-next');
+        if (hrefnext && ($(hrefnext).length > 0)) {
+            log.debug('Skipping as href-next already exists', $(hrefnext));
+            return $el.attr({href: hrefnext});
+        }
+
+        if ($el.is('.collapsible')) {
+            log.debug('will trigger on patterns-collapsible-open', $el);
+            $el.on('patterns-collapsible-open', injecthandler);
+        } else if ($el.is('a')) {
+            log.debug('will trigger on click', $el);
+            $el.on('click', injecthandler);
+        }
+
+        return $el;
     };
 
     var extract_opts = function($el, opts_spec) {
@@ -34,6 +53,10 @@ define(function(require) {
                 opts_str = $el.attr(attr) || "",
                 parser;
 
+            // XXX: support data-*-config on parents
+            // in the old new inject that was data-inject-default,
+            // unused so far
+
             // special treatment: href and action always map to url
             if ((attr === "href") || (attr === "action")) {
                 acc.url = opts_str;
@@ -41,10 +64,11 @@ define(function(require) {
                 parser = new Parser(spec);
                 acc = parser.parse(opts_str, acc);
             }
-            post_process_url(acc);
+            if (opts_str) post_process_url(acc);
             return acc;
         }, {});
 
+        log.debug('parsed:', opts, $el);
         return opts;
     };
 
@@ -57,39 +81,81 @@ define(function(require) {
              "source; target; replace; pre; post; prepend; append; url"]
         ];
 
-        var opts = extract_opts($el, opts_spec);
+        var opts = extract_opts($el, opts_spec),
+            $targets;
 
         // special target cases
         if ($el.is('.collapsible')) {
-            opts.$targets = $el.find('.panel-content');
+            $targets = $el.find('.panel-content');
         }
 
-        if (!opts.source) {
-            opts.source = '#__original_body';
+        var source = opts.source || '#__original_body';
+
+        // default: replace targets content with sources content
+        var method_name = "content",
+            method, target;
+
+        // find targets
+        if (!$targets) {
+            if (opts.replace) {
+                target = opts.replace;
+                method_name = "replace";
+            } else if (opts.pre) {
+                target = opts.pre;
+                method_name = "pre";
+            } else if (opts.post) {
+                target = opts.post;
+                method_name = "post";
+            } else if (opts.append) {
+                target = opts.append;
+                method_name = "append";
+            } else if (opts.prepend) {
+                target = opts.prepend;
+                method_name = "prepend";
+            } else if (opts.target) {
+                target = opts.target;
+            } else {
+                target = opts.source;
+            }
+            $targets = $(target);
+        }
+        method = inject[method_name];
+        if ($targets.length === 0) {
+            log.error('empty targets, aborting');
+            return;
         }
 
         if (!opts.url) {
             log.error(
                 'local source not supported yet, need url for now', $el, opts);
-        } else {
-            ajax($el, {
-                url: opts.url,
-                success: function(data, status, jqxhr) {
-                    var $data = $('<div/>').html(
-                        data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "")
-                            .replace(/<head\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/head>/gi, "")
-                            .replace(/<body(.*)>/gi, '<div id="__original_body">')
-                            .replace(/<\/body(.*)>/gi,'</div>')
-                    );
-                    inject($data, opts);
-                }
-            });
+            return;
         }
+
+        ajax($el, {
+            url: opts.url,
+            success: function(data, status, jqxhr) {
+                var $data = $('<div/>').html(
+                    data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "")
+                        .replace(/<head\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/head>/gi, "")
+                        .replace(/<body(.*)>/gi, '<div id="__original_body">')
+                        .replace(/<\/body(.*)>/gi,'</div>')
+                );
+                var $sources = $data.find(source);
+
+                if ($sources.length === 0) {
+                    log.error(
+                        'Aborting, sources are empty for selector:', opts.source, data);
+                    return;
+                }
+                method($sources, $targets);
+            }
+        });
     };
 
     var pattern = {
         initialised_class: 'inject',
-        markup_trigger: '.collapsible[data-inject]',
+        markup_trigger: '.collapsible[data-inject],'
+            + 'a.inject, a[data-inject]',
         init: init,
         triggerinject: triggerinject
     };
