fe994c788d3106d54789291f80aeb1e5599127cc
Florian Friesdorf <flo@chaoflow.net>
2012-05-14 03:23:03 +0200

    quick shot to keep patterns in order

--- src/patterns.js
+++ src/patterns.js
@@ -6,6 +6,26 @@ define(function(require) {
         jquery_plugin = utils.jquery_plugin,
         pimp_pattern = utils.pimp_pattern;
 
+    var patterns_order = [
+        'ajaxify',
+        'autosuggest',
+        'autosubmit',
+        'breadcrumbs',
+        'collapsible',
+        'checklist',
+        'chosen',
+        'edit-tinymce',
+        'expandable',
+        'inject',
+        'inject2',
+        'inject_log_old',
+        'modal',
+        'navigation',
+        'placeholder',
+        'validate'
+    ];
+
+
     var plain_patterns = {
         ajaxify: require('./patterns/ajaxify'),
         autosuggest: require('./patterns/autosuggest'),
@@ -30,8 +50,10 @@ define(function(require) {
 
     // If you use this to register custom patterns make sure to prefix
     // their names to avoid name collision.
+    patterns._ordered = [];
     patterns.register = function(name, pattern) {
         patterns[name] = pattern = pimp_pattern(name, pattern);
+        patterns._ordered.push(name);
         // make available as jquery plugins - this is optional and not
         // needed for the functionality of the patterns library. Should be
         // configurable.
@@ -46,23 +68,21 @@ define(function(require) {
 
     patterns.scan = function(content, opts) {
         var $content = $(content);
-        for (var name in patterns) {
-            if (name === "scan") continue;
-            if (name === "register") continue;
+        patterns._ordered.forEach(function(name) {
             var pattern = patterns[name],
                 trigger = pattern.markup_trigger;
-            if (!trigger) continue;
+            if (!trigger) return;
             trigger = trigger.split(',').map(function(el, idx) {
                 return el + ':not(.cant-touch-this)';
             }).join(',');
             if ($content.is(trigger)) pattern.init($content, opts);
             $content.find(trigger).each(function() { pattern.init($(this), opts); });
-        }
+        });
     };
 
-    for (var name in plain_patterns) {
+    patterns_order.forEach(function(name) {
         patterns.register(name, plain_patterns[name]);
-    }
+    });
 
     // for now this happens in main.js
     //
