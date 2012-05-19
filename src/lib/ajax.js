7751cace034fe6504e6f45f6c83a471e50f5a929
Florian Friesdorf <flo@chaoflow.net>
2012-05-19 16:40:37 +0200

    rudimentary ajaxlib form support

--- src/lib/ajax.js
+++ src/lib/ajax.js
@@ -2,18 +2,26 @@ define(function(require) {
     var log = require('../logging').getLogger('ajaxlib');
 
     var submit = function($el, opts) {
-        if ($el.is('form')) {
-            log.error('forms not supported yet');
-            return;
-        }
-
         // XXX: make these only defaults
         opts.context = $el;
         opts.error = function(a,b,c,d) {
             log.error(arguments);
         };
 
-        $.ajax(opts);
+        if ($el.is('form')) {
+            log.debug('form submit', $el);
+            opts.url = $el.attr('action');
+            if (opts.beforeSerialize) {
+                opts.beforeSerialize();
+            }
+            opts.data = $el.serialize() + '&submit=submit';
+            // XXX: check method on form, use POST as default only
+            opts.type = 'POST';
+            $.ajax(opts);
+        } else {
+            log.debug('submit', $el);
+            $.ajax(opts);
+        }
     };
 
     return submit;
