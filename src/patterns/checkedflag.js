9f1cffe3e5d335a4479f9e93a6ee4df928529821
Wichert Akkerman <wichert@wiggy.net>
2012-09-11 10:30:02 +0200

    Enhance checkedflag to support radio buttons.

--- src/patterns/checkedflag.js
+++ src/patterns/checkedflag.js
@@ -8,23 +8,37 @@ define(function(require) {
     var checkedflag = {
         init: function() {
             $(document)
-               .on("change", "input[type=checkbox]", checkedflag.onChange);
+               .on("change", "input[type=checkbox]", checkedflag.onChangeCheckbox)
+               .on("change", "input[type=radio]", checkedflag.onChangeRadio);
         },
 
         initContent: function(root) {
-             $("input[type=checkbox]", root).each(checkedflag.onChange);
+             $("input[type=checkbox]", root).each(checkedflag.onChangeCheckbox);
+             $("input[type=radio]", root).each(checkedflag.onChangeRadio);
         },
 
-        onChange: function(e) {
+        onChangeCheckbox: function(e) {
             var $el = $(this),
                 $label = $el.closest("label");
 
-            if ($label.length!==0) {
-                if (this.checked) {
-                    $label.removeClass("pt-unchecked").addClass("pt-checked");
-                } else {
-                    $label.addClass("pt-unchecked").removeClass("pt-checked");
-                }
+            if (this.checked) {
+                $label.removeClass("pt-unchecked").addClass("pt-checked");
+            } else {
+                $label.addClass("pt-unchecked").removeClass("pt-checked");
+            }
+        },
+
+        onChangeRadio: function(e) {
+            var $el = $(this),
+                $label = $el.closest("label"),
+                selector = "label:has(input[name=" + this.name + "]:not(:checked))",
+                $siblings = (this.form===null) ? $(selector) : $(selector, this.form);
+
+            $siblings.removeClass("pt-checked").addClass("pt-unchecked");
+            if (this.checked) {
+                $label.removeClass("pt-unchecked").addClass("pt-checked");
+            } else {
+                $label.addClass("pt-unchecked").removeClass("pt-checked");
             }
         }
     };
