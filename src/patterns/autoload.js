feb5199e7a42cb4c4ba05b9c040a5d4ac2563dcf
Wichert Akkerman <wichert@wiggy.net>
2012-10-08 17:08:30 +0200

    Move autoload out of core.

---
+++ src/patterns/autoload.js
@@ -0,0 +1,50 @@
+define([
+    "jquery"
+], function($) {
+    var autoload = {
+        initContent: function(root) {
+            var $root = $(root);
+
+            // find all autoloads
+            $root.find('.autoLoading-visible:not(.cant-touch-this)').each(function() {
+                var $autoload = $(this),
+                    $scrollable = $autoload.parents(":scrollable");
+
+                // ignore executed autoloads
+                if ($autoload.data('autoLoading')) return false;
+
+                // function to trigger the autoload and mark as triggered
+                var trigger = function() {
+                    $autoload.data('autoLoading', true);
+                    $autoload.trigger('click');
+                    return true;
+                };
+
+                // if autoload has no scrollable parent -> trigger it, it is visible
+                if ($scrollable.length === 0) return trigger();
+
+                // if scrollable parent and visible -> trigger it
+                // we only look at the closest scrollable parent, no nesting
+                var checkVisibility = function() {
+                    if ($autoload.data('autoLoading')) return false;
+                    var reltop = $autoload.offset().top - $scrollable.offset().top - 1000,
+                        doTrigger = reltop <= $scrollable.innerHeight();
+                    if (doTrigger) return trigger();
+                    return false;
+                };
+                if (checkVisibility()) return true;
+
+                // wait to become visible - again only immediate scrollable parent
+                $($scrollable[0]).on("scroll", checkVisibility);
+                $(window).on('resize.autoload', checkVisibility);
+                return false;
+            });
+            return $root;
+        }
+    };
+
+    return autoload;
+});
+
+// jshint indent: 4, browser: true, jquery: true, quotmark: double
+// vim: sw=4 expandtab
