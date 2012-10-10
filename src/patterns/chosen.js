8292e8f399ac851598ceaf780addf4bd810257ee
Florian Friesdorf <flo@chaoflow.net>
2012-10-10 17:30:01 +0200

    chosen

--- src/patterns/chosen.js
+++ src/patterns/chosen.js
@@ -1,20 +1,23 @@
 define([
     '../logging',
+    '../registry',
     '../../lib/chosen.jquery'
-], function(logging) {
+], function(logging, registry) {
     var log = logging.getLogger('chosen');
 
-    var init = function($el, opts) {
-        $el.chosen();
-        return $el;
+    var _ = {
+        name: "chosen",
+        trigger: "select.chosen",
+        init: function($el, opts) {
+            $el.chosen();
+            return $el;
+        },
+        destroy: function($el) {
+            // XXX
+        }
     };
 
-    var pattern = {
-        markup_trigger: 'select.chosen',
-        register_jquery_plugin: false,
-        init: init
-    };
-    return pattern;
+    return registry.register(_);
 });
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
 // vim: sw=4 expandtab
