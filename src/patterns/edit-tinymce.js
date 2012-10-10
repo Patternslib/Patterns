983a4e2a74eb9695bc72ddee50438c5875f9fffb
Florian Friesdorf <flo@chaoflow.net>
2012-10-10 17:39:20 +0200

    edit-tinyMCE

--- src/patterns/edit-tinymce.js
+++ src/patterns/edit-tinymce.js
@@ -2,80 +2,83 @@ define([
     'jquery',
     '../lib/ajax',
     '../logging',
+    '../registry',
     '../../lib/tiny_mce/tiny_mce_src'
-], function($, ajax, logging) {
-    var log = logging.getLogger('edit-tinymce');
+], function($, ajax, logging, registry) {
+    var log = logging.getLogger('editTinyMCE');
 
-    var init = function($el, opts) {
-        var $form = $el.parents('form'),
-            $resetbtn = $form.find('[type=reset]'),
-            id = $el.attr('id');
+    var _ = {
+        name: "editTinyMCE",
+        trigger: 'form textarea.edit-tinymce',
+        init: function($el, opts) {
+            var $form = $el.parents('form'),
+                $resetbtn = $form.find('[type=reset]'),
+                id = $el.attr('id');
 
-        // make sure the textarea has an id
-        if (!id) {
-            var formid = $form.attr('id'),
-                name = $el.attr('name');
-            if (!formid) {
-                log.error('Textarea or parent form needs an id', $el, $form);
-                return false;
+            // make sure the textarea has an id
+            if (!id) {
+                var formid = $form.attr('id'),
+                    name = $el.attr('name');
+                if (!formid) {
+                    log.error('Textarea or parent form needs an id', $el, $form);
+                    return false;
+                }
+                if (!name) {
+                    log.error('Textarea needs a name', $el);
+                    return false;
+                }
+                id = formid + '_' + name;
+                if ($('#'+id).length > 0) {
+                    log.error('Textarea needs an id', $el);
+                    return false;
+                }
+                $el.attr({id: id});
             }
-            if (!name) {
-                log.error('Textarea needs a name', $el);
-                return false;
+
+            // read configuration
+            var cfg = $el.data('tinymce-json');
+            if (!cfg) {
+                log.info('data-tinymce-json empty, using default config', $el);
+                cfg = '{}';
             }
-            id = formid + '_' + name;
-            if ($('#'+id).length > 0) {
-                log.error('Textarea needs an id', $el);
-                return false;
-            }
-            $el.attr({id: id});
-        }
+            cfg.elements = id;
+            cfg.mode = 'exact';
+            cfg.readonly = Boolean($el.attr('readonly'));
 
-        // read configuration
-        var cfg = $el.data('tinymce-json');
-        if (!cfg) {
-            log.info('data-tinymce-json empty, using default config', $el);
-            cfg = '{}';
-        }
-        cfg.elements = id;
-        cfg.mode = 'exact';
-        cfg.readonly = Boolean($el.attr('readonly'));
+            // initialize editor
+            var tinymce = tinyMCE.init(cfg);
 
-        // initialize editor
-        var tinymce = tinyMCE.init(cfg);
-
-        // ajaxify form
-        var ajaxopts = {
-            beforeSerialize: (function(id) {
-                return function() {
-                    tinyMCE.editors[id].save();
-                };
-            })(id)
-        };
-
-        $form.on('submit', function(ev) {
-            ev.preventDefault();
-            ajax($form, ajaxopts);
-        });
-
-        // XXX: we hijack the reset button, but currently only reset
-        // the tiny textarea.
-        $resetbtn.on('click', (function(id) {
-            return function(ev) {
-                ev.preventDefault();
-                tinyMCE.editors[id].load();
+            // ajaxify form
+            var ajaxopts = {
+                beforeSerialize: (function(id) {
+                    return function() {
+                        tinyMCE.editors[id].save();
+                    };
+                })(id)
             };
-        })(id));
 
-        return $el;
+            $form.on('submit', function(ev) {
+                ev.preventDefault();
+                ajax($form, ajaxopts);
+            });
+
+            // XXX: we hijack the reset button, but currently only reset
+            // the tiny textarea.
+            $resetbtn.on('click', (function(id) {
+                return function(ev) {
+                    ev.preventDefault();
+                    tinyMCE.editors[id].load();
+                };
+            })(id));
+
+            return $el;
+        },
+        destroy: function($el) {
+            // XXX
+        }
     };
 
-    var pattern = {
-        markup_trigger: 'form textarea.edit-tinymce',
-        initialised_class: 'edit-tinymce',
-        init: init
-    };
-    return pattern;
+    return registry.register(_);
 });
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
 // vim: sw=4 expandtab
