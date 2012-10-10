b2388b0d80592a16c03cb1c1922073081ee81345
Florian Friesdorf <flo@chaoflow.net>
2012-10-10 17:17:13 +0200

    rework collapsible

--- src/patterns/collapsible.js
+++ src/patterns/collapsible.js
@@ -1,87 +1,89 @@
+/*
+ * changes:
+ * - open($el, opts) instead of open($el, duration)
+ * - same for close
+ */
 define([
     'jquery',
-    '../logging'
-], function($, logging) {
+    '../logging',
+    '../registry'
+], function($, logging, registry) {
     var log = logging.getLogger('collapsible');
 
-    var init = function($el, opts) {
-        // create collapsible structure
-        var $ctrl = $el.children(':first'),
-            $content = $el.children(':gt(0)'),
-            $panel;
-        if ($content.length > 0) {
-            $panel = $content.wrapAll('<div class="panel-content" />')
-                .parent();
-        } else {
-            $panel = $('<div class="panel-content" />').insertAfter($ctrl);
+    var _ = {
+        name: "collapsible",
+        trigger: ".collapsible",
+        init: function($el, opts) {
+            // create collapsible structure
+            var $ctrl = $el.children(':first'),
+                $content = $el.children(':gt(0)'),
+                $panel;
+            if ($content.length > 0) {
+                $panel = $content.wrapAll('<div class="panel-content" />')
+                    .parent();
+            } else {
+                $panel = $('<div class="panel-content" />').insertAfter($ctrl);
+            }
+
+            // set initial state
+            if ((opts && opts.closed) || $el.hasClass("closed")) {
+                $el.removeClass("closed");
+                _.close($el, 0);
+            } else {
+                $el.addClass("open");
+                $el.trigger('patterns-collapsible-open');
+            }
+
+            // bind to click events
+            $ctrl.bind("click", function() { _.toggle($el, "fast"); });
+
+            return $el;
+        },
+        destroy: function($el) {
+        },
+        open: function($el, opts) {
+            opts = opts || {};
+            if ($el.hasClass("open")) return null;
+
+            toggle($el, opts.duration);
+
+            // allow for chaining
+            return $el;
+        },
+        close: function($el, opts) {
+            opts = opts || {};
+            if ($el.hasClass("closed")) return null;
+            toggle($el, opts.duration);
+
+            // allow for chaining
+            return $el;
+        },
+        toggle: function($el, opts) {
+            opts = opts || {};
+            var $panel = $el.find('.panel-content');
+            if ($el.hasClass("closed")) {
+                $el.trigger('patterns-collapsible-open');
+                _._transit($el, $panel, "closed", "open", opts.duration);
+            } else {
+                $el.trigger('patterns-collapsible-close');
+                _._transit($el, $panel, "open", "closed", opts.duration);
+            }
+
+            // allow for chaining
+            return $el;
+        },
+
+        _transit: function($el, $panel, from_cls, to_cls, duration) {
+            $el.removeClass(from_cls);
+            if (duration) $el.addClass("in-progress");
+            $panel.slideToggle(duration, function() {
+                if (duration) $el.removeClass("in-progress");
+                $el.addClass(to_cls);
+            });
         }
-
-        // set initial state
-        if ((opts && opts.closed) || $el.hasClass("closed")) {
-            $el.removeClass("closed");
-            close($el, 0);
-        } else {
-            $el.addClass("open");
-            $el.trigger('patterns-collapsible-open');
-        }
-
-        // bind to click events
-        $ctrl.bind("click", function() { toggle($el, "fast"); });
-
-        // allow for chaining
-        return $el;
     };
 
-    var open = function($el, duration) {
-        if ($el.hasClass("open")) return null;
-
-        toggle($el, duration);
-
-        // allow for chaining
-        return $el;
-    };
-
-    var close = function($el, duration) {
-        if ($el.hasClass("closed")) return null;
-        toggle($el, duration);
-
-        // allow for chaining
-        return $el;
-    };
-
-    var transit = function($el, $panel, from_cls, to_cls, duration) {
-        $el.removeClass(from_cls);
-        if (duration) $el.addClass("in-progress");
-        $panel.slideToggle(duration, function() {
-            if (duration) $el.removeClass("in-progress");
-            $el.addClass(to_cls);
-        });
-    };
-
-    var toggle = function($el, duration) {
-        var $panel = $el.find('.panel-content');
-        if ($el.hasClass("closed")) {
-            $el.trigger('patterns-collapsible-open');
-            transit($el, $panel, "closed", "open", duration);
-        } else {
-            $el.trigger('patterns-collapsible-close');
-            transit($el, $panel, "open", "closed", duration);
-        }
-
-        // allow for chaining
-        return $el;
-    };
-
-    var pattern = {
-        markup_trigger: ".collapsible",
-        initialised_class: "collapsible",
-        init: init,
-        open: open,
-        close: close,
-        toggle: toggle
-    };
-
-    return pattern;
+    return registry.register(_);
 });
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
 // vim: sw=4 expandtab
