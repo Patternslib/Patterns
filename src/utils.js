6d48eb6ef42508007b90beb7959e037ee69b0c60
Florian Friesdorf <flo@chaoflow.net>
2012-10-10 12:21:13 +0200

    start of registry, replacing patterns.js mechanisms

--- src/utils.js
+++ src/utils.js
@@ -61,75 +61,7 @@ define([
         return options;
     };
 
-    var log_init = function(name, method) {
-        var log_wrapper = function($el) {
-            var log = getLogger(name);
-            log.debug('Initialising:', $el);
-            var ret = method.apply(this, arguments);
-            if (ret === false) {
-                log.debug('skipped', $el);
-            } else {
-                log.debug('initialised:', $el);
-            }
-            return ret;
-        };
-        return log_wrapper;
-    };
-
-    var set_initialised_class = function(method, pattern) {
-        var cls = pattern.initialised_class;
-        if (!cls) return method;
-        var initialised_class_wrapper = function($el) {
-            var ret = method.apply(this, arguments);
-            if (ret !== false) $el.addClass(cls);
-            return ret;
-        };
-        return initialised_class_wrapper;
-    };
-
-    // run a method on an element only once
-    var once = function(key, method) {
-        var once_wrapper = function($el) {
-            var initialised = $el.data(key);
-            if (initialised) return undefined;
-            var ret = method.apply(this, arguments);
-            if (ret !== false) $el.data(key, true);
-            return ret;
-        };
-        return once_wrapper;
-    };
-
-    // work on elements single file
-    var turnstiled = function(method) {
-        var turnstile = function($el) {
-            var rest = Array.prototype.slice.call(arguments, 1);
-            $el.each(function() {
-                var $this = $(this);
-                return method.apply($this, [$this].concat(rest));
-            });
-        };
-        return turnstile;
-    };
-
-    // return a pimpéd pattern
-    // - init only once
-    // - put multiple elements through a turnstile
-    var pimp_pattern = function(pname, pattern) {
-        var pimped = {};
-        for (var mname in pattern) {
-            var method = pattern[mname];
-            if (mname === "init") {
-                method = log_init(pname, method);
-                method = set_initialised_class(method, pattern);
-                method = once(pname + '-' + mname, method);
-            }
-            if (typeof method === "function") method = turnstiled(method);
-            pimped[mname] = method;
-        }
-        return pimped;
-    };
-
-    var jquery_plugin = function(name, pattern) {
+    var jquery_plugin = function(pattern) {
         var plugin = function(method) {
             if (!method || typeof method === "object") {
                 pattern.init.apply(this, [this].concat(arguments));
@@ -139,7 +71,8 @@ define([
                     [this].concat(Array.prototype.slice.call(arguments, 1))
                 );
             } else {
-                $.error('Method ' + method + ' does not exist on jQuery.' + name);
+                $.error('Method ' + method +
+                        ' does not exist on jQuery.' + pattern.name);
             }
             return this;
         };
@@ -223,10 +156,6 @@ define([
         parseOptions: parseOptions,
         //load_modules: load_modules,
         // pattern pimping - own module?
-        set_initialised_class: set_initialised_class,
-        once: once,
-        turnstiled: turnstiled,
-        pimp_pattern: pimp_pattern,
         jquery_plugin: jquery_plugin,
         debounce: debounce,
 	renumber: renumber,
