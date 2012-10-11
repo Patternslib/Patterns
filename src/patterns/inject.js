cfb48e88c0c54c1e9b62ac011d925d7dcc352742
Florian Friesdorf <flo@chaoflow.net>
2012-10-11 23:42:08 +0200

    inject URI rewrite fix - uncomment as still broken

--- src/patterns/inject.js
+++ src/patterns/inject.js
@@ -196,19 +196,19 @@ define([
                         .replace(/<body(.*)>/gi, '<div id="__original_body">')
                         .replace(/<\/body(.*)>/gi,'</div>')
                 );
-                $data(":uri(is:relative)").each(function() {
-                    switch (this.tagName) {
-                        case "A":
-                            this.href=new URI(this.href).absoluteTo(url).toString();
-                            break;
-                        case "FORM":
-                            this.action=new URI(this.action).absoluteTo(url).toString();
-                            break;
-                        case "IMG":
-                            this.src=new URI(this.src).absoluteTo(url).toString();
-                            break;
-                    }
-                });
+                // $data.filter(":uri(is:relative)").each(function() {
+                //     switch (this.tagName) {
+                //         case "A":
+                //             this.href=new URI(this.href).absoluteTo(url).toString();
+                //             break;
+                //         case "FORM":
+                //             this.action=new URI(this.action).absoluteTo(url).toString();
+                //             break;
+                //         case "IMG":
+                //             this.src=new URI(this.src).absoluteTo(url).toString();
+                //             break;
+                //     }
+                // });
                 cfg.forEach(function(cfg) {
                     var $sources = $data.find(cfg.source);
 
