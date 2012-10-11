d0687f69a2d4928308f0a2020c1bc721fdd94127
Wichert Akkerman <wichert@wiggy.net>
2012-10-11 14:24:25 +0200

    Move modal tests to jasmine.

---
+++ tests/unit/modal.js
@@ -0,0 +1,64 @@
+describe("modal-pattern", function() {
+    var pattern;
+
+    requireDependencies(["patterns/modal"], function(cls) {
+        pattern = cls;
+    });
+
+    // Reset the lab before each test
+    beforeEach(function() {
+        $("#lab *").remove();
+    });
+
+    describe("init", function() {
+        it("Modal with single element", function() {
+            $("#lab").html([
+                '<div class="modal" id="modal">',
+                '  <p>Modal content</p>',
+                '</div>'
+                ].join('\n'));
+            var $modal = $("#modal");
+            pattern.init($modal);
+            expect($modal.find(".header").length).toBeTruthy();
+            expect($modal.find(".header").text()).toBe("Close");
+            expect($modal.find(".header .close-panel").length).toBeTruthy();
+            expect($modal.find(".body").length).toBeTruthy();
+            expect($modal.find(".body").text()).toBe("Modal content");
+        });
+
+        it("Modal with header ", function() {
+            $("#lab").html([
+                '<div class="modal" id="modal">',
+                '  <h3>Modal header</h3>',
+                '  <p>Modal content</p>',
+                '</div>'
+                ].join('\n'));
+            var $modal = $("#modal");
+            pattern.init($modal);
+            expect($modal.find(".header").text()).toBe("Modal headerClose");
+            expect($modal.find(".header .close-panel").length).toBeTruthy();
+            expect($modal.find(".body").text()).toBe("Modal content");
+        });
+
+        it("Modal with multiple content items ", function() {
+            $("#lab").html([
+                '<div class="modal" id="modal">',
+                '  <h3>Modal header</h3>',
+                '  <p>Modal content</p>',
+                '  <h4>Subheader</h4>',
+                '  <p>More content</p>',
+                '</div>'
+                ].join('\n'));
+            var $modal = $("#modal");
+            pattern.init($modal);
+            expect($modal.find(".header").text()).toBe("Modal headerClose");
+            expect($modal.find(".header .close-panel").length).toBeTruthy();
+            expect($modal.find(".body p").length).toBe(2);
+            expect($modal.find(".body h4").length).toBe(1);
+        });
+
+    });
+});
+
+// jshint indent: 4, browser: true, jquery: true, quotmark: double
+// vim: sw=4 expandtab
