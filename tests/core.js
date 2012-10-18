a362c11fa7b6c36b9c607ad3b7acc61efa4e99c6
Wichert Akkerman <wichert@wiggy.net>
2012-10-18 10:14:33 +0200

    Move transforms to core. Fixes #69
    And add test coverage, which found an interesting error in a jQuery
    selector that might have broken usage on IE<9.

---
+++ tests/core.js
@@ -0,0 +1,88 @@
+describe("core-init", function() {
+    var mapal;
+
+    requireDependencies(["core/init"], function(cls) {
+        mapal = cls;
+    });
+
+    // Reset the lab before each test
+    beforeEach(function() {
+        $("#lab *").remove();
+    });
+
+    describe("_convertToIframes", function() {
+        it("Keep non-html objects", function() {
+            var $lab = $("#lab");
+            $lab.append('<object type="text/plain">Plain content</object>');
+            mapal._convertToIframes($lab);
+            expect($lab.find("object").length).toBe(1);
+        });
+
+        it("Transform text/html objects", function() {
+            var $lab = $("#lab");
+            $lab.append('<object type="text/html" data="about.html">Plain content</object>');
+            mapal._convertToIframes($lab);
+            expect($lab.find("object").length).toBe(0);
+            var $iframe = $lab.find("iframe");
+            expect($iframe.length).toBe(1);
+            expect($iframe[0].src).toMatch(/about.html$/);
+        });
+    });
+
+    describe("transformContent", function() {
+        it("Convert legend to p.legend", function() {
+            var $lab = $("#lab");
+            $lab.append("<fieldset><legend>Fieldset title</legend></fieldset>");
+            mapal.transformContent($lab[0]);
+            expect($lab.html()).toBe('<fieldset><p class="legend">Fieldset title</p></fieldset>');
+        });
+
+        it("Keep text/html objects on non-IE", function() {
+            var $lab = $("#lab"),
+                old_msie = $.browser.msie;
+            spyOn(mapal, "_convertToIframes");
+            try {
+                old_msie=false;
+                mapal.transformContent($lab[0]);
+                expect(mapal._convertToIframes).not.toHaveBeenCaled();
+            } catch (e) {
+                $.browser.msie = old_msie;
+            }
+        });
+
+        it("Convert text/html objects to iframes on IE", function() {
+            var $lab = $("#lab"),
+                old_msie = $.browser.msie,
+                old_version = $.browser.version;
+            spyOn(mapal, "_convertToIframes");
+            try {
+                $.browser.msie=true;
+                $.browser.version="7.0";
+                mapal.transformContent($lab[0]);
+                expect(mapal._convertToIframes).toHaveBeenCaled();
+            } catch (e) {
+                $.browser.msie = old_msie;
+                $.browser.version = old_version;
+            }
+        });
+
+        it("Keep text/html objects IE > 8", function() {
+            var $lab = $("#lab"),
+                old_msie = $.browser.msie,
+                old_version = $.browser.version;
+            spyOn(mapal, "_convertToIframes");
+            try {
+                $.browser.msie=true;
+                $.browser.version="9.0";
+                mapal.transformContent($lab[0]);
+                expect(mapal._convertToIframes).not.toHaveBeenCaled();
+            } catch (e) {
+                $.browser.msie = old_msie;
+                $.browser.version = old_version;
+            }
+        });
+    });
+});
+
+// jshint indent: 4, browser: true, jquery: true, quotmark: double
+// vim: sw=4 expandtab
