4a60da8f5c0bde6782fc6d921008e9c1cdd301d6
Wichert Akkerman <wichert@wiggy.net>
2012-10-19 11:10:37 +0200

    Merge switch jquery plugin back into the pattern
    This is part of #82

--- tests/switch.js
+++ tests/switch.js
@@ -1,7 +1,7 @@
-describe("switch-plugin", function() {
+describe("switch-pattern", function() {
     var pattern;
 
-    requireDependencies(["jqplugins/switch"], function(cls) {
+    requireDependencies(["patterns/switch"], function(cls) {
         pattern = cls;
     });
 
@@ -10,31 +10,14 @@ describe("switch-plugin", function() {
         $("#lab *").remove();
     });
 
-    describe("parse", function() {
-        it("Shorthand notation", function() {
-            var $trigger = $("<button id='trigger' data-pat-switch='#victim; hide; show'>Click</button>"),
-                options = pattern.parse($trigger);
-            expect(options.length).toBe(1);
-            expect(options[0]).toEqual({selector: "#victim", remove: "hide", add: "show"});
-        });
-
-        it("Multiple changes", function() {
-            var $trigger = $("<button id='trigger' data-pat-switch='#victim; hide; show && #victim; hide; red'>Click</button>"),
-                options = pattern.parse($trigger);
-            expect(options.length).toBe(2);
-            expect(options[0]).toEqual({selector: "#victim", remove: "hide", add: "show"});
-            expect(options[1]).toEqual({selector: "#victim", remove: "hide", add: "red"});
-        });
-    });
-
-    describe("validateOptions", function() {
+    describe("_validateOptions", function() {
         it("Bad options", function() {
-            var options = pattern.validateOptions([{}]);
+            var options = pattern._validateOptions([{}]);
             expect(options).toBeFalsy();
         });
 
         it("Mix valid and invalid options", function() {
-            var options = pattern.validateOptions(
+            var options = pattern._validateOptions(
                 [{selector: "#victim", add: "purple"}, {}]);
             expect(options.length).toBe(1);
             expect(options[0]).toEqual({selector: "#victim", add: "purple"});
@@ -42,113 +25,114 @@ describe("switch-plugin", function() {
 
     });
 
-    describe("update", function() {
+    describe("_update", function() {
         it("No targets", function() {
             spyOn(jQuery.fn, "addClass");
             spyOn(jQuery.fn, "removeClass");
-            pattern.update(".missing");
+            pattern._update(".missing");
             expect(jQuery.fn.addClass).not.toHaveBeenCalled();
             expect(jQuery.fn.removeClass).not.toHaveBeenCalled();
         });
 
         it("Remove basic class", function() {
             $("#lab").html("<div class='on'/>");
-            pattern.update("#lab div", "on");
+            pattern._update("#lab div", "on");
             expect($("#lab div").hasClass("on")).toBe(false);
         });
 
         it("Keep other classes", function() {
             $("#lab").html("<div class='one two'/>");
-            pattern.update("#lab div", "one");
+            pattern._update("#lab div", "one");
             expect($("#lab div").attr("class")).toBe("two");
         });
 
         it("Remove uses whole words", function() {
             $("#lab").html("<div class='cheese-on-bread'/>");
-            pattern.update("#lab div", "on");
+            pattern._update("#lab div", "on");
             expect($("#lab div").attr("class")).toBe("cheese-on-bread");
         });
 
         it("Remove wildcard postfix class", function() {
             $("#lab").html("<div class='icon-small/>");
-            pattern.update("#lab div", "icon-*");
+            pattern._update("#lab div", "icon-*");
             expect($("#lab div").attr("class")).toBeFalsy();
         });
 
         it("Remove wildcard infix class", function() {
             $("#lab").html("<div class='icon-small-alert'/>");
-            pattern.update("#lab div", "icon-*-alert");
+            pattern._update("#lab div", "icon-*-alert");
             expect($("#lab div").attr("class")).toBeFalsy();
         });
 
         it("Keep other classes when removing wildcards", function() {
             $("#lab").html("<div class='icon-alert foo'/>");
-            pattern.update("#lab div", "icon-*");
+            pattern._update("#lab div", "icon-*");
             expect($("#lab div").attr("class")).toBe("foo");
         });
 
-
         it("Add class", function() {
             $("#lab").html("<div/>");
-            pattern.update("#lab div", null, "icon-alert");
+            pattern._update("#lab div", null, "icon-alert");
             expect($("#lab div").attr("class")).toBe("icon-alert");
         });
     });
 
-    describe("Initialise via jQuery", function() {
-        it("Specify defaults via API", function() {
-            $("#lab").html("<button>Click me</button>");
-            $("#lab button").patternSwitch({selector: "#victim", add: "pink"});
-            var $trigger = $("#lab button");
-            expect($trigger.data("patternSwitch")).toEqual([{selector: "#victim", add: "pink"}]);
+    describe("jQuery plugin usage", function() {
+        describe("Initialise via jQuery", function() {
+            it("Specify defaults via API", function() {
+                $("#lab").html("<button>Click me</button>");
+                $("#lab button").patternSwitch({selector: "#victim", add: "pink"});
+                var $trigger = $("#lab button");
+                expect($trigger.data("patternSwitch")).toEqual([{selector: "#victim", remove: null, add: "pink"}]);
+            });
+
+            it("Invalid defaults via API", function() {
+                $("#lab").html("<button>Click me</button>");
+                $("#lab button").patternSwitch({selector: "#victim"});
+                var $trigger = $("#lab button");
+                expect($trigger.data("patternSwitch")).toBeFalsy();
+            });
+
+            it("Parse defaults from DOM", function() {
+                $("#lab").html("<button data-pat-switch='#victim; foo ; bar'>Click me</button>");
+                $("#lab button").patternSwitch();
+                var $trigger = $("#lab button");
+                expect($trigger.data("patternSwitch")).toEqual([{selector: "#victim", remove:"foo", add: "bar"}]);
+            });
+
+            it("Setup click event handler", function() {
+                // Note that this relies on jQuery implementation details to check
+                // for registered event handlers.
+                $("#lab").html("<button data-pat-switch='#victim; foo'>Click me</button>");
+                var $trigger = $("#lab button").patternSwitch(),
+                     events = $._data($trigger[0]).events;
+                expect(events.click).toBeDefined();
+                expect(events.click[0].namespace).toBe("patternSwitch");
+            });
         });
 
-        it("Invalid defaults via API", function() {
-            $("#lab").html("<button>Click me</button>");
-            $("#lab button").patternSwitch({selector: "#victim"});
-            var $trigger = $("#lab button");
-            expect($trigger.data("patternSwitch")).toBeFalsy();
+        it("Execute changes", function() {
+            $("#lab")
+                .append("<button data-pat-switch='#victim; foo'>Click me</button>")
+                .append("<div id='victim' class='foo'/>");
+            var $trigger = $("#lab button").patternSwitch();
+            $trigger.patternSwitch("execute");
+            expect($("#victim").hasClass("foo")).toBeFalsy();
+
         });
 
-        it("Parse defaults from DOM", function() {
-            $("#lab").html("<button data-pat-switch='#victim; foo ; bar'>Click me</button>");
-            $("#lab button").patternSwitch();
-            var $trigger = $("#lab button");
-            expect($trigger.data("patternSwitch")).toEqual([{selector: "#victim", remove:"foo", add: "bar"}]);
-        });
-
-        it("Setup click event handler", function() {
-            // Note that this relies on jQuery implementation details to check
-            // for registered event handlers.
-            $("#lab").html("<button data-pat-switch='#victim; foo'>Click me</button>");
-            var $trigger = $("#lab button").patternSwitch(),
-                 events = $._data($trigger[0]).events;
-            expect(events.click).toBeDefined();
-            expect(events.click[0].namespace).toBe("patternSwitch");
-        });
-    });
-
-    it("Execute changes", function() {
-        $("#lab")
-            .append("<button data-pat-switch='#victim; foo'>Click me</button>")
-            .append("<div id='victim' class='foo'/>");
-        var $trigger = $("#lab button").patternSwitch();
-        $trigger.patternSwitch("execute");
-        expect($("#victim").hasClass("foo")).toBeFalsy();
-
-    });
-
-    describe("Destroy all hooks", function() {
-        it("Setup click event handler", function() {
-            // Note that this relies on jQuery implementation details to check
-            // for registered event handlers.
-            $("#lab").html("<button data-pat-switch='#victim; foo'>Click me</button>");
-            var $trigger = $("#lab button").patternSwitch(),
-                 events = $._data($trigger[0]).events;
-            expect(events.click).toBeDefined();
-            $trigger.patternSwitch("destroy");
-            expect(events.click).not.toBeDefined();
-            expect($trigger.data("patternSwitch")).toBeFalsy();
+        describe("Destroy all hooks", function() {
+            it("Setup click event handler", function() {
+                // Note that this relies on jQuery implementation details to check
+                // for registered event handlers.
+                $("#lab").html("<button data-pat-switch='#victim; foo'>Click me</button>");
+                var $trigger = $("#lab button").patternSwitch(),
+                     events = $._data($trigger[0]).events;
+                expect(events.click).toBeDefined();
+                $trigger.patternSwitch("destroy");
+                expect(events.click).not.toBeDefined();
+                expect($trigger.data("patternSwitch")).toBeFalsy();
+            });
         });
     });
 });
