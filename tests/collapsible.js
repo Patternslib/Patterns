b6c6859a4410db9cd660e23cdbef1d06c8a07df4
Wichert Akkerman <wichert@wiggy.net>
2012-10-19 15:38:01 +0200

    Add state storage to collapsible pattern

--- tests/collapsible.js
+++ tests/collapsible.js
@@ -14,12 +14,12 @@ describe("collapsible-pattern", function() {
         it("Create panel-content", function() {
             var $lab = $("#lab");
             $lab.html([
-                '<div class="collapsible">',
+                '<div class="pat-collapsible">',
                 '<h3>Trigger header</h3>',
                 '<p>Collapsible content</p>',
                 '</div>'
                 ].join('\n'));
-            var $collapsible = $("#lab .collapsible");
+            var $collapsible = $("#lab .pat-collapsible");
             pattern.init($collapsible);
             expect($collapsible.find(".panel-content").length).toBe(1);
         });
@@ -27,12 +27,12 @@ describe("collapsible-pattern", function() {
         it("Panels are open by default", function() {
             var $lab = $("#lab");
             $lab.html([
-                '<div class="collapsible">',
+                '<div class="pat-collapsible">',
                 '<h3>Trigger header</h3>',
                 '<p>Collapsible content</p>',
                 '</div>'
                 ].join('\n'));
-            var $collapsible = $("#lab .collapsible");
+            var $collapsible = $("#lab .pat-collapsible");
             pattern.init($collapsible);
             expect($collapsible.hasClass("open")).toBeTruthy();
         });
@@ -40,12 +40,12 @@ describe("collapsible-pattern", function() {
         it("Explicitly closed panel is not open", function() {
             var $lab = $("#lab");
             $lab.html([
-                '<div class="collapsible closed">',
+                '<div class="pat-collapsible closed">',
                 '<h3>Trigger header</h3>',
                 '<p>Collapsible content</p>',
                 '</div>'
                 ].join('\n'));
-            var $collapsible = $("#lab .collapsible");
+            var $collapsible = $("#lab .pat-collapsible");
             pattern.init($collapsible);
             expect($collapsible.hasClass("open")).toBeFalsy();
         });
@@ -55,29 +55,31 @@ describe("collapsible-pattern", function() {
         it("Toggle an open panel", function() {
             var $lab = $("#lab");
             $lab.html([
-                '<div class="collapsible">',
+                '<div class="pat-collapsible">',
                 '<h3>Trigger header</h3>',
                 '<p>Collapsible content</p>',
                 '</div>'
                 ].join('\n'));
-            var $collapsible = $("#lab .collapsible");
-            pattern.init($collapsible);
+            var $collapsible = $("#lab .pat-collapsible");
+            pattern.init($collapsible, {duration: 0});
             pattern.toggle($collapsible);
-            expect($collapsible.hasClass("open")).toBeFalsy();
+            expect($collapsible.hasClass("open")).toBe(false);
+            expect($collapsible.hasClass("closed")).toBe(true);
         });
 
         it("Toggle a closed panel", function() {
             var $lab = $("#lab");
             $lab.html([
-                '<div class="collapsible closed">',
+                '<div class="pat-collapsible closed">',
                 '<h3>Trigger header</h3>',
                 '<p>Collapsible content</p>',
                 '</div>'
                 ].join('\n'));
-            var $collapsible = $("#lab .collapsible");
-            pattern.init($collapsible);
+            var $collapsible = $("#lab .pat-collapsible");
+            pattern.init($collapsible, {duration: 0});
             pattern.toggle($collapsible);
-            expect($collapsible.hasClass("open")).toBeTruthy();
+            expect($collapsible.hasClass("open")).toBe(true);
+            expect($collapsible.hasClass("closed")).toBe(false);
         });
     });
 });
