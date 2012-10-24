dc50796addf2da93e634337540170512bebd1115
Florian Friesdorf <flo@chaoflow.net>
2012-10-24 06:06:34 +0200

    wip: simplify modal code

--- src/patterns/modal.js
+++ src/patterns/modal.js
@@ -13,66 +13,53 @@ define([
     var modal = {
         // div's are turned into modals
         // links and forms inject modals
-        trigger: ".pat-modal",
+        trigger: "div.pat-modal, a.pat-modal, form.pat-modal",
         init: function($el, opts) {
             return $el.each(function() {
                 var $el = $(this),
                     cfg = parser.parse($el, opts);
 
                 if ($el.is('div'))
-                    modal._init_div($el, cfg);
+                    modal._init_div1($el, cfg);
                 else
-                    modal._init_inject($el, cfg);
+                    modal._init_inject1($el, cfg);
             });
         },
-        _init_inject: function($el, cfg) {
+        _init_inject1: function($el, cfg) {
             var opts = {
                 target: '#modal',
                 "class": "pat-modal" + (cfg["class"] ? " " + cfg["class"] : "")
             };
+            $('#modal').detach();
             inject.init($el, opts);
         },
-        _init_div: function($el, cfg) {
-            var $first = $el.children(':first'),
-                $rest = $el.children(':not(:first)'),
-                $header = $('<div class="header" />'),
-                $body = $('<div class="body" />'),
-                $form = $el.find('form'),
-                $closebutton = $(
-                    '<button type="button" class="close-panel">Close</button>');
+        _init_div1: function($el, cfg) {
+            var $header = $('<div class="header" />'),
+                $button = $(
+                    '<button type="button" class="close-panel">Close</button>'
+                ).appendTo($header);
 
-            // separate into header and body
-            if ($rest.length === 0) {
-                $first.wrap($body);
-                $el.prepend($header);
-            } else {
-                $first.wrap($header);
-                $rest.wrapAll($body);
-            }
+            $el.children(':last, :not(:first)').wrapAll('<div class="body" />');
+            $('.body', $el).before($header);
+            $el.children(':first:not(.header)').prependTo($header);
 
-            // add close-panel button to header
-            $('.header', $el).append($closebutton);
-
-            // event handler to remove element -- make sure the form
-            // is submit and the response processed before calling this.
-            var remove = function(ev) {
-                if (ev) {
-                    ev.preventDefault();
-                }
-                $(document).off('.pat-modal');
-                $el.remove();
-            };
-
+            // event handlers remove modal - first arg to bind is ``this``
+            $(document).on('click.pat-modal', '.close-panel',
+                           modal.destroy.bind($el, $el));
             // remove on ESC
-            $(document).on('keyup.pat-modal', function(ev) {
-                if (ev.which == 27) remove(ev);
-            });
-            // remove on close-panel button click
-            $el.find('.close-panel').on('click.pat-modal', remove);
+            $(document).on('keyup.pat-modal',
+                           modal.destroy.bind($el, $el));
+        },
+        destroy: function($el, ev) {
+            if (ev && ev.type === "keyup" && ev.which !== 27)
+                return;
+            $(document).off('.pat-modal');
+            $el.remove();
         }
     };
     registry.register(modal);
     return modal;
 });
+
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
 // vim: sw=4 expandtab
