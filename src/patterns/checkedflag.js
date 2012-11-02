5c7c82de0f94968cb5304f7e596f3e793e839b7a
Wichert Akkerman <wichert@wiggy.net>
2012-11-02 11:39:09 +0100

    Checkedflag: show checked state for fieldset
    In addition to maintaining checked/unchecked on the label this change
    adds a similar class to the (topmost) containing fieldset as well.

--- src/patterns/checkedflag.js
+++ src/patterns/checkedflag.js
@@ -10,44 +10,58 @@ define([
 ], function($, patterns) {
     var checkedflag = {
         name: "checkedflag",
-        trigger: "input",
+        trigger: "input[type=checkbox],input[type=radio]",
 
         init: function($el) {
             $el
-                .filter("[type=checkbox]").each(checkedflag.onChangeCheckbox).end()
-                .filter("[type=radio]").each(checkedflag.onChangeRadio).end();
+                .filter("[type=checkbox]")
+                    .each(checkedflag.onChangeCheckbox)
+                    .on("change.patternCheckedflag", checkedflag.onChangeCheckbox)
+                    .end()
+                .filter("[type=radio]")
+                    .each(checkedflag.onChangeRadio)
+                    .on("change.patternCheckedflag", checkedflag.onChangeRadio)
+                    .end();
         },
 
-        onChangeCheckbox: function(e) {
+        onChangeCheckbox: function(event) {
             var $el = $(this),
-                $label = $el.closest("label");
+                $label = $el.closest("label"),
+                $fieldset = $el.closest("fieldset");
 
             if (this.checked) {
-                $label.removeClass("unchecked").addClass("checked");
+                $label.add($fieldset).removeClass("unchecked").addClass("checked");
             } else {
                 $label.addClass("unchecked").removeClass("checked");
+                if ($fieldset.find("input:checked").length)
+                    $fieldset.removeClass("unchecked").addClass("checked");
+                else
+                    $fieldset.addClass("unchecked").removeClass("checked");
             }
         },
 
-        onChangeRadio: function(e) {
+        onChangeRadio: function(event) {
             var $el = $(this),
                 $label = $el.closest("label"),
+                $fieldset = $el.closest("fieldset"),
                 selector = "label:has(input[name='" + this.name + "']:not(:checked))",
                 $siblings = (this.form===null) ? $(selector) : $(selector, this.form);
 
             $siblings.removeClass("checked").addClass("unchecked");
             if (this.checked) {
-                $label.removeClass("unchecked").addClass("checked");
+                $label.add($fieldset).removeClass("unchecked").addClass("checked");
             } else {
                 $label.addClass("unchecked").removeClass("checked");
+                if ($fieldset.find("input:checked").length)
+                    $fieldset.removeClass("unchecked").addClass("checked");
+                else
+                    $fieldset.addClass("unchecked").removeClass("checked");
             }
         }
     };
 
-    $(document)
-       .on("change", "input[type=checkbox]", checkedflag.onChangeCheckbox)
-       .on("change", "input[type=radio]", checkedflag.onChangeRadio);
     patterns.register(checkedflag);
+    return checkedflag;
 });
 
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
