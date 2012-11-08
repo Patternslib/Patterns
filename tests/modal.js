415ec4d5b3d6b1abb0bdc64bdea61c03fbe0ad25
Peter Lamut <peter.lamut@niteoweb.com>
2012-11-08 17:34:20 +0100

    Add provisional test for #111 fix

--- tests/modal.js
+++ tests/modal.js
@@ -1,22 +1,25 @@
-describe("modal-pattern", function() {
+/*global $, beforeEach, describe, expect, it, requireDependencies*/
+describe("modal-pattern", function () {
+    "use strict";
+
     var pattern;
 
-    requireDependencies(["patterns/modal"], function(cls) {
+    requireDependencies(["patterns/modal"], function (cls) {
         pattern = cls;
     });
 
     // Reset the lab before each test
-    beforeEach(function() {
+    beforeEach(function () {
         $("#lab *").remove();
     });
 
-    describe("init", function() {
-        it("Modal with single element", function() {
+    describe("init", function () {
+        it("Modal with single element", function () {
             $("#lab").html([
                 '<div class="pat-modal" id="modal">',
                 '  <p>Modal content</p>',
                 '</div>'
-                ].join('\n'));
+            ].join('\n'));
             var $modal = $("#modal");
             pattern.init($modal);
             expect($modal.find(".header").length).toBeTruthy();
@@ -26,13 +29,13 @@ describe("modal-pattern", function() {
             expect($modal.find(".panel-content").text()).toBe("Modal content");
         });
 
-        it("Modal with header ", function() {
+        it("Modal with header ", function () {
             $("#lab").html([
                 '<div class="pat-modal" id="modal">',
                 '  <h3>Modal header</h3>',
                 '  <p>Modal content</p>',
                 '</div>'
-                ].join('\n'));
+            ].join('\n'));
             var $modal = $("#modal");
             pattern.init($modal);
             expect($modal.find(".header").text()).toBe("Modal headerClose");
@@ -40,7 +43,7 @@ describe("modal-pattern", function() {
             expect($modal.find(".panel-content").text()).toBe("Modal content");
         });
 
-        it("Modal with multiple content items ", function() {
+        it("Modal with multiple content items ", function () {
             $("#lab").html([
                 '<div class="pat-modal" id="modal">',
                 '  <h3>Modal header</h3>',
@@ -48,7 +51,7 @@ describe("modal-pattern", function() {
                 '  <h4>Subheader</h4>',
                 '  <p>More content</p>',
                 '</div>'
-                ].join('\n'));
+            ].join('\n'));
             var $modal = $("#modal");
             pattern.init($modal);
             expect($modal.find(".header").text()).toBe("Modal headerClose");
@@ -57,6 +60,39 @@ describe("modal-pattern", function() {
             expect($modal.find(".panel-content h4").length).toBe(1);
         });
 
+        it("Modal with a form that has the pat-modal CSS class", function () {
+
+            var $modal,       // main modal container
+                $modalLink;   // link that triggers the modal
+
+            $("#lab").html([
+                '<a id="modalLink" class="pat-modal"',
+                '   href="../demo/bug_modal/modal-sources.html">Open a modal',
+                '   containing a form with pat-modal CSS class.</a>'
+            ].join('/n'));
+
+            $modalLink = $("#modalLink");
+            pattern.init($modalLink);
+
+            // najprej #modalLink ne obstaja
+            $modal = $("div#pat-modal");
+            expect($modal.length).toBe(0);
+
+            $modalLink.click();    // trigger panel loading
+
+            // TODO: we need to insert a delay here so that the asynchronous
+            // AJAX injection of the potentially problematic HTML finishes ...
+            // and only AFTER THAT we test that everything is still OK
+            // (read: that the modal still exists in DOM)
+            // NOTE: If AJAX request in inject.execute() is performed in a
+            // synchronous way, a delay is not need and this test detects a
+            // bug in modal._init_inject1()
+
+            // div#pat-modal should still exist even after the click has
+            // loaded and injected problematic HTML
+            $modal = $("div#pat-modal");
+            expect($modal.length).toBeGreaterThan(0);
+        });
     });
 });
 
