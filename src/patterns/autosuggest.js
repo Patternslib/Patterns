fbfc5d220cea4ed6ef891348d34e4072ab2a7041
Florian Friesdorf <flo@chaoflow.net>
2012-11-09 06:53:29 +0100

    add comment

--- src/patterns/autosuggest.js
+++ src/patterns/autosuggest.js
@@ -51,6 +51,9 @@ define([
                     ($el.parents('.pat-autosubmit2').length > 0);
             log.debug('autosubmit', autosubmit, $el);
 
+            // XXX: this feels hacky and is either needed for fast
+            // typers in which case it should be in autosubmit or to
+            // work around triggers in autoSuggest.
             var $form;
             if (autosubmit) {
                 $form = $el.parents('form');
