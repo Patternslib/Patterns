b8a5b2adb0f7f664d0a4b5d0724e8abfa616cafd
Florian Friesdorf <flo@chaoflow.net>
2012-11-09 12:58:49 +0100

    whitespace-cleanup

--- src/patterns/bumper.js
+++ src/patterns/bumper.js
@@ -3,42 +3,42 @@ define([
     "../core/parser",
     "../registry"
 ], function($, Parser, registry) {
-	var parser = new Parser("bumper");
-	
-	parser.add_argument("margin", 0);
-	
+    var parser = new Parser("bumper");
+
+    parser.add_argument("margin", 0);
+
     var _ = {
         name: "bumper",
         trigger: ".pat-bumper",
 
         init: function($el, options) {
-			// initialize the elements
-			$el.each(function() {
-				var $this = $(this),
-					opts = parser.parse($this, options),
-					top = $this.offset().top - parseFloat($this.css('marginTop').replace(/auto/, 0));
-				
-				$this.data('patterns.bumper', {'top': top, 'margin': opts.margin, 'threshold': top - opts.margin});
-			});
-			
-			$(window).scroll(function() {
-				_._testBump($el, $(this).scrollTop());
-			});
-			
-			_._testBump($el, $(window).scrollTop());
-		},
-		
-		_testBump: function($el, y) {
-			$el.each(function() {
-				var $this = $(this), top = $this.data('patterns.bumper').threshold;
-				if (y > top) {
-					$this.addClass('bumped');
-				} else {
-					$this.removeClass('bumped');
-				}
-			});
-		}
-	}
+            // initialize the elements
+            $el.each(function() {
+                var $this = $(this),
+                    opts = parser.parse($this, options),
+                    top = $this.offset().top - parseFloat($this.css('marginTop').replace(/auto/, 0));
+
+                $this.data('patterns.bumper', {'top': top, 'margin': opts.margin, 'threshold': top - opts.margin});
+            });
+
+            $(window).scroll(function() {
+                _._testBump($el, $(this).scrollTop());
+            });
+
+            _._testBump($el, $(window).scrollTop());
+        },
+
+        _testBump: function($el, y) {
+            $el.each(function() {
+                var $this = $(this), top = $this.data('patterns.bumper').threshold;
+                if (y > top) {
+                    $this.addClass('bumped');
+                } else {
+                    $this.removeClass('bumped');
+                }
+            });
+        }
+    }
     registry.register(_);
     return _;
 });
