1ef040bd114d12568877a2b47ffe0659b8ffed90
Peter Lamut <peter.lamut@niteoweb.com>
2012-11-12 13:47:08 +0100

    Use new autosubmit JS for the issue 145 demo

--- src/main.js
+++ src/main.js
@@ -59,7 +59,6 @@ define([
     'prefixfree',
     './patterns/autofocus',
     './patterns/autosubmit',
-    './patterns/autosubmit2',
     './patterns/autosuggest',
     './patterns/breadcrumbs',
     './patterns/bumper',
