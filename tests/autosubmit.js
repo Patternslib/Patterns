058076f6484076b0bed485f9273a531a2bc9126a
Florian Friesdorf <flo@chaoflow.net>
2012-11-13 09:02:12 +0100

    Revert "Make code in autosubmit test JSlint-compliant"
    This reverts commit 992441aecf4c722dc99986add94df2dd01d5eb0e.
    
    We are currently not using jslint. While this is an idea, it needs
    discussion before changing existing style.

--- tests/autosubmit.js
+++ tests/autosubmit.js
@@ -1,66 +1,63 @@
-/*global $, beforeEach, describe, expect, it, requireDependencies */
-describe("autosubmit-plugin", function () {
-    "use strict";
-
+describe("autosubmit-plugin", function() {
     var pattern;
 
-    requireDependencies(["patterns/autosubmit"], function (cls) {
+    requireDependencies(["patterns/autosubmit"], function(cls) {
         pattern = cls;
     });
 
     // Reset the lab before each test
-    beforeEach(function () {
+    beforeEach(function() {
         $("#lab *").remove();
     });
 
-    describe("parse", function () {
-        it("Shorthand notation", function () {
-            var $trigger = $("<input data-pat-autosubmit='500'/>"),
+    describe("parse", function() {
+        it("Shorthand notation", function() {
+            var $trigger = $("<input data-pat-autosubmit='500'/>");
                 options = pattern.parse($trigger);
             expect(options.delay).toBe("500");
         });
 
-        it("Multiple options not accepted", function () {
-            var $trigger = $("<input data-pat-autosubmit='500 && 400'/>"),
+        it("Multiple options not accepted", function() {
+            var $trigger = $("<input data-pat-autosubmit='500 && 400'/>");
                 options = pattern.parse($trigger);
             expect(options.delay).toBe("500");
         });
     });
 
-    describe("validateOptions", function () {
+    describe("validateOptions", function() {
         var defaultKeyupDelay = 400;  // as defined in patterns/autosubmit.js
 
-        it("Integer delay", function () {
+        it("Integer delay", function() {
             var options = pattern.validateOptions({delay: 500});
             expect(options.delay).toBe(500);
         });
 
-        it("String integer delay", function () {
+        it("String integer delay", function() {
             var options = pattern.validateOptions({delay: "500"});
             expect(options.delay).toBe(500);
         });
 
-        it("Delay keyword", function () {
+        it("Delay keyword", function() {
             var options = pattern.validateOptions({delay: "delay"});
             expect(options.delay).toBe(defaultKeyupDelay);
         });
 
-        it("Delay set to true", function () {
+        it("Delay set to true", function() {
             var options = pattern.validateOptions({delay: "true"});
             expect(options.delay).toBe(defaultKeyupDelay);
         });
 
-        it("Invalid syntax", function () {
+        it("Invalid syntax", function() {
             var options = pattern.validateOptions({delay: "invalid"});
             expect(options.delay).toBe(defaultKeyupDelay);
         });
 
-        it("Bad type", function () {
+        it("Bad type", function() {
             var options = pattern.validateOptions({delay: [500]});
             expect(options.delay).toBe(defaultKeyupDelay);
         });
 
-        it("Negative delay", function () {
+        it("Negative delay", function() {
             var options = pattern.validateOptions({delay: -500});
             expect(options.delay).toBe(defaultKeyupDelay);
         });
