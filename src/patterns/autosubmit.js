0d2fb6ed3354cf8be0e69b3162a8b01791b281fc
Florian Friesdorf <flo@chaoflow.net>
2012-11-13 04:18:53 +0100

    use autosubmit2 as base for autosubmit
    autosubmit introduced undesired behaviour and the code regarding
    type=search is clearly broken.

--- src/patterns/autosubmit.js
+++ src/patterns/autosubmit.js
@@ -1,124 +1,68 @@
 define([
     "jquery",
-    "../registry",
-    "../core/logger",
-    "../core/parser",
-    "../utils"
-], function($, patterns, logging, Parser, utils) {
+    '../registry',
+    '../core/logging',
+    '../utils'
+], function($, registry, logging, utils) {
+    var log = logging.getLogger('autosubmit');
 
-    //TODO: delete this comment
-    //by default the keyup submission needs to be debounced with 400ms (like before). - NOTE: DONE
-    //keyup can be enabled by default if there is a way to deactivate. - TODO not done yet
+    var _ = {
+        name: "autosubmit2",
+        trigger: ".pat-autosubmit2, .pat-autosubmit2-keyup",
+        init: function($el) {
+            return $el.each(function() {
+                var $el = $(this);
 
-    var DELAY_KEYUP_MS = 400,
-        log = logging.getLogger("autosubmit"),
-        parser = new Parser("autosubmit");
-    parser.add_argument("delay");
+                // submit if a (specific) form element changed
+                $el.on("change.pat-autosubmit2", _.submit);
 
-    var autosubmit = {
-        name: "autosubmit",
-        trigger: ".pat-autosubmit",
-
-        parse: function($trigger) {
-            var options = parser.parse($trigger);
-            if (Array.isArray(options)) {
-                log.error("autosubmit does not support multiple options");
-                options = options[0];
-            }
-            return options;
-        },
-
-        validateOptions: function(options) {
-            if (options.delay === null) {
-                options.delay = DELAY_KEYUP_MS;
-            } else if (typeof options.delay==="string") {
-                if (options.delay==="delay" || options.delay==="true")
-                    options.delay=DELAY_KEYUP_MS;
-                else {
-                    var number = parseInt(options.delay, 10);
-                    if (isNaN(number)) {
-                        log.error("Invalid delay value");
-                        options.delay = DELAY_KEYUP_MS;
-                    } else {
-                        options.delay=number;
-                    }
+                // debounced keyup submit, if enabled
+                if ($el.hasClass('pat-autosubmit2-keyup')) {
+                    ($el.is('input') ? $el : $el.find('input'))
+                        .on("keyup.pat-autosubmit2", utils.debounce(_.submit, 400));
                 }
-            } else if (typeof options.delay==="number") {
-                if (options.delay<0) {
-                    log.error("Timetravel machine broken - negative delay not possible.");
-                    options.delay = DELAY_KEYUP_MS;
-                }
-            } else if (options.delay) {
-                log.error("Invalid delay value");
-                options.delay = DELAY_KEYUP_MS;
-            }
 
-            return options;
+                // XXX: test whether on webkit and enable only if supported
+                ($el.is('input[type=search]') ? $el : $el.find('input[type=search]'))
+                    .on("click.pat-autosubmit2", _.submit);
+            });
         },
+        submit: function(ev) {
+            var $el = $(this),
+                $form = $el.is('form') ? $el : $el.parents('form').first();
 
-        onChange: function(event) {
-            var $trigger = $(this),
-                $form = this.tagName==="FORM" ? $trigger : $trigger.closest("form");
-
-            if ($trigger.hasClass("auto-suggest")) {
-                log.debug("Ignored event from autosuggest field.");
+            // ignore auto-suggest fields, the change event will be
+            // triggered on the hidden input
+            if ($el.is('.pat-autosuggest')) {
+                log.debug('ignored event from autosuggest field');
                 return;
             }
 
-            if ($trigger.is("input[type=search]")) {
+            if ($el.is('input[type=search]')) {
                 // clicking X on type=search deletes data attrs,
                 // therefore we store the old value on the form.
-                var name = $victim.attr('name'),
+                var name = $el.attr('name'),
                     key = name + '-autosubmit-oldvalue',
                     oldvalue = $form.data(key) || "",
-                    curvalue = $target[0].value || "";
+                    curvalue = $el[0].value || "";
 
-                if (!name)
+                if (!name) {
                     log.warn('type=search without name, will be a problem' +
-                             ' if there are multiple', $target);
-                if (oldvalue===curvalue)
-                    return;
+                             ' if there are multiple', $el);
+                }
+                if (oldvalue === curvalue) return;
+
                 $form.data(key, curvalue);
             }
 
-            log.info("triggered by " + event.type);
+            log.debug("triggered by " + ev.type);
+
             $form.submit();
-            event.stopPropagation();
-        },
-
-        init: function($root, defaults) {
-            defaults = defaults || {};
-            return $root
-                .find("input[type-search]").andSelf()
-                .each(function() {
-                var $trigger = $(this),
-                    options = $.extend({}, autosubmit.parse($trigger), defaults);
-                options=autosubmit.validateOptions(options);
-                if (!options)
-                    return;
-
-                var func = autosubmit.onChange;
-                if (options.delay)
-                    func=utils.debounce(func, options.delay);
-                $trigger
-                    .data("patternAutosubmit", options)
-                    .off(".patternAutosubmit")
-                    .on("change.patternAutosubmit", func)
-                    .on("keyup.patternAutosubmit", "input:not([type=file],[type=checkbox],[type=radio],[type=hidden],[type=image],[type=password],[type=submit])", func);
-            });
-        },
-
-        destroy: function($root) {
-            return $root
-                .find("input[type-search]").andSelf()
-                .each(function() {
-                          $(this).removeData("patternAutosubmit").off(".patternAutosubmit");
-                      });
         }
     };
 
-    patterns.register(autosubmit);
-    return autosubmit;
+    registry.register(_);
+    return _;
 });
 
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
