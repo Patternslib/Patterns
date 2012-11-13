395c2c0cfc78173d304dccd261704819e43d76cb
Florian Friesdorf <flo@chaoflow.net>
2012-11-13 05:07:09 +0100

    remove autosubmit tests concerned with option validation

--- tests/autosubmit.js
+++ tests/autosubmit.js
@@ -13,55 +13,9 @@ describe("autosubmit-plugin", function() {
     describe("parse", function() {
         it("Shorthand notation", function() {
             var $trigger = $("<input data-pat-autosubmit='500'/>");
-                options = pattern.parse($trigger);
-            expect(options.delay).toBe("500");
-        });
-
-        it("Multiple options not accepted", function() {
-            var $trigger = $("<input data-pat-autosubmit='500 && 400'/>");
-                options = pattern.parse($trigger);
-            expect(options.delay).toBe("500");
-        });
-    });
-
-    describe("validateOptions", function() {
-        var defaultKeyupDelay = 400;  // as defined in patterns/autosubmit.js
-
-        it("Integer delay", function() {
-            var options = pattern.validateOptions({delay: 500});
+                options = pattern.parser.parse($trigger);
             expect(options.delay).toBe(500);
         });
-
-        it("String integer delay", function() {
-            var options = pattern.validateOptions({delay: "500"});
-            expect(options.delay).toBe(500);
-        });
-
-        it("Delay keyword", function() {
-            var options = pattern.validateOptions({delay: "delay"});
-            expect(options.delay).toBe(defaultKeyupDelay);
-        });
-
-        it("Delay set to true", function() {
-            var options = pattern.validateOptions({delay: "true"});
-            expect(options.delay).toBe(defaultKeyupDelay);
-        });
-
-        it("Invalid syntax", function() {
-            var options = pattern.validateOptions({delay: "invalid"});
-            expect(options.delay).toBe(defaultKeyupDelay);
-        });
-
-        it("Bad type", function() {
-            var options = pattern.validateOptions({delay: [500]});
-            expect(options.delay).toBe(defaultKeyupDelay);
-        });
-
-        it("Negative delay", function() {
-            var options = pattern.validateOptions({delay: -500});
-            expect(options.delay).toBe(defaultKeyupDelay);
-        });
-
     });
 });
 
