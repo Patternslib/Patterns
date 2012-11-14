5ea893658fa7b758323eb405e813eef5b08cbc4d
Florian Friesdorf <flo@chaoflow.net>
2012-11-14 16:07:03 +0100

    add comment to startsWith/endsWith

--- src/jquery-ext.js
+++ src/jquery-ext.js
@@ -216,6 +216,9 @@ define(["jquery"], function($) {
     };
 
 
+    // XXX: In compat.js we include things for browser compatibility,
+    // but these two seem to be only convenience. Do we really want to
+    // include these as part of patterns?
     String.prototype.startsWith = function(str) { return (this.match("^"+str) !== null); };
     String.prototype.endsWith = function(str) { return (this.match(str+"$") !== null); };
 
