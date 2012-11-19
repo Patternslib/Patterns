fb131385b2dd134745076324941f351c64da2e03
Florian Friesdorf <flo@chaoflow.net>
2012-11-19 07:53:22 +0100

    rewrite form-state base on textchange event - getting there

--- src/patterns/form-state.js
+++ src/patterns/form-state.js
@@ -1,8 +1,11 @@
 define([
     "jquery",
+    "../core/logging",
     "../registry",
     '../utils'
-], function($, registry, utils) {
+], function($, logging, registry, utils) {
+    var log = logging.getLogger('form-state');
+
     var _ = {
         name: "form-state",
         trigger: "form.pat-form-state",
@@ -10,53 +13,51 @@ define([
             if ($form.length > 1)
                 return $form.each(function() { _.init($(this)); });
 
-            var $reset = $form.find('[type=reset]'),
-                $submit = $form.find('[type=submit]');
-
-            // initially disable submit and reset button, wait for
-            // something to be modified to enable them again
-            $reset.prop('disabled', true);
-            $submit.prop('disabled', true);
-
-            var modified = function() {
-                $reset.prop('disabled', false);
-                $submit.prop('disabled', false);
-                $form.removeClass("saved").addClass("modified")
-                    .off('.pat-form-state')
-                    .on('reset.pat-form-state', reset)
-                    .on('pat-ajax-success.pat-form-state', saved);
-            };
-            var debouncedModified = utils.debounce(modified, 400);
-
-            var reset = function() {
-                $reset.prop('disabled', true);
-                $submit.prop('disabled', true);
-                $form
-                    .removeClass("modified")
-                    .off('.pat-form-state')
-                    .on('keyup.pat-form-state', debouncedModified)
-                    .on('change.pat-form-state', modified);
-            };
-
-            var saved = function() {
-                reset();
-                var time = new Date(),
-                    timestr = time.getHours() + ':'
-                        + time.getMinutes() + ':'
-                        + time.getSeconds();
-                $form.find('time.last-saved').remove();
-                $form.prepend(
-                    '<time class="last-saved" datetime="' + timestr + '">' +
-                        timestr + '</time>'
-                );
-                $form.addClass("saved");
-            };
-
-            $form.off('.pat-form-state')
-                .on('keyup.pat-form-state', debouncedModified)
-                .on('change.pat-form-state', modified);
+            _.setReset.call($form);
 
             return $form;
+        },
+        setModified: function() {
+            var $form = $(this);
+
+            $form.find('[type=reset]').prop('disabled', false);
+            $form.find('[type=submit]').prop('disabled', false);
+
+            $form.removeClass("saved").addClass("modified")
+                .off('.pat-form-state')
+                .one('reset.pat-form-state', _.setReset)
+                .one('pat-ajax-success.pat-form-state', _.setSaved);
+            log.debug('modified');
+        },
+        setReset: function() {
+            var $form = $(this);
+
+            $form.find('[type=reset]').prop('disabled', true);
+            $form.find('[type=submit]').prop('disabled', true);
+
+            $form
+                .removeClass("modified")
+                .off('.pat-form-state')
+                .one('change.pat-form-state textchange.pat-form-state',
+                     _.setModified);
+            log.debug('reset');
+        },
+        setSaved: function() {
+            var $form = $(this);
+            _.setReset.call($form);
+
+            var time = new Date(),
+                timestr = time.getHours() + ':'
+                    + time.getMinutes() + ':'
+                    + time.getSeconds();
+            $form.find('time.last-saved').remove();
+            $form.prepend(
+                '<time class="last-saved" datetime="' + timestr + '">' +
+                    timestr + '</time>'
+            );
+
+            $form.addClass("saved");
+            log.debug('saved');
         }
     };
     registry.register(_);
