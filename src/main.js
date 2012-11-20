4dca75ac71227415ce5f70a39e97b96a7036f862
Humberto Sermeno <hsermeno@gmail.com>
2012-11-20 13:49:33 -0600

    New Pattern: Image Crop (issue #34)
    Added the Jcrop jquery plugin, the base pattern, documentation and demo.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ requirejs.config({
         jquery_ext: "./jquery-ext",
         jquery_form: "./lib/jquery.form/jquery.form",
         jquery_fullcalendar: "./3rdparty/fullcalendar/fullcalendar",
+        jquery_jcrop: './3rdparty/jCrop/js/jquery.Jcrop',
         jquery_placeholder: "./3rdparty/jquery.placeholder",
         jquery_textchange: "./3rdparty/jquery.textchange/jquery.textchange",
         jquery_validate: "./3rdparty/jquery-validation/jquery.validate",
@@ -34,6 +35,9 @@ requirejs.config({
             deps: ["jquery"]
         },
         jquery_fullcalendar: {
+            deps: ["jquery"]
+        },
+        jquery_jcrop: {
             deps: ["jquery"]
         },
         jquery_placeholder: {
@@ -78,6 +82,7 @@ define([
     "./patterns/focus",
     "./patterns/form-state",
     "./patterns/fullcalendar",
+    "./patterns/image-crop",
     "./patterns/inject",
     "./patterns/markdown",
     "./patterns/menu",
