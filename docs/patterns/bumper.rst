a29789913b7534160365e1e377a1e45c1e8248cb
Humberto Sermeno <hsermeno@gmail.com>
2012-11-20 07:51:18 -0600

    Added extra bumped classes (issue #107)
    Besides the bumped class, the elements are now assigned classes
    bumped-{top|left|right|bottom} depending on the edge where the element
    touched the viewport.

--- docs/patterns/bumper.rst
+++ docs/patterns/bumper.rst
@@ -1,8 +1,8 @@
 Bumper
 ======
 
-A *bumper* is an element that, when the user starts scrolling, stays at the top of 
-the viewport when it touches it.
+A *bumper* is an element that, when the user starts scrolling, stays in view when 
+a viewport border touches it.
 
 Below is a simple example.
 
@@ -12,16 +12,36 @@ Below is a simple example.
       Bumper content
    </div>
 
-When the user starts scrolling down the page and the top of the above DIV reaches
-the top of the viewport, it will be assigned a *bumped* class. The default class
+When the user starts scrolling the page and an edge of the above DIV reaches
+an edge of the viewport, a *bumped* class will be added. Additionally,
+it will be assigned a *bumped-{top|left|right|bottom}* class depending on 
+which edge of the element was touched by the viewport. The default classes
 should look like
 
 .. code-block:: css
 
    .bumped {
       position: fixed;
+   }
+   
+   .bumped-top {
       top: 0;
    }
+
+   .bumped-bottom {
+      bottom: 0;
+   }
+   
+   .bumped-left {
+      left: 0;
+   }
+   
+   .bumped-right {
+      right: 0;
+   }
+
+Please note that classes *bumped-left* and *bumped-right* will not be assigned
+at the same time. The same is true for *bumped-top* and *bumped-bottom*.
 
 Option Reference
 ----------------
@@ -32,7 +52,7 @@ The available options are:
 +------------------+------------+-----------------------------------------------+
 | Field            | default    | Description                                   |
 +==================+============+===============================================+
-| ``margin``       |   0        | The distance from the top edge of the element |
+| ``margin``       |   0        | The distance from the edge of the element     |
 |                  |            | from which the 'bumped' behavior will be      |
 |                  |            | activated                                     |
 +------------------+------------+-----------------------------------------------+
--- src/patterns/bumper.js
+++ src/patterns/bumper.js
@@ -11,27 +11,96 @@ define([
         name: "bumper",
         trigger: ".pat-bumper",
 
-        init: function($el, options) {
+        init: function($el, options) {            
             // initialize the elements
             $el.each(function() {
                 var $this = $(this),
                     opts = parser.parse($this, options),
-                    top = $this.offset().top - parseFloat($this.css('marginTop').replace(/auto/, 0));
-
-                $this.data('patterns.bumper', {'top': top, 'margin': opts.margin, 'threshold': top - opts.margin});
+                    data = _._getElementBox($this);
+                
+                data.threshold = {
+                    top:    data.top - opts.margin,
+                    bottom: data.bottom + opts.margin,
+                    left:   data.left - opts.margin,
+                    right:  data.right + opts.margin
+                };
+                data.margin = opts.margin;
+                
+                $this.data('patterns.bumper', data);
             });
 
             $(window).scroll(function() {
-                _._testBump($el, $(this).scrollTop());
+                _._testBump($el, _._getViewport());
             });
 
-            _._testBump($el, $(window).scrollTop());
+            _._testBump($el, _._getViewport());
+        },
+        
+        /**
+         * Calculates the bounding box for the current viewport
+         */
+        _getViewport: function() {
+            var $win = $(window), view = {
+                top: $win.scrollTop(),
+                left: $win.scrollLeft()
+            };
+            
+            view.right = view.left + $win.width();
+            view.bottom = view.top + $win.height();
+            
+            return view;
+        },
+        
+        /**
+         * Calculates the bounding box for a given element, taking margins
+         * into consideration
+         *
+         * @param $elem The element
+         */
+        _getElementBox: function($elem) {
+            var box = $elem.offset();
+            
+            box.top -= parseFloat($elem.css('marginTop').replace(/auto/, 0));
+            box.left -= parseFloat($elem.css('marginLeft').replace(/auto/, 0));
+            box.right = box.left + $elem.outerWidth(true);
+            box.bottom = box.top + $elem.outerHeight(true);
+            
+            return box;
         },
 
-        _testBump: function($el, y) {
+        /**
+         * Determines whether an element should be bumped
+         *
+         * @param $el  The element to look for
+         * @param view The bounding box in which the element will be bumped   
+         */
+        _testBump: function($el, box) {
             $el.each(function() {
-                var $this = $(this), top = $this.data('patterns.bumper').threshold;
-                if (y > top) {
+                var $this = $(this),
+                    data = $this.data('patterns.bumper'),
+                    bumped = false;
+
+                if (box.top > data.threshold.top) {
+                    $this.addClass('bumped-top');
+                    bumped = true;
+                } else if (box.bottom < data.threshold.bottom) {
+                    $this.addClass('bumped-bottom');
+                    bumped = true;
+                } else {
+                    $this.removeClass('bumped-top bumped-bottom');
+                }
+                
+                if (box.left > data.threshold.left) {
+                    $this.addClass('bumped-left');
+                    bumped = true;
+                } else if (box.right < data.threshold.right) {
+                    $this.addClass('bumped-right');
+                    bumped = true;
+                } else {
+                    $this.removeClass('bumped-left bumped-right');
+                }
+                
+                if (bumped) {
                     $this.addClass('bumped');
                 } else {
                     $this.removeClass('bumped');
