5a95c93ee5a88926291fff5cc58b2594f6348fba
Wichert Akkerman <wichert@wiggy.net>
2013-01-04 13:43:20 +0100

    Fix jshint errors.

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,6 +112,7 @@ module.exports = function(grunt) {
                     "require",
                     "define",
                     "module",
+                    "Markdown",
                     "Modernizr",
                     "tinyMCE"
                 ]
