273d57aa438e58444089d7470e5876c55f476f12
Wichert Akkerman <wichert@wiggy.net>
2013-01-04 12:09:39 +0100

    Remove unused utility functions.

--- src/utils.js
+++ src/utils.js
@@ -1,60 +1,6 @@
 define([
     "jquery"
 ], function($) {
-    var extractParameters = function(params, sources) {
-        var tmp,
-            j,
-            paramObjs = {};
-        if (params.length > 0) {
-            var p = params.slice(1).split("!");
-            for (var i = p.length-1; i >= 0; i--) {
-                // support injection parameters in other patterns
-                if (p[i][0] === "#") {
-                    var param, effect;
-
-                    if (p[i].indexOf(".") > 0) {
-                        tmp = p[i].split(".");
-                        param = tmp[0];
-                        effect = tmp[1];
-                    } else {
-                        param = p[i];
-                        effect = undefined;
-                    }
-                    var source = [sources.pop()];
-                    // XXX: $a and url were also not defined in the old context
-                    // We need automated tests
-                    mapal.injection.load($a, url, param.slice(1), source);
-                } else if (p[i].indexOf("=") > 0) {
-                    j = p[i].indexOf("=");
-                    paramObjs[p[i].slice(0, j)] = p[i].slice(j+1);
-                } else {
-                    paramObjs[p[i]] = true;
-                }
-            }
-        }
-        return paramObjs;
-    };
-
-    // input = "a!b=1 2!c=x"
-    // --> options = {a: true, b: "1 2", c: "x"}
-    var parseOptions = function(input) {
-        var params = input.split("!"),
-            options = {}, name, value, index;
-
-        for (var i=0; i<params.length; i++) {
-            index = params[i].indexOf("=");
-            if (index === -1) {
-                name = params[i];
-                value = true;
-            } else {
-                name = params[i].slice(0, index);
-                value = params[i].slice(index+1);
-            }
-            options[name] = value;
-        }
-        return options;
-    };
-
     var jquery_plugin = function(pattern) {
         var plugin = function(method) {
             var $this = this;
@@ -77,41 +23,6 @@ define([
         };
         return plugin;
     };
-
-    function _renumberAttribute(el, attr, i) {
-        var $el = $(el),
-            buf = $el.attr(attr);
-        if (buf) {
-            $el.attr(attr, buf.replace(/[0-9]+/, i));
-        }
-    }
-
-    function _renumberEl(i) {
-	_renumberAttribute(this, "for", i);
-	_renumberAttribute(this, "id", i);
-	_renumberAttribute(this, "name", i);
-    }
-
-    function renumber($container, selector) {
-        var $entries = $container.find(selector ? selector : "fieldset,tr,dd"),
-            entry, i;
-
-        for (i=0; i<$entries.length; i++) {
-            entry = $entries.get(i);
-            _renumberAttribute(entry, "id", i);
-            $("label, :input", entry).each(_renumberEl);
-        }
-    }
-
-
-    function hasContent($el) {
-        if ($el.is(":input")) {
-            return $el.val();
-        } else {
-            return $el.text().replace(/\s*/g, "") || $el.find("img,object,video,audio").length;
-        }
-    }
-
 
     //     Underscore.js 1.3.1
     //     (c) 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
@@ -138,14 +49,9 @@ define([
     };
 
     var utils = {
-        extractParameters: extractParameters,
-        parseOptions: parseOptions,
-        //load_modules: load_modules,
         // pattern pimping - own module?
         jquery_plugin: jquery_plugin,
-        debounce: debounce,
-	renumber: renumber,
-	hasContent: hasContent
+        debounce: debounce
     };
 
     return utils;
