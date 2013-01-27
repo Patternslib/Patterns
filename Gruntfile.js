d215ae4dda1e765ebbe8a32c4c65334f1bde4a71
Florian Friesdorf <flo@chaoflow.net>
2013-01-27 03:41:12 +0100

    grunt for now is our testrunner

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,5 +16,5 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks("grunt-contrib-jasmine");
     grunt.registerTask("test", ["jasmine"]);
+    grunt.registerTask("default", ["test"]);
 };
-
