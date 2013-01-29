6ef35c1a7965fc6e36510fee84ab47fbe9cf960c
Marko Durkovic <marko@miding.de>
2013-01-29 14:27:04 +0100

    Make all tests use require and move to specs dir.

---
+++ tests/specs/bumper.js
@@ -0,0 +1,112 @@
+define(["pat/bumper"], function(pattern) {
+
+    describe("bumper-pattern", function() {
+
+        beforeEach(function() {
+            $("<div/>", {id: "lab"}).appendTo(document.body);
+        });
+
+        afterEach(function() {
+            $("#lab").remove();
+        });
+
+        describe("init", function(){
+            it("Return jQuery object", function() {
+                var jq = jasmine.createSpyObj("jQuery", ["each"]);
+                jq.each.andReturn(jq);
+                expect(pattern.init(jq)).toBe(jq);
+            });
+
+            it("Element witout margin option", function(){
+                var $lab = $("#lab");
+                $lab.html([
+                    "<div class=\"pat-bumper\">",
+                    "<p>Hello World</p>",
+                    "</div>"
+                    ].join("\n"));
+                var $bumper = $("#lab .pat-bumper");
+                pattern.init($bumper);
+                expect($bumper.data("patterns.bumper").margin).toBe(0);
+            });
+
+            it("Element with margin option", function(){
+                var $lab = $("#lab");
+                $lab.html([
+                    "<div class=\"pat-bumper\" data-pat-bumper=\"margin: 100\">",
+                    "<p>Hello World</p>",
+                    "</div>"
+                    ].join("\n"));
+                var $bumper = $("#lab .pat-bumper");
+                pattern.init($bumper);
+                expect($bumper.data("patterns.bumper").margin).toBe(100);
+            });
+
+            it("Element with css margin", function(){
+                var $lab = $("#lab");
+                $lab.html([
+                    "<div class=\"pat-bumper\" style=\"margin-top: 30px;\">",
+                    "<p>Hello World</p>",
+                    "</div>"
+                    ].join("\n"));
+                var $bumper = $("#lab .pat-bumper");
+                pattern.init($bumper);
+                expect($bumper.data("patterns.bumper").threshold.top === $bumper.offset().top - 30).toBeTruthy();
+            });
+
+        });
+
+        describe("_testBump", function() {
+            it("Element bumped/not bumped", function() {
+                var $lab = $("#lab");
+                $lab.html([
+                    "<div class=\"pat-bumper\">",
+                    "<p>Hello World</p>",
+                    "</div>"
+                    ].join("\n"));
+
+                var $bumper = $("#lab .pat-bumper");
+                pattern.init($bumper);
+                var opts = $bumper.data("patterns.bumper");
+                var box = {
+                    top: opts.threshold.top - 10, // should not be bumped on top
+                    bottom: opts.threshold.bottom + 10, // not bumped on bottom
+                    left: opts.threshold.left - 10, // not bumped left
+                    right: opts.threshold.right + 10 // not bumped right
+                };
+
+                pattern._testBump($bumper, box);
+                expect($bumper.hasClass("bumped")).toBeFalsy();
+
+                // bump top
+                box.top += 20;
+                pattern._testBump($bumper, box);
+                expect($bumper.hasClass("bumped")).toBe(true);
+                expect($bumper.hasClass("bumped-top")).toBe(true);
+                expect($bumper.hasClass("bumped-bottom")).toBe(false);
+                expect($bumper.hasClass("bumped-left")).toBe(false);
+                expect($bumper.hasClass("bumped-right")).toBe(false);
+
+                // bump bottom
+                box.top -= 20;
+                box.bottom -= 20;
+                pattern._testBump($bumper, box);
+                expect($bumper.hasClass("bumped")).toBe(true);
+                expect($bumper.hasClass("bumped-top")).toBe(false);
+                expect($bumper.hasClass("bumped-bottom")).toBe(true);
+                expect($bumper.hasClass("bumped-left")).toBe(false);
+                expect($bumper.hasClass("bumped-right")).toBe(false);
+
+                // bump bottom and left
+                box.left += 20;
+                pattern._testBump($bumper, box);
+                expect($bumper.hasClass("bumped")).toBe(true);
+                expect($bumper.hasClass("bumped-top")).toBe(false);
+                expect($bumper.hasClass("bumped-bottom")).toBe(true);
+                expect($bumper.hasClass("bumped-left")).toBe(true);
+                expect($bumper.hasClass("bumped-right")).toBe(false);
+
+            });
+        });
+    });
+
+});
---
+++ tests/specs/carousel.js
@@ -0,0 +1,70 @@
+define(["pat/carousel"], function(pattern) {
+
+    describe("carousel-plugin", function() {
+
+        beforeEach(function() {
+            $("<div/>", {id: "lab"}).appendTo(document.body);
+        });
+
+        afterEach(function() {
+            $("#lab").remove();
+        });
+
+        describe("init", function() {
+            it("Default options", function() {
+                $("#lab").html(
+                    "<ul class='pat-carousel'>" +
+                    "  <li>Panel 1</li>" +
+                    "  <li>Panel 2</li>" +
+                    "</ul>");
+                var $carousel = $("#lab ul");
+                spyOn($.fn, "anythingSlider").andCallThrough();
+                pattern.init($carousel);
+                expect($.fn.anythingSlider).toHaveBeenCalled();
+                var options = $.fn.anythingSlider.calls[0].args[0];
+                expect(options.autoPlay).toBe(false);
+                expect(options.stopAtEnd).toBe(false);
+                expect(options.resizeContents).toBe(false);
+                expect(options.expand).toBe(false);
+                expect(options.buildArrows).toBe(true);
+                expect(options.expand).toBe(false);
+            });
+
+            it("Default options (DOM test)", function() {
+                $("#lab").html(
+                    "<ul class='pat-carousel'>" +
+                    "  <li>Panel 1</li>" +
+                    "  <li>Panel 2</li>" +
+                    "</ul>");
+                var $carousel = $("#lab ul");
+                pattern.init($carousel);
+                // No arrows created
+                expect($carousel.find(".arrow").length).toBe(0);
+                // No start-stop button created
+                expect($carousel.find(".start-stop").length).toBe(0);
+                // No navigation boxes
+                expect($carousel.find(".anythingNavWindow").length).toBe(0);
+            });
+
+            it("Tweak options via DOM", function() {
+                $("#lab").html(
+                    "<ul class='pat-carousel' data-pat-carousel='auto-play: false; loop: false; time-delay: 50'>" +
+                    "  <li>Panel 1</li>" +
+                    "  <li>Panel 2</li>" +
+                    "</ul>");
+                var $carousel = $("#lab ul");
+                spyOn($.fn, "anythingSlider").andCallThrough();
+                pattern.init($carousel);
+                expect($.fn.anythingSlider).toHaveBeenCalled();
+                var options = $.fn.anythingSlider.calls[0].args[0];
+                expect(options.autoPlay).toBe(false);
+                expect(options.stopAtEnd).toBe(true);
+                expect(options.delay).toBe(50);
+            });
+        });
+    });
+
+});
+
+// jshint indent: 4, browser: true, jquery: true, quotmark: double
+// vim: sw=4 expandtab
