8138541eaac149ab6574dc5294029213d41e2cd8
Florian Friesdorf <flo@chaoflow.net>
2013-02-05 21:15:01 +0100

    pat/ajax now handles anchors and forms, lib/ajax is gone
    - patajax.request replaces old lib functionality
    - saveState moved to form-state
    - added tests for pat/ajax

---
+++ tests/pat/ajax.js
@@ -0,0 +1,61 @@
+define(['pat/ajax'], function(pattern) {
+    var $lab;
+
+    describe('pat/ajax', function() {
+
+        beforeEach(function() {
+            $lab = $('<div/>', {id: 'lab'}).appendTo(document.body);
+        });
+
+        afterEach(function() {
+            $('#lab').remove();
+        });
+
+        describe('anchor', function() {
+            it('triggers ajax request on click', function() {
+                var $a = $('<a href="href.html" />').appendTo($lab);
+                pattern.init($a);
+                spyOn($, 'ajax');
+                $a.click();
+                expect($.ajax.calls[0].args[0].context[0]).toBe($a[0]);
+                expect($.ajax.calls[0].args[0].url).toBe('href.html');
+            });
+        });
+        //
+        // ATTENTION: These might fail on IE8 due to different code
+        // path in jquery.form that does not use $.ajax
+        //
+        describe('form', function() {
+            var $form, $button;
+            beforeEach(function() {
+                $form = $('<form action="action.html"></form>').appendTo($lab);
+                $button = $(
+                    '<button type="submit" name="submit" value="submit" />'
+                ).appendTo($form);
+                $('<input name="input1" value="value1" />').appendTo($form);
+                pattern.init($form);
+                spyOn($, 'ajax');
+            });
+            it('triggers ajax request on submit', function() {
+                $form.submit();
+                expect($.ajax).toHaveBeenCalled();
+            });
+            it('honors method="post"', function() {
+                $form.attr('method', 'post');
+                $form.submit();
+                expect($.ajax.calls[0].args[0].url).toEqual('action.html');
+                expect($.ajax.calls[0].args[0].data).toEqual('input1=value1');
+            });
+            it('triggers ajax request on click submit', function() {
+                $button.click();
+                expect($.ajax).toHaveBeenCalled();
+            });
+            it('does not include submit buttons if not clicked', function() {
+                $form.submit();
+                expect($.ajax.calls[0].args[0].url)
+                    .toEqual('action.html?input1=value1');
+            });
+        });
+    });
+
+});
