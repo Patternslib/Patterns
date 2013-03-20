013d0a4d7dc52fda6e2672e33e3b85969e0ea067
Marko Durkovic <marko@miding.de>
2013-03-20 15:09:01 +0100

    Whitespace cleanup.

--- src/pat/edit-tinymce.js
+++ src/pat/edit-tinymce.js
@@ -96,11 +96,11 @@ define([
                         handler();
                     }
                 };
-                
+
                 var clickHandler = function() {
                     $el.trigger("click");
                 };
-                
+
                 ed.onKeyUp.add(handler);
                 ed.onChange.add(handler);
                 ed.onUndo.add(handler);
