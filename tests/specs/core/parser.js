Patternslib/Patterns

commit 0723913c110c81916f4b09adc118e73fdd9f3243

parser: support escaping ; by doubling it to ;;

src/core/parser.js
@@ -168,8 +168,18 @@ define([
 
         _parseExtendedNotation: function(parameter) {
             var opts = {}, i,
-                parts = parameter.split(";"),
-                matches;
+                matches,
+                parts = parameter.split(";").reduce(function(acc, x) {
+                    var prev = acc[acc.length - 1];
+                    if (x === '') {
+                        acc[acc.length - 1] = prev + ';';
+                    } else if (prev && prev[prev.length - 1] === ';') {
+                        acc[acc.length - 1] = prev + x;
+                    } else {
+                        acc = acc.concat(x);
+                    }
+                    return acc;
+                }, []);
 
             for (i=0; i<parts.length; i++) {
                 if (!parts[i])

tests/core/parser.js
@@ -146,6 +146,16 @@ define(["core/parser"], function(ArgumentParser) {
                     expect(opts).toEqual({foo: "one", bar: "two"});
                 });
 
+                it("Escaping a semicolon by duplication", function() {
+                    var parser=new ArgumentParser();
+                    parser.add_argument("foo");
+                    parser.add_argument("bar");
+                    parser.add_argument("baz");
+                    var opts = parser._parse("foo: one;; bar: still one;baz: three");
+                    expect(opts).toEqual({foo: "one; bar: still one",
+                                          baz: "three"});
+                });
+
                 it("Ignore unknown named parameter", function() {
                     var parser=new ArgumentParser();
                     parser.add_argument("selector");

commit 2e19e2447c8164bdaf24fd657e8f7b18a502bfa1

Add a test proving #295 is indeed caused by 0723913c110c81916f4b09adc118e73fdd9f3243

tests/specs/core/parser.js
@@ -146,6 +146,13 @@ define(["core/parser"], function(ArgumentParser) {
                     expect(opts).toEqual({foo: "one", bar: "two"});
                 });
 
+                it("Trailing semicolon", function() {
+                    var parser=new ArgumentParser();
+                    parser.add_argument("foo");
+                    var opts = parser._parse("foo: bar;");
+                    expect(opts).toEqual({foo: "bar"});
+                });
+
                 it("Escaping a semicolon by duplication", function() {
                     var parser=new ArgumentParser();
                     parser.add_argument("foo");