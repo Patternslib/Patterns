12c179943a95c2a05a4cd9dde7d0c3ea9e3c4127
Wichert Akkerman <wichert@wiggy.net>
2013-03-29 11:10:57 +0100

    Add skeleton for gallery pattern.

---
+++ tests/specs/pat/gallery.js
@@ -0,0 +1,19 @@
+define(["pat/gallery"], function(pattern) {
+    describe("Gallery pattern", function() {
+        beforeEach(function() {
+            $("<div/>", {id: "lab"}).appendTo(document.body);
+        });
+
+        afterEach(function() {
+            $("#lab").remove();
+        });
+
+        describe("init", function() {
+            it("Return jQuery wrapper", function() {
+                var jq = jasmine.createSpyObj("jQuery", ["each"]);
+                jq.each.andReturn(jq);
+                expect(pattern.init(jq)).toBe(jq);
+            });
+        });
+    });
+});
