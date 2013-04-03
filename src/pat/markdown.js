5276c638bb4cbfc2050f13eae8c1462ae95811c1
Wichert Akkerman <wichert@wiggy.net>
2013-04-03 16:56:02 +0200

    Attempt at rendering markdown header content.

--- src/pat/markdown.js
+++ src/pat/markdown.js
@@ -16,7 +16,13 @@ define([
     var _ = {
         name: "markdown",
         trigger: ".pat-markdown",
-        url_re: /\.md$/,
+
+        _url_re: /\.md$/,
+        _header_tags: new RegExp(['^<\\/?(a|abbr|acronym|b|bdo|big|button|',
+                    'cite|code|del|dfn|em|i|img|input|ins|kbd|label|map|mark|',
+                    'meter|progress|q|ruby|rp|rt|s|samp|select|small|span|',
+                    ',strike|strong|sub|sup|time|tt|u|var|wbr)[^>]*>$']
+            .join(''), 'i'),
 
         init: function($el) {
             return $el.each(function() {
@@ -27,38 +33,50 @@ define([
             });
         },
 
-        _renderHtml5Headers: function(text, runBlockGamut) {
-                text = text.replace(/^(.+)?\s*\n=+\s*\n+((?:.|\n)*?(?=^.*?\n=+\s*$)|(?:.|\n)*)/gm,
-                    function (wholeMatch, m1, m2) {
-                        return "<section>\n" +
-                               "  <h1>" + (m1) + "</h1>\n" +
-                               runBlockGamut(m2) + "\n" +
-                               "</section>\n"; }
-                );
-
-                text = text.replace(/^(.+)?\s*\n-+\s*\n+((?:.|\n)*?(?=^.*?\n-+\s*$)|(?:.|\n)*)/gm,
-                    function (wholeMatch, m1, m2) {
-                        return "<section>\n" +
-                               "  <h1>" + (m1) + "</h1>\n" +
-                               runBlockGamut(m2) + "\n" +
-                               "</section>\n"; }
-                );
-
-                var pattern = "^#{@LEVEL@}\\s*(.+?)\\s*$\\n+((?:.|\\n)*?(?=^#{1,@LEVEL@}\\s)|.*(?:.|\\n)*)",
-                    replacer = (function(wholeMatch, m1, m2) {
-                        return "<section>\n" +
-                               "  <h1>" + (m1) + "</h1>\n" +
-                               runBlockGamut(m2) + "\n" +
-                               "</section>\n";
-                });
-                for (var level=6; level>0; level--) {
-                    var matcher = new RegExp(pattern.replace(/@LEVEL@/g, level), "gm");
-                    text = text.replace(matcher, replacer);
-                }
-                return text;
+        _renderHeader: function(converter, text) {
+            // This is essentially a copy of pagedown-extra's convertSpans
+            text = text.replace(/~D/g, "$$");
+            text = text.replace(/~T/g, "~");
+            var html = converter.makeHtml(text);
+            return html.replace(/<[^>]*>?/gi, function(tag) {
+                return tag.match(_._header_tags) ? tag : '';
+            });
         },
 
-	_makeConverter: function() {
+        _renderHtml5Headers: function(text, runBlockGamut) {
+            var span_converter = Markdown.getSanitizingConverter();
+
+            text = text.replace(/^(.+)?\s*\n=+\s*\n+((?:.|\n)*?(?=^.*?\n=+\s*$)|(?:.|\n)*)/gm,
+                function (wholeMatch, m1, m2) {
+                    return "<section>\n" +
+                           "  <h1>" + _._renderHeader(span_converter, m1) + "</h1>\n" +
+                           runBlockGamut(m2) + "\n" +
+                           "</section>\n"; }
+            );
+
+            text = text.replace(/^(.+)?\s*\n-+\s*\n+((?:.|\n)*?(?=^.*?\n-+\s*$)|(?:.|\n)*)/gm,
+                function (wholeMatch, m1, m2) {
+                    return "<section>\n" +
+                           "  <h1>" + _._renderHeader(span_converter, m1) + "</h1>\n" +
+                           runBlockGamut(m2) + "\n" +
+                           "</section>\n"; }
+            );
+
+            var pattern = "^#{@LEVEL@}\\s*(.+?)\\s*$\\n+((?:.|\\n)*?(?=^#{1,@LEVEL@}\\s)|.*(?:.|\\n)*)",
+                replacer = (function(wholeMatch, m1, m2) {
+                    return "<section>\n" +
+                           "  <h1>" + _._renderHeader(span_converter, m1) + "</h1>\n" +
+                           runBlockGamut(m2) + "\n" +
+                           "</section>\n";
+            });
+            for (var level=6; level>0; level--) {
+                var matcher = new RegExp(pattern.replace(/@LEVEL@/g, level), "gm");
+                text = text.replace(matcher, replacer);
+            }
+            return text;
+        },
+
+        _makeConverter: function() {
             // A convertor can not be used in multiple threads at the same
             // time, so create a new one for every rendering.
             var converter = Markdown.getSanitizingConverter();
@@ -79,7 +97,7 @@ define([
     $(document.body).on("patterns-inject-triggered.pat-markdown", "a.pat-inject", function() {
         var $this = $(this), cfgs = $this.data("patterns.inject");
 
-        if (_.url_re.test(cfgs[0].url)) {
+        if (_._url_re.test(cfgs[0].url)) {
             cfgs.forEach(function(cfg) {
                 cfg.dataType = "markdown";
             });
