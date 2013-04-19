dee5bcb3bd13822845fbc45faffaf2aa6df27c85
Wichert Akkerman <wichert@wiggy.net>
2013-04-19 22:50:28 +0200

    Start of parsley integration.

--- src/pat/validate.js
+++ src/pat/validate.js
@@ -5,14 +5,59 @@
  */
 define([
     "jquery",
-    "../registry"
-], function($, patterns) {
+    "../registry",
+    "../utils",
+    "parsley"
+], function($, patterns, utils) {
     var validate = {
         name: "validate",
-        trigger: ".pat-validate",
+        trigger: "form.pat-validate",
 
         init: function($el) {
-            return $el;
+            return $el.each(function() {
+                this.noValidate=true;
+                var parsley_form, field, i;
+                
+                parsley_form=$(this).parsley({
+                    successClass: "valid",
+                    errorClass: "warning",
+                    errors: {
+                        classHandler: validate._classHandler,
+                        container: validate._container,
+                    }
+                });
+                for (i=0; i<parsley_form.items.length; i++) {
+                    field=parsley_form.items[i];
+                    field.addError=validate._addFieldError;
+                    field.removeError=validate._removeFieldError;
+                }
+            });
+        },
+
+        // Parsley error class handler, used to determine which element will
+        // receive the status class.
+        _classHandler: function(elem, isRadioOrCheckbox) {
+            return $(elem).add(elem.map(function(idx, el) { return utils.findLabel(el); }));
+        },
+
+        // Parsley hook to determine where error messages are inserted.
+        _container: function(element, isRadioOrCheckbox) {
+            return $();
+        },
+
+        // Parsley method to add an error to a field
+        _addFieldError: function(error) {
+            for (var constraint in error) {
+                var $message = $("<em/>", {"class": "warning message"});
+                $message.attr("data-validate-constraint", constraint);
+                $message.text(error[constraint]);
+                $message.insertAfter(this.element);
+            }
+        },
+
+        // Parsley method to remove all error messages for a field
+        _removeFieldError: function(constraintName) {
+            this.element.siblings("em.warning.message[data-validate-constraint="+constraintName+"]").remove();
         }
     };
 
