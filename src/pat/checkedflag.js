a0252819de01777944b4048519478d0472ea4395
Florian Friesdorf <flo@chaoflow.net>
2013-05-30 14:15:38 +0200

    checkedflag cleanup, prepare merge of select-option
    They are both doing something very similar: maintaining the state of a
    control on a parent element label/span.
    
    I need $(...).patCheckedflag('toggle') and ('set', true/false/value).

--- src/pat/checkedflag.js
+++ src/pat/checkedflag.js
@@ -1,42 +1,45 @@
 /**
- * Patterns checkedflag - Add checked flag to checkbox labels
+ * Patterns checkedflag - Add checked flag to checkbox labels and API
+ * for (un)checking.
  *
  * Copyright 2012-2013 Simplon B.V. - Simplon B.V. - Wichert Akkerman
  * Copyright 2012 JC Brand
- * Copyright 2012 Florian Friesdorf
+ * Copyright 2012-2013 Florian Friesdorf
  */
 define([
     "jquery",
     "../registry"
 ], function($, patterns) {
-    var checkedflag = {
+    var _ = {
         name: "checkedflag",
         trigger: "input[type=checkbox],input[type=radio]",
+        jquery_plugin: true,
 
         init: function($el) {
             var $forms = $();
-            $el
-                .each(function() {
-                    if (this.form===null)
-                        return;
-                    var $form = $(this.form);
-                    if ($form.data("patternCheckedflag.reset"))
-                        return;
-                    $form.data("patternCheckedflag.reset", true);
-                    $forms = $forms.add(this.form);
-                })
-                .filter("[type=checkbox]")
-                    .each(checkedflag.onChangeCheckbox)
-                    .on("change.patternCheckedflag", checkedflag.onChangeCheckbox)
-                    .end()
-                .filter("[type=radio]")
-                    .each(checkedflag.onChangeRadio)
-                    .on("change.patternCheckedflag", checkedflag.onChangeRadio)
-                    .end()
-                .filter("input:disabled").each(function() {
-                    $(this).closest("label").addClass('disabled');
-                }).end();
-            $forms.on("reset.patternCheckedflag", checkedflag.onFormReset);
+            $el.each(function() {
+                if (this.form === null)
+                    return;
+                var $form = $(this.form);
+                if ($form.data("pat-checkedflag.reset"))
+                    return;
+                $form.data("pat-checkedflag.reset", true);
+                $forms = $forms.add(this.form);
+            });
+
+            $el.filter("[type=checkbox]")
+                .each(_.onChangeCheckbox)
+                .on("change.pat-checkedflag", _.onChangeCheckbox);
+
+            $el.filter("[type=radio]")
+                .each(_.onChangeRadio)
+                .on("change.pat-checkedflag", _.onChangeRadio);
+
+            $el.filter("input:disabled").each(function() {
+                $(this).closest("label").addClass('disabled');
+            });
+
+            $forms.on("reset.pat-checkedflag", _.onFormReset);
         },
 
         onFormReset: function() {
@@ -45,8 +48,8 @@ define([
             // to fix this.
             var form = this;
             setTimeout(function() {
-                $("input[type=checkbox]", form).each(checkedflag.onChangeCheckbox);
-                $("input[type=radio]", form).each(checkedflag.onChangeRadio);
+                $("input[type=checkbox]", form).each(_.onChangeCheckbox);
+                $("input[type=radio]", form).each(_.onChangeRadio);
             }, 50);
         },
 
@@ -56,13 +59,15 @@ define([
                 $fieldset = $el.closest("fieldset");
 
             if (this.checked) {
-                $label.add($fieldset).removeClass("unchecked").addClass("checked");
+                $label.add($fieldset)
+                    .removeClass("unchecked").addClass("checked");
             } else {
                 $label.addClass("unchecked").removeClass("checked");
-                if ($fieldset.find("input:checked").length)
+                if ($fieldset.find("input:checked").length) {
                     $fieldset.removeClass("unchecked").addClass("checked");
-                else
+                } else{
                     $fieldset.addClass("unchecked").removeClass("checked");
+                }
             }
         },
 
@@ -70,24 +75,29 @@ define([
             var $el = $(this),
                 $label = $el.closest("label"),
                 $fieldset = $el.closest("fieldset"),
-                selector = "label:has(input[name='" + this.name + "']:not(:checked))",
-                $siblings = (this.form===null) ? $(selector) : $(selector, this.form);
+                selector = 'label' +
+                    ':has(input[name="' + this.name + '"]' +
+                    ':not(:checked))',
+                $siblings = (this.form === null) ?
+                    $(selector) : $(selector, this.form);
 
             $siblings.removeClass("checked").addClass("unchecked");
             if (this.checked) {
-                $label.add($fieldset).removeClass("unchecked").addClass("checked");
+                $label.add($fieldset)
+                    .removeClass("unchecked").addClass("checked");
             } else {
                 $label.addClass("unchecked").removeClass("checked");
-                if ($fieldset.find("input:checked").length)
+                if ($fieldset.find("input:checked").length) {
                     $fieldset.removeClass("unchecked").addClass("checked");
-                else
+                } else {
                     $fieldset.addClass("unchecked").removeClass("checked");
+                }
             }
         }
     };
 
-    patterns.register(checkedflag);
-    return checkedflag;
+    patterns.register(_);
+    return _;
 });
 
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
