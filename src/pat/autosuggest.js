295a6d2fc7c2526954707cbb7a52d66a235e39ef
Marko Durkovic <durkovic@tum.de>
2013-09-27 01:29:31 +0200

    Set initSelection only when prefill has values.

--- src/pat/autosuggest.js
+++ src/pat/autosuggest.js
@@ -40,7 +40,7 @@ define([
                 openOnEnter: false
             };
 
-            if (prefill) {
+            if (prefill.length) {
                 config.initSelection = function (element, callback) {
                     var data = [];
                     $(element.val().split(",")).each(function () {
