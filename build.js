b56940620608329ea0081aa3be1d222beb63caf3
Yiorgis Gozadinos <ggozad@crypho.com>
2014-01-29 13:41:55 +0100

    Make build.js to hold the paths for all patterns and build with r.js

---
+++ build.js
@@ -0,0 +1,21 @@
+({
+    baseUrl: "src",
+    out: 'bundle.js',
+    name: 'patterns',
+
+    paths: {
+
+        // Externals
+        jquery: 'bower_components/jquery/jquery',
+
+        // Core
+
+        // Patterns
+        patterns: 'patterns'
+    },
+
+    optimize: 'none'
+})
+
+
+
---
+++ src/patterns.js
@@ -0,0 +1,5 @@
+define([
+    "jquery",
+], function($) {
+    return {};
+});
