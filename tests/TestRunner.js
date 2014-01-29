c23aa4c3269a4e7ef3bf030fb07a22f0251d0ce1
Yiorgis Gozadinos <ggozad@crypho.com>
2014-01-29 14:26:48 +0100

    Make a Testrunner example for jquery-ext

---
+++ tests/TestRunner.js
@@ -0,0 +1,34 @@
+
+define("TestRunner", function() {
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 1000;
+
+    var htmlReporter = new jasmine.HtmlReporter();
+    jasmineEnv.addReporter(htmlReporter);
+
+    //var consoleReporter = new jasmine.ConsoleReporter();
+    //window.console_reporter = consoleReporter;
+    //jasmineEnv.addReporter(consoleReporter);
+
+    jasmineEnv.specFilter = function(spec) {
+        return htmlReporter.specFilter(spec);
+    };
+//             "../tests/specs/core/store.js","../tests/specs/lib/depends_parse.js","../tests/specs/lib/dependshandler.js",,"../tests/specs/pat/autoscale.js","../tests/specs/pat/autosubmit.js","../tests/specs/pat/bumper.js","../tests/specs/pat/carousel.js","../tests/specs/pat/checkedflag.js","../tests/specs/pat/checklist.js","../tests/specs/pat/collapsible.js","../tests/specs/pat/depends.js","../tests/specs/pat/equaliser.js","../tests/specs/pat/focus.js","../tests/specs/pat/gallery.js","../tests/specs/pat/image-crop.js","../tests/specs/pat/inject.js","../tests/specs/pat/legend.js","../tests/specs/pat/markdown.js","../tests/specs/pat/modal.js","../tests/specs/pat/slideshow-builder.js","../tests/specs/pat/stacks.js","../tests/specs/pat/switch.js","../tests/specs/pat/toggle.js","../tests/specs/pat/validate.js","../tests/specs/pat/zoom.js"]
+
+    require([
+        "specs/core/jquery-ext.js",
+         // "../tests/specs/utils.js",
+         // "../tests/specs/core/parser.js",
+         // "../tests/specs/core/utils.js",
+         // "../tests/specs/lib/htmlparser.js",
+         // "../tests/specs/pat/ajax.js",
+         // "../tests/specs/pat/inject.js",
+         // "../tests/specs/pat/markdown.js",
+         // "../tests/specs/pat/slides.js"
+    ], function() {
+        jasmineEnv.execute();
+    });
+});
+
+require(["TestRunner"]);
+
