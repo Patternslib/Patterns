25ab323a9d0b34170b64dc5965e768980b193b2c
Wichert Akkerman <wichert@wiggy.net>
2014-01-29 13:50:16 +0100

    Add explanatory comment to patterns.js

--- src/patterns.js
+++ src/patterns.js
@@ -1,3 +1,11 @@
+/* Patterns bundle configuration.
+ *
+ * This file is used to tell r.js which Patterns to load when it generates a
+ * bundle. This is only used when generating a full Patterns bundle, or when
+ * you want a simple way to include all patterns in your own project. If you
+ * only want to use selected patterns you will need to pull in the patterns
+ * directly in your RequireJS configuration.
+ */
 define([
     "jquery",
 ], function($) {
