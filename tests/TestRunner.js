ba03e14a88fdac3ecc6bfbf4f177d6394c19ecd9
Yiorgis Gozadinos <ggozad@crypho.com>
2014-01-31 10:31:20 +0100

    store pat

--- tests/TestRunner.js
+++ tests/TestRunner.js
@@ -13,7 +13,8 @@ define("TestRunner", function() {
     jasmineEnv.specFilter = function(spec) {
         return htmlReporter.specFilter(spec);
     };
-//             "../tests/specs/core/store.js","../tests/specs/lib/depends_parse.js","../tests/specs/lib/dependshandler.js",,"../tests/specs/pat/autoscale.js","../tests/specs/pat/autosubmit.js","../tests/specs/pat/bumper.js","../tests/specs/pat/carousel.js","../tests/specs/pat/checkedflag.js","../tests/specs/pat/checklist.js","../tests/specs/pat/collapsible.js","../tests/specs/pat/depends.js","../tests/specs/pat/equaliser.js","../tests/specs/pat/focus.js","../tests/specs/pat/gallery.js","../tests/specs/pat/image-crop.js","../tests/specs/pat/inject.js","../tests/specs/pat/legend.js","../tests/specs/pat/markdown.js","../tests/specs/pat/modal.js","../tests/specs/pat/slideshow-builder.js","../tests/specs/pat/stacks.js","../tests/specs/pat/switch.js","../tests/specs/pat/toggle.js","../tests/specs/pat/validate.js","../tests/specs/pat/zoom.js"]
+
+    //   "../tests/specs/lib/depends_parse.js","../tests/specs/lib/dependshandler.js",,"../tests/specs/pat/autoscale.js","../tests/specs/pat/autosubmit.js","../tests/specs/pat/bumper.js","../tests/specs/pat/carousel.js","../tests/specs/pat/checkedflag.js","../tests/specs/pat/checklist.js","../tests/specs/pat/collapsible.js","../tests/specs/pat/depends.js","../tests/specs/pat/equaliser.js","../tests/specs/pat/focus.js","../tests/specs/pat/gallery.js","../tests/specs/pat/image-crop.js","../tests/specs/pat/inject.js","../tests/specs/pat/legend.js","../tests/specs/pat/markdown.js","../tests/specs/pat/modal.js","../tests/specs/pat/slideshow-builder.js","../tests/specs/pat/stacks.js","../tests/specs/pat/switch.js","../tests/specs/pat/toggle.js","../tests/specs/pat/validate.js","../tests/specs/pat/zoom.js"]
 
     require([
         "specs/core/jquery-ext.js",
@@ -21,6 +22,7 @@ define("TestRunner", function() {
         "specs/core/parser.js",
         "specs/core/utils.js",
         "specs/core/url.js",
+        "specs/core/store.js",
         "specs/lib/htmlparser.js",
         "specs/pat/ajax.js",
         "specs/pat/inject.js",
