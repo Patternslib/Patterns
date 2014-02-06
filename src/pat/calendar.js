c483b9c35c417bf94aa9cff3f3344c015458f43c
Marko Durkovic <marko@miding.de>
2014-02-06 17:37:14 +0100

    Update calendar pattern to support >=fullcalendar-2

--- src/pat/calendar.js
+++ src/pat/calendar.js
@@ -20,9 +20,9 @@ define([
     parser.add_argument('height', 'auto');
     parser.add_argument('start-date');
     parser.add_argument('time-format', 'h(:mm)t');
-    parser.add_argument('title-month', 'MMMM yyyy');
-    parser.add_argument('title-week', 'MMM d[ yyyy]{ &#8212; [ MMM] d yyyy}');
-    parser.add_argument('title-day', 'dddd, MMM d, yyyy');
+    parser.add_argument('title-month', 'MMMM YYYY');
+    parser.add_argument('title-week', 'MMM D YYYY');
+    parser.add_argument('title-day', 'dddd, MMM d, YYYY');
     parser.add_argument('column-month', 'ddd');
     parser.add_argument('column-week', 'ddd M/d');
     parser.add_argument('column-day', 'dddd M/d');
@@ -32,7 +32,7 @@ define([
     parser.add_argument('category-controls', '');
     parser.add_argument('default-view', 'month');
 
-    var _ = {
+    var _ = +$.fullCalendar.version.substr(0,1) !== 2 ? {
         name: 'calendar',
         trigger: '.pat-calendar',
 
@@ -290,6 +290,292 @@ define([
                         (location ? (' (' + location + ')') : ''),
                     start: $('.start', event).attr('datetime'),
                     end: $('.end', event).attr('datetime'),
+                    allDay: $(event).hasClass('all-day'),
+                    url: $('a', event).attr('href'),
+                    className: classNames,
+                    attrs: attrs,
+                    editable: $(event).hasClass('editable')
+                };
+                if (!ev.title) {
+                    log.error('No event title for:', event);
+                }
+                if (!ev.start) {
+                    log.error('No event start for:', event);
+                }
+                if (!ev.url) {
+                    log.error('No event url for:', event);
+                }
+                return ev;
+            }).toArray();
+            return events;
+        }
+    } : {
+        name: 'calendar',
+        trigger: '.pat-calendar',
+
+        init: function($el) {
+            var cfg = parser.parse($el),
+                calOpts = {
+                    header: false,
+                    droppable: true,
+                    drop: function(date, event, undef, view) {
+                        var $this = $(this),
+                            $ev = $this.hasClass('cal-event') ?
+                                $this : $this.parents('.cal-event'),
+                            $cal = $(view.element).parents('.pat-calendar');
+
+                        $ev.appendTo($cal.find('.cal-events'));
+                        var $start = $ev.find('.start');
+                        if (!$start.length) {
+                            $('<time class="start"/>').attr('datetime', date.format())
+                                .appendTo($ev);
+                        }
+                        var $end = $ev.find('.end');
+                        if (!$end.length) {
+                            $('<time class="end"/>').appendTo($ev);
+                        }
+
+                        if (date.hasTime()) {
+                            $ev.addClass('all-day');
+                        } else {
+                            $ev.removeClass('all-day');
+                        }
+                        $cal.fullCalendar('refetchEvents');
+                        $cal.find('a').each(function(a) { $(a).draggable = 1; });
+                    },
+                    events: function(start, end, timezone, callback) {
+                        var events = _.parseEvents($el);
+                        callback(events);
+                    },
+                    firstHour: cfg.first.hour,
+                    axisFormat: cfg.timeFormat,
+                    timeFormat: cfg.timeFormat,
+                    titleFormat: cfg.title,
+                    columnFormat: cfg.column,
+                    ignoreTimezone: false,
+                    viewRender: _.highlightButtons,
+                    defaultView: cfg.defaultView
+                };
+
+            if (cfg.startDate) {
+                calOpts.defaultDate = $.fullCalendar.moment(cfg.startDate, 'YYYY-MM-DD');
+            } else {
+                calOpts.defaultDate = $.fullCalendar.moment();
+            }
+
+            if (cfg.height !== 'auto') {
+                calOpts.height = cfg.height;
+            }
+
+            var dayNames = [ 'su', 'mo', 'tu', 'we', 'th', 'fr', 'sa' ];
+            if (dayNames.indexOf(cfg.first.day) >= 0) {
+                calOpts.firstDay = dayNames.indexOf(cfg.first.day);
+            }
+
+            var refetch = function() {
+                $el.fullCalendar('refetchEvents');
+            };
+            var refetch_deb = utils.debounce(refetch, 400);
+
+            var $filter = $el.find('.filter');
+            if ($filter && $filter.length > 0) {
+                $('.search-text', $filter).on('keyup', refetch_deb);
+                $('.search-text[type=search]', $filter).on('click', refetch_deb);
+                $('select[name=state]', $filter).on('change', refetch);
+                $('.check-list', $filter).on('change', refetch);
+            }
+
+            var $categoryRoot = cfg.categoryControls ?
+                $(cfg.categoryControls) : $el;
+
+            $el.categories = $el.find('.cal-events .cal-event')
+                .map(function() {
+                    return this.className.split(' ').filter(function(cls) {
+                        return (/^cal-cat/).test(cls);
+                    });
+                });
+
+            $el.$catControls = $categoryRoot.find('input[type=checkbox]');
+            $el.$catControls.on('change', refetch);
+
+
+            $el.fullCalendar(calOpts);
+
+            // move to end of $el
+            $el.find('.fc-content').appendTo($el);
+
+            if (cfg.height === 'auto') {
+                $el.fullCalendar('option', 'height',
+                    $el.find('.fc-content').height());
+
+                $(window).on('resize.pat-calendar', function() {
+                    $el.fullCalendar('option', 'height',
+                        $el.find('.fc-content').height());
+                });
+                $(document).on('pat-update.pat-calendar', function() {
+                    $el.fullCalendar('option', 'height',
+                        $el.find('.fc-content').height());
+                });
+            }
+
+            // update title
+            var $title = $el.find('.cal-title');
+            $title.text($el.fullCalendar('getView').title);
+
+            var $controlRoot = cfg.calendarControls ?
+                $(cfg.calendarControls) : $el;
+            $controlRoot.find('.view-month').addClass('active');
+
+            $controlRoot.find('.jump-next').on('click', function() {
+                $el.fullCalendar('next');
+                $title.html($el.fullCalendar('getView').title);
+            });
+            $controlRoot.find('.jump-prev').on('click', function() {
+                $el.fullCalendar('prev');
+                $title.html($el.fullCalendar('getView').title);
+            });
+            $controlRoot.find('.jump-today').on('click', function() {
+                $el.fullCalendar('today');
+                $title.html($el.fullCalendar('getView').title);
+            });
+            $controlRoot.find('.view-month').on('click', function() {
+                $el.fullCalendar('changeView', 'month');
+                $title.html($el.fullCalendar('getView').title);
+                if (cfg.height === 'auto') {
+                    $el.fullCalendar('option', 'height',
+                        $el.find('.fc-content').height());
+                }
+            });
+            $controlRoot.find('.view-week').on('click', function() {
+                $el.fullCalendar('changeView', 'agendaWeek');
+                $title.html($el.fullCalendar('getView').title);
+                if (cfg.height === 'auto') {
+                    $el.fullCalendar('option', 'height',
+                        $el.find('.fc-content').height());
+                }
+            });
+            $controlRoot.find('.view-day').on('click', function() {
+                $el.fullCalendar('changeView', 'agendaDay');
+                $title.html($el.fullCalendar('getView').title);
+                if (cfg.height === 'auto') {
+                    $el.fullCalendar('option', 'height',
+                        $el.find('.fc-content').height());
+                }
+            });
+
+            $el.find('.cal-events').css('display', 'none');
+
+            // make .cal-event elems draggable
+            dnd.draggable($('.cal-events .cal-event'));
+
+            // emulate jQueryUI dragstop and mousemove during drag.
+            $('.cal-events .cal-event').on('dragend', function() {
+                $(this).trigger('dragstop');
+            });
+            $el.on('dragover', function(event) {
+                event.preventDefault();
+                event.type = 'mousemove';
+                $(document).trigger(event);
+            });
+
+            if (!$.fn.draggable) {
+                $.fn.draggable = function(opts) {
+                    var start = opts.start,
+                        stop = opts.stop;
+                    this.on('dragstart', function(event) {
+                        start(event, null);
+                    });
+                    this.on('dragend', function(event) {
+                        stop(event, null);
+                    });
+                };
+            }
+        },
+
+        highlightButtons: function(view, element) {
+            var $el = element.parents('.pat-calendar').first(),
+                $today = $el.find('.jump-today');
+            $today.removeClass('active');
+            if (view.name === 'agendaDay') {
+                var calDate = $el.fullCalendar('getDate'),
+                    today = $.fullCalendar.moment();
+                if (calDate.date() === today.date() &&
+                    calDate.month() === today.month() &&
+                    calDate.year() === today.year()) {
+                    $today.addClass('active');
+                }
+            }
+
+            var classMap = {
+                month: '.view-month',
+                agendaWeek: '.view-week',
+                agendaDay: '.view-day'
+            };
+            $el.find('.view-month').removeClass('active');
+            $el.find('.view-week').removeClass('active');
+            $el.find('.view-day').removeClass('active');
+            $el.find(classMap[view.name]).addClass('active');
+        },
+
+        parseEvents: function($el) {
+            var $events = $el.find('.cal-events'),
+                $filter = $el.find('.filter'),
+                searchText,
+                regex;
+
+            // parse filters
+            if ($filter && $filter.length > 0) {
+                searchText = $('.search-text', $filter).val();
+                regex = new RegExp(searchText, 'i');
+            }
+
+            var hiddenCats = $el.categories.filter(function() {
+                var cat = this;
+                return $el.$catControls.filter(function() {
+                    return !this.checked &&
+                        $(this)
+                            .parents()
+                            .andSelf()
+                            .hasClass(cat);
+                }).length;
+            });
+
+            var events = $events.find('.cal-event').filter(function() {
+                var $event = $(this);
+
+                if (searchText && !regex.test($event.find('.title').text())) {
+                    log.debug('remove due to search-text='+searchText, $event);
+                    return false;
+                }
+
+                return !hiddenCats.filter(function() {
+                    return $event.hasClass(this);
+                }).length;
+            }).map(function(idx, event) {
+                var attr, i;
+
+                // classNames: all event classes without 'event' + anchor classes
+                var classNames = $(event).attr('class').split(/\s+/)
+                    .filter(function(cls) { return (cls !== 'cal-event'); })
+                    .concat($('a', event).attr('class').split(/\s+/));
+
+                // attrs: all 'data-' attrs from anchor
+                var allattrs = $('a', event)[0].attributes,
+                    attrs = {};
+                for (attr, i=0; i<allattrs.length; i++){
+                    attr = allattrs.item(i);
+                    if (attr.nodeName.slice(0,5) === 'data-') {
+                        attrs[attr.nodeName] = attr.nodeValue;
+                    }
+                }
+
+                var location = ($('.location', event).html() || '').trim();
+
+                var ev = {
+                    title: $('.title', event).text().trim() +
+                        (location ? (' (' + location + ')') : ''),
+                    start: $.fullCalendar.moment($('.start', event).attr('datetime')),
+                    end: $.fullCalendar.moment($('.end', event).attr('datetime')),
                     allDay: $(event).hasClass('all-day'),
                     url: $('a', event).attr('href'),
                     className: classNames,
