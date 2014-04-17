2bb055381f64307aaf38def06ff9472be2a6da79
JC Brand <jc@opkode.com>
2014-04-17 09:23:40 +0200

    Sort deps by name

--- tests/TestRunner.js
+++ tests/TestRunner.js
@@ -15,17 +15,16 @@ define("TestRunner", function() {
     };
 
     require([
-        "./specs/core/jquery-ext.js",
-        "specs/core/utils.js",
+        "specs/core/jquery-ext.js",
         "specs/core/parser.js",
-        "specs/core/utils.js",
-        "specs/core/url.js",
         "specs/core/store.js",
-        "specs/lib/htmlparser.js",
+        "specs/core/url.js",
+        "specs/core/utils.js",
+        "specs/core/utils.js",
         "specs/lib/depends_parse.js",
         "specs/lib/dependshandler",
+        "specs/lib/htmlparser.js",
         "specs/pat/ajax.js",
-        "specs/pat/inject.js",
         "specs/pat/autoscale.js",
         "specs/pat/autosubmit.js",
         "specs/pat/bumper.js",
@@ -36,18 +35,19 @@ define("TestRunner", function() {
         "specs/pat/depends.js",
         "specs/pat/equaliser.js",
         "specs/pat/focus.js",
+        "specs/pat/gallery.js",
+        "specs/pat/image-crop.js",
+        "specs/pat/inject.js",
         "specs/pat/legend",
+        "specs/pat/markdown.js",
         "specs/pat/modal",
-        "specs/pat/slideshow-builder",
         "specs/pat/slides.js",
+        "specs/pat/slideshow-builder",
         "specs/pat/stacks.js",
         "specs/pat/switch.js",
         "specs/pat/toggle.js",
         "specs/pat/validate.js",
-        "specs/pat/zoom.js",
-        "specs/pat/image-crop.js",
-        "specs/pat/gallery.js",
-        "specs/pat/markdown.js"
+        "specs/pat/zoom.js"
     ], function() {
         jasmineEnv.execute();
     });
