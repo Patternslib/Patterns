12cdcedc29a2e81fd5391392262432efc7b4894c
JC Brand <jc@opkode.com>
2014-04-24 12:19:36 +0200

    Conform to Jasmine conventions around naming of test methods
    Behavior-driven tests, when written with end-user readability in mind, can be
    easy-to read and will resemble user-stories or a usage specification.
    
    This means "finishing the sentence" after the "it(" or "describe(" methods.
    I.e. taking the names of those methods as the start of a sentence, that is
    completed with the spec name.

--- tests/specs/pat/checklist.js
+++ tests/specs/pat/checklist.js
@@ -1,6 +1,6 @@
 define(["pat-checklist"], function() {
 
-    describe("checklist-pattern", function() {
+    describe("pat-checklist", function() {
 
         beforeEach(function() {
             $("<div/>", {id: "lab"}).appendTo(document.body);
@@ -10,27 +10,22 @@ define(["pat-checklist"], function() {
             $("#lab").remove();
         });
 
-        describe("jQuery plugin usage", function() {
+        describe("Initialization via jQuery", function() {
+            it("can be configured by passing in arguments to the jQuery plugin method", function () {
+                $("#lab").html("<div></div>");
+                $("#lab div").patternChecklist({select: ".one", deselect: ".two"});
+                var $trigger = $("#lab div");
+                expect($trigger.data("patternChecklist")).toEqual({select: ".one", deselect: ".two"});
+            });
 
-            describe("Initialise via jQuery", function() {
-                it("Specify options via API", function() {
-                    $("#lab").html("<div></div>");
-                    $("#lab div").patternChecklist({select: ".one", deselect: ".two"});
-                    var $trigger = $("#lab div");
-                    expect($trigger.data("patternChecklist")).toEqual({select: ".one", deselect: ".two"});
-                });
-
-                it("Parse options from DOM", function() {
-                    $("#lab").html("<div data-pat-checklist='.one .two'></div>");
-                    $("#lab div").patternChecklist();
-                    var $trigger = $("#lab div");
-                    expect($trigger.data("patternChecklist")).toEqual({select: ".one", deselect: ".two"});
-                });
+            it("can be configured via DOM element data- attributes", function() {
+                $("#lab").html("<div data-pat-checklist='.one .two'></div>");
+                $("#lab div").patternChecklist();
+                var $trigger = $("#lab div");
+                expect($trigger.data("patternChecklist")).toEqual({select: ".one", deselect: ".two"});
             });
         });
     });
-
 });
-
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
 // vim: sw=4 expandtab
