97a5c3762af38b08b3758fe7ef5ba7bfe37a1fe3
JC Brand <jc@opkode.com>
2015-01-12 20:09:25 +0100

    Set regex as attr and check for leading whitespace.

--- src/core/parser.js
+++ src/core/parser.js
@@ -22,6 +22,7 @@ define([
 
     ArgumentParser.prototype = {
         group_pattern: /([a-z][a-z0-9]*)-([A-Z][a-z0-0\-]*)/i,
+        json_param_pattern: /^\s*{/i,
         named_param_pattern: /^\s*([a-z][a-z0-9\-]*)\s*:(.*)/i,
         token_pattern: /((["']).*?(?!\\)\2)|\s*(\S+)\s*/g,
 
@@ -256,20 +257,20 @@ define([
         _parse: function ArgumentParser_parse(parameter) {
             var opts, extended, sep;
             if (!parameter) { return {}; }
-            if (parameter.match(/^{/)) {
+            if (parameter.match(this.json_param_pattern)) {
                 try {
                     return JSON.parse(parameter);
                 } catch (e) {
-                this.log.warn("Invalid JSON argument found: "+parameter);
+                    this.log.warn("Invalid JSON argument found: "+parameter);
                 }
             }
-            if (parameter.match(this.named_param_pattern))
+            if (parameter.match(this.named_param_pattern)) {
                 return this._parseExtendedNotation(parameter);
-
+            }
             sep = parameter.indexOf(";");
-            if (sep===-1)
+            if (sep===-1) {
                 return this._parseShorthandNotation(parameter);
-
+            }
             opts = this._parseShorthandNotation(parameter.slice(0, sep));
             extended = this._parseExtendedNotation(parameter.slice(sep+1));
             for (var name in extended)
