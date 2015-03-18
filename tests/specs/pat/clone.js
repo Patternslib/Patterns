0f22ba7e2748d888eec0bbc6ce1f1048ee6785e8
JC Brand <jc@opkode.com>
2015-03-18 12:49:34 +0100

    Fix tests after rename of argument.

--- tests/specs/pat/clone.js
+++ tests/specs/pat/clone.js
@@ -9,16 +9,16 @@ define(["pat-registry", "pat-clone"], function(registry) {
             $("#lab").remove();
         });
 
-        it("clones the node's first child when .clone-trigger is clicked and places the clone after the cloned element", function() {
+        it("clones the node's first child when .add-clone is clicked and places the clone after the cloned element", function() {
             var $lab = $('#lab');
             $lab.html(
                 '<div class="pat-clone">' +
                 '    <div class="item">Clone Me</div>' +
-                '    <button class="clone-trigger">Clone!</button>' +
+                '    <button class="add-clone">Clone!</button>' +
                 '</div>');
             registry.scan($lab);
             expect($('div.item').length).toBe(1);
-            $lab.find('.clone-trigger').click();
+            $lab.find('.add-clone').click();
             expect($('div.item').length).toBe(2);
 
             // Try now a different variation with the trigger inside the cloned
@@ -26,32 +26,32 @@ define(["pat-registry", "pat-clone"], function(registry) {
             $lab.empty().html(
                 '<div class="pat-clone">' +
                 '    <div class="item">' +
-                '       <button class="clone-trigger">Clone!</button>' +
+                '       <button class="add-clone">Clone!</button>' +
                 '    </div>' +
                 '</div>');
             registry.scan($lab);
             expect($('div.item').length).toBe(1);
-            $lab.find('.clone-trigger').click();
+            $lab.find('.add-clone').click();
             expect($('div.item').length).toBe(2);
-            $lab.find('.clone-trigger:last').click();
+            $lab.find('.add-clone:last').click();
             expect($('div.item').length).toBe(3);
-            $lab.find('.clone-trigger:first').click();
+            $lab.find('.add-clone:first').click();
             expect($('div.item').length).toBe(4);
         });
 
         it("allows the trigger element to be configured", function() {
             var $lab = $('#lab');
             $lab.html(
-                '<div class="pat-clone" data-pat-clone="trigger-element: .real-clone-trigger;">' +
+                '<div class="pat-clone" data-pat-clone="trigger-element: .real-add-clone;">' +
                 '    <div class="item">Clone Me</div>' +
-                '    <button class="clone-trigger">Will not Clone!</button>' +
-                '    <button class="real-clone-trigger">Clone!</button>' +
+                '    <button class="add-clone">Will not Clone!</button>' +
+                '    <button class="real-add-clone">Clone!</button>' +
                 '</div>');
             registry.scan($lab);
             expect($('div.item').length).toBe(1);
-            $lab.find('.clone-trigger').click();
+            $lab.find('.add-clone').click();
             expect($('div.item').length).toBe(1);
-            $lab.find('.real-clone-trigger').click();
+            $lab.find('.real-add-clone').click();
             expect($('div.item').length).toBe(2);
         });
 
@@ -62,10 +62,10 @@ define(["pat-registry", "pat-clone"], function(registry) {
                 '    <div class="item">Original item</div>' +
                 '    <div class="item" id="my-template">Clone template</div>' +
                 '</div>' +
-                '<button class="clone-trigger">Clone!</button>');
+                '<button class="add-clone">Clone!</button>');
             registry.scan($lab);
             expect($('div.item').length).toBe(2);
-            $lab.find('.clone-trigger').click();
+            $lab.find('.add-clone').click();
             expect($('div.item').length).toBe(3);
             expect($('div.item:last').text()).toBe('Clone template');
             expect($("div.item:contains('Clone template')").length).toBe(2);
@@ -77,15 +77,15 @@ define(["pat-registry", "pat-clone"], function(registry) {
                 '<div class="pat-clone">' +
                 '    <p class="legend" name="item-1">Family member #{1}</p>' +
                 '</div>' +
-                '<button class="clone-trigger">Clone!</button>');
+                '<button class="add-clone">Clone!</button>');
             registry.scan($lab);
             expect($('p.legend').length).toBe(1);
 
-            $lab.find('.clone-trigger').click();
+            $lab.find('.add-clone').click();
             expect($('p.legend').length).toBe(2);
             expect($('p.legend:last').attr('name')).toBe('item-2');
 
-            $lab.find('.clone-trigger').click();
+            $lab.find('.add-clone').click();
             expect($('p.legend').length).toBe(3);
             expect($('p.legend:last').attr('name')).toBe('item-3');
         });
@@ -96,11 +96,11 @@ define(["pat-registry", "pat-clone"], function(registry) {
                 '<div class="pat-clone">' +
                 '    <div class="item"><button type="button" class="remove-clone">Remove</button></div>' +
                 '</div>' +
-                '<button class="clone-trigger">Clone!</button>');
+                '<button class="add-clone">Clone!</button>');
             registry.scan($lab);
             expect($('div.item').length).toBe(1);
 
-            $lab.find('.clone-trigger').click();
+            $lab.find('.add-clone').click();
             expect($('div.item').length).toBe(2);
 
             $lab.find('.remove-clone:last').click();
@@ -116,11 +116,11 @@ define(["pat-registry", "pat-clone"], function(registry) {
                 '<div class="pat-clone" data-pat-clone="remove-element: .custom-remove-class;">' +
                 '    <div class="item"><button type="button" class="custom-remove-class">Remove</button></div>' +
                 '</div>' +
-                '<button class="clone-trigger">Clone!</button>');
+                '<button class="add-clone">Clone!</button>');
             registry.scan($lab);
             expect($('div.item').length).toBe(1);
 
-            $lab.find('.clone-trigger').click();
+            $lab.find('.add-clone').click();
             expect($('div.item').length).toBe(2);
 
             $lab.find('.custom-remove-class:last').click();
