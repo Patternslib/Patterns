2f2b26abc7fd191901dd2b2052ea165ba5fb5c68
JC Brand <jc@opkode.com>
2015-04-15 13:22:21 +0200

    Add new arguments to pat-collapsible
    close-trigger and open-trigger for opening or closing collapsibles.
    See here: https://github.com/euphorie/osha.oira/issues/13
    
    * Updated the documentation
    * Add tests

--- tests/specs/pat/collapsible.js
+++ tests/specs/pat/collapsible.js
@@ -10,21 +10,21 @@ define(["pat-registry", "pat-collapsible"], function(registry, Pattern) {
             $("#lab").remove();
         });
 
-        describe("init", function() {
-            it("Create panel-content", function() {
-                var $lab = $("#lab");
-                $lab.html([
-                    "<div class=\"pat-collapsible\">",
-                    "<h3>Trigger header</h3>",
-                    "<p>Collapsible content</p>",
-                    "</div>"
-                    ].join("\n"));
-                var $collapsible = $("#lab .pat-collapsible");
-                Pattern.init($collapsible);
-                expect($collapsible.find(".panel-content").length).toBe(1);
-            });
+        it("wraps the collapsible within a div.panel-content", function() {
+            var $lab = $("#lab");
+            $lab.html([
+                "<div class=\"pat-collapsible\">",
+                "<h3>Trigger header</h3>",
+                "<p>Collapsible content</p>",
+                "</div>"
+                ].join("\n"));
+            var $collapsible = $("#lab .pat-collapsible");
+            Pattern.init($collapsible);
+            expect($collapsible.find(".panel-content").length).toBe(1);
+        });
 
-            it("Panels are open by default", function() {
+        describe("A Collapsible", function() {
+            it("is open by default", function() {
                 var $lab = $("#lab");
                 $lab.html([
                     "<div class=\"pat-collapsible\">",
@@ -37,7 +37,7 @@ define(["pat-registry", "pat-collapsible"], function(registry, Pattern) {
                 expect($collapsible.hasClass("open")).toBeTruthy();
             });
 
-            it("Explicitly closed panel is not open", function() {
+            it("can be explicitly closed by adding the class 'closed'", function() {
                 var $lab = $("#lab");
                 $lab.html([
                     "<div class=\"pat-collapsible closed\">",
@@ -49,10 +49,8 @@ define(["pat-registry", "pat-collapsible"], function(registry, Pattern) {
                 Pattern.init($collapsible);
                 expect($collapsible.hasClass("open")).toBeFalsy();
             });
-        });
 
-        describe("toggle", function() {
-            it("Toggle an open panel", function() {
+            it("can be toggled closed if it's open", function() {
                 var $lab = $("#lab");
                 $lab.html([
                     "<div class=\"pat-collapsible\">",
@@ -70,7 +68,7 @@ define(["pat-registry", "pat-collapsible"], function(registry, Pattern) {
                 expect($trigger.hasClass("collapsible-closed")).toBe(true);
             });
 
-            it("Toggle a closed panel", function() {
+            it("can be toggled open if it's closed", function() {
                 var $lab = $("#lab");
                 $lab.html([
                     "<div class=\"pat-collapsible closed\">",
@@ -87,9 +85,44 @@ define(["pat-registry", "pat-collapsible"], function(registry, Pattern) {
                 expect($trigger.hasClass("collapsible-open")).toBe(true);
                 expect($trigger.hasClass("collapsible-closed")).toBe(false);
             });
+
+            it("can be configured to have trigger which only opens it", function() {
+                var $lab = $("#lab"), $collapsible;
+                $lab.html([
+                    "<div class=\"closed pat-collapsible\" data-pat-collapsible=\"open-trigger: #open\">",
+                    "<button id=\"open\">open</button>",
+                    "<p>Collapsible content</p>",
+                    "</div>"
+                    ].join("\n"));
+                $collapsible = $("#lab .pat-collapsible");
+                registry.scan($collapsible);
+                Pattern.init($collapsible, {transition: "none"});
+                expect($collapsible.hasClass("open")).toBe(false);
+                expect($collapsible.hasClass("closed")).toBe(true);
+                $('#open').click();
+                expect($collapsible.hasClass("open")).toBe(true);
+                expect($collapsible.hasClass("closed")).toBe(false);
+            });
+
+            it("can be configured to have trigger which only closes it", function() {
+                var $lab = $("#lab"), $collapsible;
+                $lab.html([
+                    "<div class=\"pat-collapsible\" data-pat-collapsible=\"open-trigger: #close\">",
+                    "<button id=\"close\">close</button>",
+                    "<p>Collapsible content</p>",
+                    "</div>"
+                    ].join("\n"));
+                $collapsible = $("#lab .pat-collapsible");
+                registry.scan($collapsible);
+                Pattern.init($collapsible, {transition: "none"});
+                expect($collapsible.hasClass("close")).toBe(false);
+                expect($collapsible.hasClass("open")).toBe(true);
+                $('#close').click();
+                expect($collapsible.hasClass("close")).toBe(true);
+                expect($collapsible.hasClass("open")).toBe(false);
+            });
         });
     });
-
 });
 
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
