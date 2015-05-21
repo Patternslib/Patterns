77994803bccab7919d33a52c4e5c5d9bccebb4ee
JC Brand <jc@opkode.com>
2015-05-21 15:51:43 +0200

    We need almond when making the bundle.

--- build.js
+++ build.js
@@ -2,9 +2,13 @@
     baseUrl: "src",
     out: "bundle.js",
     include: ["patterns"],
+    name: "almond",
     mainConfigFile: 'main.js',
     wrap: {
         endFile: "src/wrap-end.js"
     },
-    optimize: "none"
+    optimize: "none",
+    paths: {
+        "almond":                      "bower_components/almond/almond"
+    }
 })
