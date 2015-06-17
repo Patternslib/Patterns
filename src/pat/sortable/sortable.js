29563b71cd3baaebf2a639809f37025fb0fbc126
JC Brand <jc@opkode.com>
2015-06-17 18:05:57 +0200

    Rewrite pat-sortable to rely on pat-base.

--- src/pat/sortable/sortable.js
+++ src/pat/sortable/sortable.js
@@ -1,134 +1,128 @@
 define([
     "jquery",
-    "pat-registry",
-    "pat-logger",
+    "pat-base",
     "pat-parser"
-], function($, patterns, logger, Parser) {
+], function($, Base, Parser) {
     var parser = new Parser("sortable");
-
     parser.addArgument("selector", "li");
 
-    var _ = {
+    return Base.extend({
         name: "sortable",
         trigger: ".pat-sortable",
 
-        init: function($el) {
-            if ($el.length > 1)
-                return $el.each(function() { _.init($(this)); });
-
-            var cfgs = parser.parse($el, true);
-            $el.data("patterns.sortable", cfgs);
-
+        init: function ($el) {
+            this.options = parser.parse(this.$el, true);
+            $el.data("patterns.sortable", this.options); // XXX: Necessary?
             // use only direct descendants to support nested lists
-            var $sortables = $el.children().filter(cfgs[0].selector);
+            this.$sortables = $el.children().filter(this.options[0].selector);
+            this.addHandles().initScrolling();
+        },
 
-            // add handles and make them draggable for HTML5 and IE8/9
-            // it has to be an "a" tag (or img) to make it draggable in IE8/9
-            var $handles = $("<a href=\"#\" class=\"handle\"></a>").appendTo($sortables);
-            if("draggable" in document.createElement("span"))
+        addHandles: function () {
+            /* Add handles and make them draggable for HTML5 and IE8/9
+             * it has to be an "a" tag (or img) to make it draggable in IE8/9
+             */
+            var $handles = $("<a href=\"#\" class=\"handle\"></a>").appendTo(this.$sortables);
+            if("draggable" in document.createElement("span")) {
                 $handles.attr("draggable", true);
-            else
+            } else {
                 $handles.bind("selectstart", function(event) {
                     event.preventDefault();
                 });
+            }
+            $handles.bind("dragstart", this.onDragStart.bind(this));
+            $handles.bind("dragend", this.onDragEnd.bind(this));
+            return this;
+        },
 
+        initScrolling: function () {
             // invisible scroll activation areas
             var scrollup = $("<div id=\"pat-scroll-up\">&nbsp;</div>"),
                 scrolldn = $("<div id=\"pat-scroll-dn\">&nbsp;</div>"),
                 scroll = $().add(scrollup).add(scrolldn);
-
             scrollup.css({ top: 0 });
             scrolldn.css({ bottom: 0 });
             scroll.css({
                 position: "fixed", zIndex: 999999,
                 height: 32, left: 0, right: 0
             });
-
             scroll.bind("dragover", function(event) {
                 event.preventDefault();
-                if ($("html,body").is(":animated")) return;
-
-                var newpos = $(window).scrollTop() +
-                    ($(this).attr("id")==="pat-scroll-up" ? -32 : 32);
-
+                if ($("html,body").is(":animated")) { return; }
+                var newpos = $(window).scrollTop() + ($(this).attr("id")==="pat-scroll-up" ? -32 : 32);
                 $("html,body").animate({scrollTop: newpos}, 50, "linear");
             });
+            return this;
+        },
 
-            $handles.bind("dragstart", function(event) {
-                // Firefox seems to need this set to any value
-                event.originalEvent.dataTransfer.setData("Text", "");
-                event.originalEvent.dataTransfer.effectAllowed = ["move"];
-                if ("setDragImage" in event.originalEvent.dataTransfer)
-                    event.originalEvent.dataTransfer.setDragImage(
-                        $(this).parent()[0], 0, 0);
-                $(this).parent().addClass("dragged");
+        onDragEnd: function (event) {
+            $(".dragged").removeClass("dragged");
+            this.$sortables.unbind(".pat-sortable");
+            this.$el.unbind(".pat-sortable");
+            $("#pat-scroll-up, #pat-scroll-dn").detach();
+        },
 
-                // Scroll the list if near the borders
-                $el.bind("dragover.pat-sortable", function(event) {
-                    event.preventDefault();
-                    if ($el.is(":animated")) return;
+        onDragStart: function (event) {
+            var $handle = $(event.target),
+                $draggable = $handle.parent();
 
-                    var pos = event.originalEvent.clientY + $("body").scrollTop();
+            // Firefox seems to need this set to any value
+            event.originalEvent.dataTransfer.setData("Text", "");
+            event.originalEvent.dataTransfer.effectAllowed = ["move"];
+            if ("setDragImage" in event.originalEvent.dataTransfer) {
+                event.originalEvent.dataTransfer.setDragImage($draggable[0], 0, 0);
+            }
+            $draggable.addClass("dragged");
 
-                    if (pos - $el.offset().top < 32)
-                        $el.animate({scrollTop: $el.scrollTop()-32}, 50, "linear");
-                    else if ($el.offset().top+$el.height() - pos < 32)
-                        $el.animate({scrollTop: $el.scrollTop()+32}, 50, "linear");
-                });
+            // Scroll the list if near the borders
+            this.$el.bind("dragover.pat-sortable", function(event) {
+                event.preventDefault();
+                if (this.$el.is(":animated")) return;
 
-                // list elements are only drop targets when one element of the
-                // list is being dragged. avoids dragging between lists.
-                $sortables.bind("dragover.pat-sortable", function(event) {
-                    var $this = $(this),
-                        midlineY = $this.offset().top - $(document).scrollTop() +
-                            $this.height()/2;
+                var pos = event.originalEvent.clientY + $("body").scrollTop();
 
+                if (pos - this.$el.offset().top < 32)
+                    this.$el.animate({scrollTop: this.$el.scrollTop()-32}, 50, "linear");
+                else if (this.$el.offset().top+this.$el.height() - pos < 32)
+                    this.$el.animate({scrollTop: this.$el.scrollTop()+32}, 50, "linear");
+            }.bind(this));
+
+            // list elements are only drop targets when one element of the
+            // list is being dragged. avoids dragging between lists.
+            this.$sortables.bind("dragover.pat-sortable", function(event) {
+                var $this = $(this),
+                    midlineY = $this.offset().top - $(document).scrollTop() + $this.height()/2;
+
+                if ($(this).hasClass("dragged")) {
                     // bail if dropping on self
-                    if ($(this).hasClass("dragged"))
-                        return;
-
-                    $this.removeClass("drop-target-above drop-target-below");
-                    if (event.originalEvent.clientY > midlineY)
-                        $this.addClass("drop-target-below");
-                    else
-                        $this.addClass("drop-target-above");
-                    event.preventDefault();
-                });
-
-                $sortables.bind("dragleave.pat-sortable", function() {
-                    $sortables.removeClass("drop-target-above drop-target-below");
-                });
-
-                $sortables.bind("drop.pat-sortable", function(event) {
-                    if ($(this).hasClass("dragged"))
-                        return;
-
-                    if ($(this).hasClass("drop-target-below"))
-                        $(this).after($(".dragged"));
-                    else
-                        $(this).before($(".dragged"));
-                    $(this).removeClass("drop-target-above drop-target-below");
-                    event.preventDefault();
-                });
-
-                //XXX: Deactivate document scroll areas, as DnD affects only
-                //     scrolling of parent element
-                //scroll.appendTo("body");
+                    return;
+                }
+                $this.removeClass("drop-target-above drop-target-below");
+                if (event.originalEvent.clientY > midlineY)
+                    $this.addClass("drop-target-below");
+                else
+                    $this.addClass("drop-target-above");
+                event.preventDefault();
             });
 
-            $handles.bind("dragend", function() {
-                $(".dragged").removeClass("dragged");
-                $sortables.unbind(".pat-sortable");
-                $el.unbind(".pat-sortable");
-                $("#pat-scroll-up, #pat-scroll-dn").detach();
-            });
+            this.$sortables.bind("dragleave.pat-sortable", function() {
+                this.$sortables.removeClass("drop-target-above drop-target-below");
+            }.bind(this));
 
-            return $el;
+            this.$sortables.bind("drop.pat-sortable", function(event) {
+                var $sortable = $(this);
+                if ($sortable.hasClass("dragged"))
+                    return;
+
+                if ($sortable.hasClass("drop-target-below"))
+                    $sortable.after($(".dragged"));
+                else
+                    $sortable.before($(".dragged"));
+                $sortable.removeClass("drop-target-above drop-target-below");
+                event.preventDefault();
+            });
         }
-    };
-
-    patterns.register(_);
-    return _;
+    });
 });
 
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
