77ec0daf03b02fc25ef9e9c3f5eb656387fd7061
JC Brand <jc@opkode.com>
2015-06-29 18:45:09 +0200

    Refactored auto-submit.
    Instead of having .pat-autosubmit initialized for each input, only initialize
    it for the particular element which has "pat-autosubmit" as class.
    
    This is as attempt to tackle these issues:
    
    - Selecting all or none via pat-checklist caused an Ajax call for each input.
    - The docs specify that you can have .pat-autosubmit on a single input element,
      this wasn't the case.
    - The docs specify that you can have .pat-autosubmit on a element which groups
      inputs and that only those will trigger a submit, but in reality all inputs
      would trigger a submit.

--- src/pat/auto-submit/auto-submit.js
+++ src/pat/auto-submit/auto-submit.js
@@ -4,15 +4,17 @@
  * Copyright 2012-2013 Florian Friesdorf
  * Copyright 2012 Simplon B.V. - Wichert Akkerman
  * Copyright 2013 Marko Durkovic
+ * Copyright 2014-2015 Syslab.com GmbH - JC Brand 
  */
 define([
     "jquery",
     "pat-registry",
+    "pat-base",
     "pat-logger",
     "pat-parser",
     "pat-input-change-events",
     "pat-utils"
-], function($, registry, logging, Parser, input_change_events, utils) {
+], function($, registry, Base, logging, Parser, input_change_events, utils) {
     var log = logging.getLogger("autosubmit"),
         parser = new Parser("autosubmit");
 
@@ -21,9 +23,9 @@ define([
     // - defocus
     parser.addArgument("delay", "400ms");
 
-    var _ = {
+    return Base.extend({
         name: "autosubmit",
-        trigger: ".pat-autosubmit :input",
+        trigger: ".pat-autosubmit",
         parser: {
             parse: function($el, opts) {
                 var cfg = parser.parse($el, opts);
@@ -35,68 +37,46 @@ define([
         },
 
         init: function($el, opts) {
-            if ($el.length > 1)
-                return $el.each(function() { _.init($(this), opts); });
+            input_change_events.setup($el, "autosubmit");
+            this.$el.on("input-change-delayed.pat-autosubmit", this.onInputChange);
+            this.options = this.parser.parse(this.$el, opts);
+            this.registerHandlers();
+            return this.$el;
+        },
 
-            // handle the form itself
-            if ($el.is("form,.pat-subform")) {
-                if ($el.data("pat-autosubmit-initialized")) {
-                    return $el;
-                }
-                input_change_events.setup($el, "autosubmit");
-                $el.on("input-change-delayed.pat-autosubmit", _.onInputChange)
-                    .data("pat-autosubmit-initialized", true);
-                return $el;
-            }
-
-            // make sure the form is initialized if it does not have the pat-autosubmit class
-            var $form = $el.parents("form,.pat-subform").first();
-            if (!$form.data("pat-autosubmit-initialized")) {
-                _.init($form);
-            }
-
-            var cfg = _.parser.parse($el, opts),
-                isText = $el.is("input:text, input[type=search], textarea");
-
-            if (cfg.delay === "defocus" && !isText) {
+        registerHandlers: function() {
+            var isText = this.$el.is("input:text, input[type=search], textarea");
+            if (this.options.delay === "defocus" && !isText) {
                 log.error("The defocus delay value makes only sense on text input elements.");
-                return $el;
+                return this.$el;
             }
-
-            if (cfg.delay === "defocus") {
-                $el.on("input-defocus.pat-autosubmit", function() {
-                    $el.trigger("input-change-delayed");
+            if (this.options.delay === "defocus") {
+                this.$el.on("input-defocus.pat-autosubmit", function() {
+                    $(this).trigger("input-change-delayed");
                 });
-            } else if (cfg.delay > 0) {
-                $el.on("input-change.pat-autosubmit", utils.debounce(function() {
-                    $el.trigger("input-change-delayed");
-                }, cfg.delay));
+            } else if (this.options.delay > 0) {
+                this.$el.on("input-change.pat-autosubmit", utils.debounce(function() {
+                    $(this).trigger("input-change-delayed");
+                }, this.options.delay));
             } else {
-                $el.on("input-change.pat-autosubmit", function() {
-                    $el.trigger("input-change-delayed");
+                this.$el.on("input-change.pat-autosubmit", function() {
+                    $(this).trigger("input-change-delayed");
                 });
             }
-            return $el;
         },
 
         destroy: function($el) {
             input_change_events.remove($el, "autosubmit");
             $el.removeData("pat-autosubmit-initialized");
-            $el.off(".pat-autosubmit")
-                .find(":input").off(".pat-autosubmit");
+            $el.off(".pat-autosubmit").find(":input").off(".pat-autosubmit");
         },
 
         onInputChange: function(ev) {
             ev.stopPropagation();
-
             $(this).submit();
-
             log.debug("triggered by " + ev.type);
         }
-    };
-
-    registry.register(_);
-    return _;
+    });
 });
 
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
--- src/pat/auto-submit/tests.js
+++ src/pat/auto-submit/tests.js
@@ -1,4 +1,4 @@
-define(["pat-autosubmit"], function(pattern) {
+define(["pat-registry", "pat-autosubmit"], function(registry, pattern) {
 
     describe("pat-autosubmit", function() {
 
@@ -10,15 +10,60 @@ define(["pat-autosubmit"], function(pattern) {
             $("#lab").remove();
         });
 
-        describe("parse", function() {
-            it("Shorthand notation", function() {
-                var $trigger = $("<input data-pat-autosubmit='500ms'/>"),
-                    options = pattern.parser.parse($trigger);
-                expect(options.delay).toBe(500);
+        describe("Triggering of the pattern", function() {
+            it("happens when a form has the pat-autosubmit class", function() {
+                var $form = $(
+                    '<form class="pat-autosubmit">'+
+                    '  <fieldset>'+
+                    '   <input type="text" name="q" placeholder="Search query"/>'+
+                    '   <label><input type="checkbox" name="local"/> Only search in this section</label>'+
+                    ' </fieldset>'+
+                    '</form>');
+                spyOn(pattern, "init");
+                registry.scan($form);
+                expect(pattern.init).toHaveBeenCalled();
             });
-            it("defocus", function() {
-                var $trigger = $("<input data-pat-autosubmit='defocus'/>"),
-                    options = pattern.parser.parse($trigger);
+
+            it("when a grouping of inputs has the pat-autosubmit class", function() {
+                var $form = $('<form>'+
+                '  <fieldset class="pat-autosubmit">'+
+                '   <input type="text" name="q" placeholder="Search query"/>'+
+                '   <label><input type="checkbox" name="local"/> Only search in this section</label>'+
+                ' </fieldset>'+
+                '</form>');
+                spyOn(pattern, "init");
+                registry.scan($form);
+                expect(pattern.init).toHaveBeenCalled();
+            });
+
+            it("when a single input has the pat-autosubmit class", function() {
+                var $form = $('<form><input class="pat-autosubmit" type="text" name="q" placeholder="Search query"/></form>');
+                spyOn(pattern, "init");
+                registry.scan($form);
+                expect(pattern.init).toHaveBeenCalled();
+            });
+        });
+
+        describe("parsing of the delay option", function() {
+            it("can be done in shorthand notation", function() {
+                pattern.$el = $('<form></form>');
+                var pat = pattern.init(pattern.$el);
+                options = pat.parser.parse($("<input data-pat-autosubmit='500ms'/>"));
+                expect(options.delay).toBe(500);
+                options = pat.parser.parse($("<input data-pat-autosubmit='500ms'/>"));
+                expect(options.delay).toBe(500);
+                options = pat.parser.parse($("<input data-pat-autosubmit='defocus'/>"));
+                expect(options.delay).toBe("defocus");
+            });
+
+            it("can be done in longhand notation", function() {
+                pattern.$el = $('<form></form>');
+                var pat = pattern.init(pattern.$el);
+                options = pat.parser.parse($("<input class=\pat-autosubmit\" data-pat-autosubmit='delay: 500ms'/>"));
+                expect(options.delay).toBe(500);
+                options = pat.parser.parse($("<input class=\pat-autosubmit\" data-pat-autosubmit='delay: 500ms'/>"));
+                expect(options.delay).toBe(500);
+                options = pat.parser.parse($("<input class=\pat-autosubmit\" data-pat-autosubmit='defocus'/>"));
                 expect(options.delay).toBe("defocus");
             });
         });
