3a6f942307652d96d7f7f4f43efd69825706821d
JC Brand <jc@opkode.com>
2015-07-28 17:58:17 +0200

    Refactored to properly validate fields referencing one another.
    Date fields using not-before or not-after need to be validated in tandem, since
    either field might resolve a validation failure.

--- src/pat/validation/validation.js
+++ src/pat/validation/validation.js
@@ -12,6 +12,7 @@ define([
     "moment",
     "validate"
 ], function($, Parser, Base, utils, moment, validate) {
+    "use strict";
     validate.moment = moment;
     var parser = new Parser("validate");
     parser.addArgument("disable-selector"); // Elements which must be disabled if there are errors
@@ -20,6 +21,8 @@ define([
     parser.addArgument("message-email", "This value must be a valid email address");
     parser.addArgument("message-number", "This value must be a number");
     parser.addArgument("message-required", "This field is required");
+    parser.addArgument("not-after");
+    parser.addArgument("not-before");
 
     return Base.extend({
         name: "validate",
@@ -29,24 +32,75 @@ define([
             this.errors = 0;
             this.options = parser.parse(this.$el, opts);
             this.$inputs = this.$el.find(':input[name]');
-            this.$inputs.on('change.pat-validate', this.validateElement.bind(this));
+            this.$inputs.on('change.pat-validate', function (ev) { this.validateElement(ev.target); }.bind(this));
             this.$el.on('submit.pat-validate', this.validateForm.bind(this));
             this.$el.on('pat-update.pat-validate', this.onPatternUpdate.bind(this));
         },
 
+        setLocalDateConstraints: function (input, opts, constraints) {
+            /* Set the relative date constraints, i.e. not-after and not-before, as well as custom messages.
+             */
+            var name, c, type = input.getAttribute('type');
+            if (typeof opts == "undefined") {
+                return constraints;
+            }
+            name = input.getAttribute('name').replace(/\./g, '\\.');
+            c = constraints[name][type];
+            _.each(['before', 'after'], function (relation) {
+                var isDate = validate.moment.isDate,
+                    relative = opts.not[relation], arr, constraint, $ref;
+                if (opts.message[type]) {
+                    c.message = '^'+opts.message[type];
+                }
+                if (typeof relative == "undefined") {
+                    return;
+                }
+                constraint = relation === "before" ? 'earliest' : 'latest';
+                if (isDate(relative)) {
+                    c[constraint] = relative;
+                } else {
+                    try {
+                        $ref = $(relative);
+                    } catch (e) {
+                        console.log(e);
+                    }
+                    arr = $ref.data('pat-validate-refs') || [];
+                    if (!_.contains(arr, input)) {
+                        arr.unshift(input);
+                        $ref.data('pat-validate-refs', arr);
+                    }
+                    c[constraint] = $ref.val();
+                }
+            });
+            return constraints;
+        },
+
+        setLocalConstraints: function (input, constraints) {
+            /* Some form fields might have their own data-pat-validate
+             * attribute, used to set field-specific constraints.
+             *
+             * We parse them and add them to the passed in constraints obj.
+             */
+            if (input.dataset.patValidate) {
+                if (_.contains(['datetime', 'date'], input.getAttribute('type'))) {
+                    this.setLocalDateConstraints(input, parser.parse($(input)), constraints);
+                }
+            }
+            return constraints;
+        },
+
         getConstraints: function (input) {
             // Get validation constraints by parsing the input element for hints
-            var name = input.getAttribute('name'),
-                constraints = {};
+            var name = input.getAttribute('name'), constraints = {};
             if (!name) { return; }
             constraints[name.replace(/\./g, '\\.')] = {
                 'presence': input.getAttribute('required') ? { 'message': '^'+this.options.message.required } : false,
-                'email': input.getAttribute('type') == 'email' ? { 'message': '^'+this.options.message.email } : false,
-                'numericality': input.getAttribute('type') == 'number' ? { 'message': '^'+this.options.message.number } : false,
-                'datetime': input.getAttribute('type') == 'datetime' ? { 'message': '^'+this.options.message.datetime } : false,
-                'date': input.getAttribute('type') == 'date' ? { 'message': '^'+this.options.message.date } : false
+                'email': type == 'email' ? { 'message': '^'+this.options.message.email } : false,
+                'numericality': type == 'number' ? { 'message': '^'+this.options.message.number } : false,
+                'datetime': type == 'datetime' ? { 'message': '^'+this.options.message.datetime } : false,
+                'date': type == 'date' ? { 'message': '^'+this.options.message.date } : false
             };
-            return constraints;
+            return this.setLocalConstraints(input, constraints);
         },
 
         getValueDict: function (input) {
@@ -68,10 +122,8 @@ define([
                 return !input.hasAttribute('disabled');
             });
             for (var i=0; i<$not_disabled.length; i++) {
-                input = $not_disabled[i];
-                error = validate(this.getValueDict(input), this.getConstraints(input));
+                error = this.validateElement($not_disabled[i]);
                 if (typeof error != "undefined") {
-                    this.showError(error, input);
                     if (!has_errors) {
                         ev.preventDefault();
                         ev.stopPropagation();
@@ -82,17 +134,21 @@ define([
             }
         },
 
-        validateElement: function (ev) {
+        validateElement: function (input, no_recurse) {
             /* Handler which gets called when a single form :input element
              * needs to be validated. Will prevent the event's default action
              * if validation fails.
              */
-            var error = validate(this.getValueDict(ev.target), this.getConstraints(ev.target));
+            var error = validate(this.getValueDict(input), this.getConstraints(input));
             if (!error) {
-                this.removeError(ev.target);
+                this.removeError(input);
             } else {
-                this.showError(error, ev.target);
+                this.showError(error, input);
             }
+            if (!no_recurse) {
+                _.each($(input).data('pat-validate-refs') || [], _.partial(this.validateElement.bind(this), _, true));
+            }
+            return error;
         },
 
         onPatternUpdate: function (ev, data) {
