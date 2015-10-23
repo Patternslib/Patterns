489bd754b8275afa3cc7dad5ec2d72fd587f6863
JC Brand <jc@opkode.com>
2015-10-23 07:08:08 +0000

    Formatting.

--- src/pat/stacks/stacks.js
+++ src/pat/stacks/stacks.js
@@ -25,10 +25,7 @@ define([
 
         init: function($el, opts) {
             var fragment = this._currentFragment();
-
-            return $el.each(function() {
-                stacks._setupStack(this, opts, fragment);
-            });
+            stacks._setupStack($el, opts, fragment);
         },
 
         _setup: function() {
@@ -38,10 +35,9 @@ define([
         _setupStack: function(container, options, selected) {
             var $container = $(container),
                 $sheets, $visible, $invisible;
-            options=parser.parse($container, options);
+            options = parser.parse($container, options);
             $container.data("pat-stacks", options);
-            $sheets=$container.find(options.selector);
-
+            $sheets = $container.find(options.selector);
             if ($sheets.length < 2) {
                 log.warn("Stacks pattern: must have more than one sheet.", $container[0]);
                 return;
@@ -55,10 +51,10 @@ define([
                 }
             }
             if (!$visible.length) {
-                $visible=$sheets.first();
-                selected=$visible[0].id;
+                $visible = $sheets.first();
+                selected = $visible[0].id;
             }
-            $invisible=$sheets.not($visible);
+            $invisible = $sheets.not($visible);
             utils.hideOrShow($visible, true, {transition: "none"}, stacks.name);
             utils.hideOrShow($invisible, false, {transition: "none"}, stacks.name);
             stacks._updateAnchors($container, selected);
@@ -70,8 +66,9 @@ define([
 
         _currentFragment: function() {
             var parts = this.document.URL.split("#");
-            if (parts.length===1)
+            if (parts.length === 1) {
                 return null;
+            }
             return parts[parts.length-1];
         },
 
@@ -80,11 +77,13 @@ define([
                 href_parts = e.currentTarget.href.split("#"),
                 $stack;
             // Check if this is an in-document link and has a fragment
-            if (base_url!==href_parts[0] || !href_parts[1])
+            if (base_url !== href_parts[0] || !href_parts[1]) {
                 return;
-            $stack=$(stacks.trigger+":has(#"+href_parts[1]+")");
-            if (!$stack.length)
+            }
+            $stack = $(stacks.trigger+":has(#"+href_parts[1]+")");
+            if (!$stack.length) {
                 return;
+            }
             e.preventDefault();
             stacks._updateAnchors($stack, href_parts[1]);
             stacks._switch($stack, href_parts[1]);
@@ -93,28 +92,30 @@ define([
         _updateAnchors: function($container, selected) {
             var options = $container.data("pat-stacks"),
                 $sheets = $container.find(options.selector),
-                base_url = stacks._base_URL();
+                base_url = stacks._base_URL(),
+                sheet, $anchors;
             for (var i=0; i<$sheets.length; i++) {
                 // This may appear odd, but: when querying a browser uses the
                 // original href of an anchor as it appeared in the document
                 // source, but when you access the href property you always
                 // the fully qualified version.
-                var sheet = $sheets[i],
-                    $anchors = $("a[href=\""+base_url+"#"+sheet.id+"\"],a[href=\"#"+sheet.id+"\"]");
-                if (sheet.id===selected)
+                sheet = $sheets[i];
+                $anchors = $("a[href=\""+base_url+"#"+sheet.id+"\"],a[href=\"#"+sheet.id+"\"]");
+                if (sheet.id === selected) {
                     $anchors.addClass("current");
-                else
+                } else {
                     $anchors.removeClass("current");
+                }
             }
         },
 
         _switch: function($container, sheet_id) {
             var options = $container.data("pat-stacks"),
-                $sheet = $container.find("#"+sheet_id),
-                $invisible;
-            if (!$sheet.length || $sheet.hasClass("visible"))
+                $sheet = $container.find("#"+sheet_id);
+            if (!$sheet.length || $sheet.hasClass("visible")) {
                 return;
-            $invisible=$container.find(options.selector).not($sheet);
+            }
+            var $invisible = $container.find(options.selector).not($sheet);
             utils.hideOrShow($invisible, false, options, stacks.name);
             utils.hideOrShow($sheet, true, options, stacks.name);
         }
