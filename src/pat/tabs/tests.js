b77d1f9ef252df8d4a5fe80fcd9d0ffe888a7f3c
witek <witekdev@gmail.com>
2015-11-26 04:22:01 +0100

    add some tests for pat-tabs

--- src/pat/tabs/tests.js
+++ src/pat/tabs/tests.js
@@ -1,8 +1,8 @@
-define(["pat-tabs"], function(Pattern) {
+define(["pat-tabs"], function(pattern) {
 
     describe("pat-tabs", function() {
 
-        describe("TODO once this test is written", function() {
+        describe("When the size of all the tabs cannot fit in the pat-tabs div", function() {
             beforeEach(function() {
                 $("<div/>", {id: "lab"}).appendTo(document.body);
             });
@@ -10,8 +10,87 @@ define(["pat-tabs"], function(Pattern) {
                 $("#lab").remove();
             });
 
-            it("will test something", function() {
-                Pattern.init($(".pat-tabs"));
+            it("some tabs will be placed in the extra-tabs span, which is a child of the pat-tabs element", function() {
+                $("#lab").html([
+                    '<nav class="navigation tabs pat-tabs" style="width:400px;">',
+                    '<a href="" style="width:100px; display:block;">General</a>',
+                    '<a href="" style="width:100px; display:block;">Members</a>',
+                    '<a href="" style="width:100px; display:block;">Security</a>',
+                    '<a href="" style="width:100px; display:block;">Advanced</a>',
+                    '</nav>'
+                    ].join("\n"));
+                var $tabs = $(".pat-tabs");
+                pattern.init($tabs);
+                expect($tabs.find(".extra-tabs").length).toBeTruthy();
+            });
+        });
+
+        describe("When the size of all the tabs (padding included) cannot fit in the pat-tabs div", function() {
+            beforeEach(function() {
+                $("<div/>", {id: "lab"}).appendTo(document.body);
+            });
+            afterEach(function() {
+                $("#lab").remove();
+            });
+
+            it("some tabs will be placed in the extra-tabs span, which is a child of the pat-tabs element", function() {
+                $("#lab").html([
+                    '<nav class="navigation tabs pat-tabs" style="width:440px;">',
+                    '<a href="" style="width:100px; padding: 0px 5px 0px 5px; display:block;">General</a>',
+                    '<a href="" style="width:100px; padding: 0px 5px 0px 5px; display:block;">Members</a>',
+                    '<a href="" style="width:100px; padding: 0px 5px 0px 5px; display:block;">Security</a>',
+                    '<a href="" style="width:100px; padding: 0px 5px 0px 5px; display:block;">Advanced</a>',
+                    '</nav>'
+                    ].join("\n"));
+                var $tabs = $(".pat-tabs");
+                pattern.init($tabs);
+                expect($tabs.find(".extra-tabs").length).toBeTruthy();
+            });
+        });
+
+        describe("When the size of all the tabs can fit in the pat-tabs div", function() {
+            beforeEach(function() {
+                $("<div/>", {id: "lab"}).appendTo(document.body);
+            });
+            afterEach(function() {
+                $("#lab").remove();
+            });
+
+            it("the extra-tabs span will not exist as a child of the pat-tabs element", function() {
+                $("#lab").html([
+                    '<nav class="navigation tabs pat-tabs" style="width:401px;">',
+                    '<a href="" style="width:100px; display:block;">General</a>',
+                    '<a href="" style="width:100px; display:block;">Members</a>',
+                    '<a href="" style="width:100px; display:block;">Security</a>',
+                    '<a href="" style="width:100px; display:block;">Advanced</a>',
+                    '</nav>'
+                    ].join("\n"));
+                var $tabs = $(".pat-tabs");
+                pattern.init($tabs);
+                expect($tabs.find(".extra-tabs").length).toBeFalsy();
+            });
+        });
+
+        describe("When the size of all the tabs (padding included) can fit in the pat-tabs div", function() {
+            beforeEach(function() {
+                $("<div/>", {id: "lab"}).appendTo(document.body);
+            });
+            afterEach(function() {
+                $("#lab").remove();
+            });
+
+            it("the extra-tabs span will not exist as a child of the pat-tabs element", function() {
+                $("#lab").html([
+                    '<nav class="navigation tabs pat-tabs" style="width:441px;">',
+                    '<a href="" style="width:100px; padding: 0px 5px 0px 5px; display:block;">General</a>',
+                    '<a href="" style="width:100px; padding: 0px 5px 0px 5px; display:block;">Members</a>',
+                    '<a href="" style="width:100px; padding: 0px 5px 0px 5px; display:block;">Security</a>',
+                    '<a href="" style="width:100px; padding: 0px 5px 0px 5px; display:block;">Advanced</a>',
+                    '</nav>'
+                    ].join("\n"));
+                var $tabs = $(".pat-tabs");
+                pattern.init($tabs);
+                expect($tabs.find(".extra-tabs").length).toBeFalsy();
             });
         });
     });
