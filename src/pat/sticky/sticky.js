b7c4335806922eec7a0cd76b18c830541c90fb4a
Alexander Pilz <pilz@syslab.com>
2017-01-15 22:55:49 +0100

    added support for selectors, adding a class and listening to updates from other patterns. Refs #473

--- src/pat/sticky/sticky.js
+++ src/pat/sticky/sticky.js
@@ -4,18 +4,40 @@ define([
     "pat-parser",
     "pat-registry",
     "pat-base",
+    "pat-logger",
     "stickyfill",
-], function(_, Parser, registry, Base, Stickyfill) {
+], function(_, Parser, registry, Base, logger, Stickyfill) {
     "use strict";
     var parser = new Parser("sticky");
+    var log = logger.getLogger("sticky")
+    parser.addArgument("selector", "");
 
     return Base.extend({
         name: "sticky",
         trigger: ".pat-sticky",
         init: function() {
-            this.options = $.extend(this.options, parser.parse(this.$el));
-            this.$el.Stickyfill();
+            this.options = parser.parse(this.$el);
+            this.makeSticky();
+            this.$el.on('pat-update', this.onPatternUpdate.bind(this));
             return this.$el;
+        },
+        onPatternUpdate: function (ev, data) {
+            /* Handler which gets called when pat-update is triggered within
+             * the .pat-sticky element.
+             */
+            this.makeSticky();
+            return true;
+        },
+        makeSticky: function() {
+            if (this.options.selector === "") {
+                this.$stickies = this.$el;
+            } else {
+                this.$stickies = this.$el.children().filter(this.options.selector);
+            }
+            this.$stickies.each(function () {
+                $(this).Stickyfill();
+                $(this).addClass('sticking');
+            });
         }
     });
 });
