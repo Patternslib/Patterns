597a6ab74c53d2dc4acec107e1f3d0f46289f9f0
Alexander Pilz <pilz@syslab.com>
2017-08-31 14:01:34 +0200

    cleanup

--- webpack/karma.config.js
+++ webpack/karma.config.js
@@ -2,13 +2,13 @@
 // Generated on Thu Aug 31 2017 11:03:54 GMT+0200 (CEST)
 // See more on https://www.npmjs.com/package/webpack-karma-jasmine
 const path = require('path');
+var webpackOptions = require('./base.config.js');
 
 module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -17,7 +17,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'tests/specs/lib/*.js'
+      '../tests/specs/*/*.js'
       //'test_requires.js'
     ],
 
@@ -33,16 +33,10 @@ module.exports = function(config) {
       //use babel-loader from webpack to compile es2015 features in .js files
       //add webpack as preprocessor
       //'test_requires.js': ['webpack'],
-      'tests/specs/*/*.js': ['webpack', 'sourcemap'],
+      '../tests/specs/*/*.js': ['webpack', 'sourcemap'],
     },
 
-    webpack: {
-        resolve:{
-            alias:{
-                'pat-depends_parse': path.join(__dirname, 'src/lib/depends_parse.js'),
-            }
-        }
-    },
+    webpack: webpackOptions,
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
