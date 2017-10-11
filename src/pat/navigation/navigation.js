1bf9d8cc7334cb312203da718c2b68069160141f
ale-rt <alessandro.pisa@gmail.com>
2017-10-11 15:26:19 +0200

    Revert "Merge pull request #526 from Patternslib/pat-navigation-item-parent-selector"
    This reverts commit ef00a2d0c98acd16482264f89769c4b425457a64, reversing
    changes made to 6d4705eedd3cb6e139578e2a9e0b0b7e1f6e58cd.

--- src/pat/navigation/navigation.js
+++ src/pat/navigation/navigation.js
@@ -1,59 +1,82 @@
 define([
     "jquery",
-    "pat-parser",
     "pat-logger",
-    "pat-registry",
-    "pat-base"
-], function($, Parser, logger, registry, Base) {
-    var log = logger.getLogger("pat.navigation"),
-        parser = new Parser("navigation");
+    "pat-registry"
+], function($, logger, registry) {
+    var log = logger.getLogger("pat.navigation");
 
-    parser.addArgument("item-parent-selector", "li");
-
-    return Base.extend({
+    var _ = {
         name: "navigation",
         trigger: "nav, .navigation, .pat-navigation",
-        init: function($el, opts) {
-            var that = this;
-            this.options = parser.parse($el, opts);
-            this.anchors = this._findOwnAnchors().click(function(ev) {
-                that._markCurrent($(ev.target));
-            });
+        init: function($el) {
+            return $el.each(function() {
+                var $el = $(this);
+                var curpath = window.location.pathname;
+                log.debug("current path:", curpath);
 
-            if ($el.find(".current").length === 0) {
-                that._autosetCurrent.bind(that)();
-            }
-
-            if ($el.hasClass("navigation-load-current")) {
-                this._loadCurrent.bind(this)();
-            }
-        },
-        _findNavigation: function($el) {
-            if ($el.is(this.trigger)) {
-                // $el is already a nav element
-                return $el;
-            }
-            return $el.parents(this.trigger).first();
-        },
-        _findOwnAnchors: function($target) {
-            /*
-             * Find the anchors that are inside this navigation element
-             * but discard the one contained in a nested one
-             */
-            var that = this,
-                check = this.$el[0];
-            $target = $target || this.$el;
-            return $target.find(this.options.itemParentSelector + ' a').filter(
-                function(idx, anchor) {
-                    return that._findNavigation($(anchor))[0] === check;
+                // check whether to load
+                if ($el.hasClass("navigation-load-current")) {
+                    $el.find("a.current, .current a").click();
+                    // check for current elements injected here
+                    $el.on("patterns-injected-scanned", function(ev) {
+                        var $target = $(ev.target);
+                        if ($target.is("a.current"))
+                            $target.click();
+                        if ($target.is(".current"))
+                            $target.find("a").click();
+                        _._updatenavpath($el);
+                    });
                 }
-            )
+
+                // An anchor within this navigation triggered injection
+                $el.on("patterns-inject-triggered", "a", function(ev) {
+                    var $target = $(ev.target);
+                    // remove all set current classes
+                    $el.find(".current").removeClass("current");
+                    // set .current on target
+                    $target.addClass("current");
+                    // If target's parent is an LI, also set current there
+                    $target.parent("li").addClass("current");
+                    _._updatenavpath($el);
+                });
+
+                // set current class if it is not set
+                if ($el.find(".current").length === 0) {
+                    $el.find("li a").each(function() {
+                        var $a = $(this),
+                            $li = $a.parents("li:first"),
+                            url = $a.attr("href"),
+                            path;
+                        if (typeof url === "undefined") {
+                            return;
+                        }
+                        path = _._pathfromurl(url);
+                        log.debug("checking url:", url, "extracted path:", path);
+                        if (_._match(curpath, path)) {
+                            log.debug("found match", $li);
+                            $li.addClass("current");
+                        }
+                    });
+                }
+                _._updatenavpath($el);
+            });
         },
-        _loadCurrent: function() {
-            /*
-             * Simulate a click on the currently selected tab
-             */
-            this.anchors.filter("a.current, .current a").click();
+        _updatenavpath: function($el) {
+            $el.find(".navigation-in-path").removeClass("navigation-in-path");
+            $el.find("li:has(.current)").addClass("navigation-in-path");
+        },
+        _match: function(curpath, path) {
+            if (!path) {
+                log.debug("path empty");
+                return false;
+            }
+            // current path needs to end in the anchor's path
+            if (path !== curpath.slice(- path.length)) {
+                log.debug(curpath, "does not end in", path);
+                return false;
+            }
+            // XXX: we might need more exclusion tests
+            return true;
         },
         _pathfromurl: function(url) {
             var path = url.split("#")[0].split("://");
@@ -63,66 +86,10 @@ define([
             }
             if (path.length === 1) return path[0];
             return path[1].split("/").slice(1).join("/");
-        },
-        _match: function(curpath, path) {
-            if (!path) {
-                log.debug("path empty");
-                return false;
-            }
-            // current path needs to end in the anchor's path
-            if (path !== curpath.slice(-path.length)) {
-                log.debug(curpath, "does not end in", path);
-                return false;
-            }
-            // XXX: we might need more exclusion tests
-            return true;
-        },
-        _autosetCurrent: function() {
-            var that = this;
-            this.anchors.each(function() {
-                var $a, url, path, curpath;
-                $a = $(this);
-                url = $a.attr("href");
-                if (typeof url === "undefined") {
-                    return;
-                }
-                path = that._pathfromurl(url);
-                log.debug("checking url:", url, "extracted path:", path);
-                curpath = window.location.pathname
-                if (path.indexOf('#') !== -1) {
-                    curpath += '#' + window.location.hash;
-                }
-                if (that._match(window.location.pathname, path)) {
-                    that._markCurrent($a);
-                }
-            });
-        },
-        _markCurrent: function($el) {
-            var that = this;
-            // First unset the current class in all the anchors
-            this.anchors.each(function (idx, el) {
-                that._unmarkCurrent($(el));
-            });
-            // then set it to the proper element
-            $el.addClass("current");
-            if (this.options.itemParentSelector) {
-                $el.parents(this.options.itemParentSelector).first().addClass("current");
-            }
-            // finally update the navigation-in-path class
-            $el.find(".navigation-in-path").removeClass("navigation-in-path");
-            if (this.options.itemParentSelector) {
-                $el.find(this.options.itemParentSelector + ":has(.current)").addClass("navigation-in-path");
-            }
-            return $el;
-        },
-        _unmarkCurrent: function($el) {
-            $el.removeClass("current");
-            $el.find(".navigation-in-path").removeClass("navigation-in-path");
-            if (this.options.itemParentSelector) {
-                $el.parents(this.options.itemParentSelector).first().removeClass("current");
-            }
         }
-    });
+    };
+    registry.register(_);
+    return _;
 });
 
 // jshint indent: 4, browser: true, jquery: true, quotmark: double
