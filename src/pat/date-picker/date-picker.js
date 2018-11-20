Patternslib/Patterns

commit 9c98f5919c7b51cc41a5a1164773bd4af2cf8f20

pat-datetime-picker: Add new pattern for setting the date and time.

src/pat/date-picker/date-picker.js
@@ -7,12 +7,12 @@ define(
         'pat-base',
         'pikaday',
         'moment',
-        'moment-timezone-data',
         'modernizr'
     ],
-    function(_, Parser, registry, Base, Pikaday, moment, momenttimezone) {
+    function(_, Parser, registry, Base, Pikaday, moment) {
         var parser = new Parser('date-picker');
         parser.addArgument('behavior', 'styled', ['native', 'styled']);
+        parser.addArgument('format', 'YYYY-MM-DD');
         parser.addArgument('week-numbers', [], ['show', 'hide']);
         parser.addArgument('i18n'); // URL pointing to JSON resource with i18n values
         /* JSON format for i18n
@@ -28,7 +28,7 @@ define(
             name: 'date-picker',
             trigger: '.pat-date-picker',
             init: function() {
-                this.options = $.extend(this.options, parser.parse(this.$el));
+                this.options = $.extend(parser.parse(this.$el), this.options);
                 this.polyfill = this.options.behavior === 'native';
                 if (this.polyfill && Modernizr.inputtypes.date) {
                     return;
@@ -36,15 +36,15 @@ define(
                 if (this.$el.attr('type') === 'date') {
                     this.$el.attr('type', 'text');
                 }
+
                 var config = {
                     field: this.$el[0],
-                    minDate: this.$el.attr('min')
-                        ? moment(this.$el.attr('min')).toDate()
-                        : undefined,
-                    maxDate: this.$el.attr('max')
-                        ? moment(this.$el.attr('max')).toDate()
-                        : undefined,
+                    format: this.options.format,
                     showWeekNumber: this.options.weekNumbers === 'show',
+                    toString: function (date, format) {
+                        var date = moment(date).format(format);
+                        return date;
+                    },
                     onSelect: function() {
                         $(this._o.field)
                             .closest('form')
@@ -53,16 +53,29 @@ define(
                         $(this._o.field).trigger('input-change');
                     }
                 };
+
+                if (this.$el.attr('min')) {
+                    config.minDate = moment(this.$el.attr('min')).toDate();
+                }
+                if (this.$el.attr('max')) {
+                    config.maxDate = moment(this.$el.attr('max')).toDate();
+                }
+
                 if (this.options.i18n) {
                     $.getJSON(this.options.i18n, function(data) {
                         config.i18n = data;
-                        new Pikaday(config);
                     });
-                } else {
-                    new Pikaday(config);
                 }
+
+                new Pikaday(config);
                 return this.$el;
+            },
+
+            isodate: function() {
+                var now = new Date();
+                return now.toISOString().substr(0,10);
             }
+
         });
     }
 );

.gitignore
@@ -36,7 +36,6 @@ src/pat/checklist/checklist.css
 src/pat/clone/clone.css
 src/pat/collapsible/_collapsible.css
 src/pat/collapsible/collapsible.css
-src/pat/date-picker/date-picker.css
 src/pat/depends/depends.css
 src/pat/edit-tinymce/edit-tinymce.css
 src/pat/equaliser/equaliser.css

Makefile
@@ -18,7 +18,7 @@ JSHINTEXCEPTIONS = $(GENERATED) \
 		   $(TESTSOURCES)
 CHECKSOURCES	= $(filter-out $(JSHINTEXCEPTIONS),$(SOURCES))
 
-all:: bundle.js css
+all:: bundle.js
 
 ########################################################################
 ## Install dependencies
@@ -53,7 +53,7 @@ check:: stamp-npm jshint
 ########################################################################
 ## Builds
 
-build:: bundle all_css
+build:: bundle
 
 bundle bundle.js: $(GENERATED) $(SOURCES) stamp-npm
 	npm run build
@@ -79,6 +79,7 @@ all_css:: css
 	@$(SASS) -I . -I _sass src/pat/clone/_clone.scss src/pat/clone/clone.css
 	@$(SASS) -I . -I _sass src/pat/collapsible/_collapsible.scss src/pat/collapsible/collapsible.css
 	@$(SASS) -I . -I _sass src/pat/date-picker/_date-picker.scss src/pat/date-picker/date-picker.css
+	@$(SASS) -I . -I _sass src/pat/datetime-picker/_datetime-picker.scss src/pat/datetime-picker/datetime-picker.css
 	@$(SASS) -I . -I _sass src/pat/depends/_depends.scss src/pat/depends/depends.css
 	@$(SASS) -I . -I _sass src/pat/edit-tinymce/_edit-tinymce.scss src/pat/edit-tinymce/edit-tinymce.css
 	@$(SASS) -I . -I _sass src/pat/equaliser/_equaliser.scss src/pat/equaliser/equaliser.css

_sass/_patterns.scss
@@ -24,6 +24,7 @@
 @import "src/pat/checklist/checklist";
 @import "src/pat/collapsible/collapsible";
 @import "src/pat/date-picker/date-picker";
+@import "src/pat/datetime-picker/datetime-picker";
 @import "src/pat/depends/depends";
 @import "src/pat/edit-tinymce/edit-tinymce";
 @import "src/pat/equaliser/equaliser";

index.html
@@ -42,7 +42,8 @@
 			<a class="navigation auto-submit" href="/src/pat/auto-submit/index.html" title="Auto submit">Auto submit<sup>β</sup></a>
 			<a class="navigation auto-suggest" href="/src/pat/auto-suggest/index.html" title="Auto suggest">Auto suggest<sup>β</sup></a>
 			<a class="navigation focus" href="/src/pat/focus/index.html" title="">Focus<sup>stable</sup></a>
-			<a class="navigation date-picker" href="/src/pat/date-picker/index.html" title="">Date picker<sup>unknown</sup></a>
+			<a class="navigation date-picker" href="/src/pat/date-picker/index.html" title="">Date picker<sup>β</sup></a>
+			<a class="navigation datetime-picker" href="/src/pat/datetime-picker/index.html" title="">Date/Time picker<sup>β</sup></a>
 			<a class="navigation checked-flag" href="/src/pat/checked-flag/index.html" title="checkedflag">Checked flag<sup>unknown</sup></a>
 			<a class="navigation checkboxes" href="/src/pat/checklist/index.html" title="checklist">Checkboxes<sup>unknown</sup></a>
             <a class="navigation validate" href="/src/pat/validation/index.html">Validation<sup>β</sup></a>

main.js
@@ -1,0 +1,158 @@
+var config;
+if (typeof(require) === "undefined") {
+    /* XXX: Hack to work around r.js's stupid parsing.
+     * We want to save the configuration in a variable so that we can reuse it in
+     * tests/main.js.
+     */
+    require = { config: function (c) { config = c; } };
+}
+
+require.config({
+    baseUrl: "src",
+    paths: {
+        "ev-emitter":                       "bower_components/ev-emitter",
+        "get-size":                         "bower_components/get-size/get-size",
+        "google-code-prettify":             "bower_components/google-code-prettify/src/prettify",
+        "imagesloaded":                     "bower_components/imagesloaded/imagesloaded",
+        "jcrop":                            "bower_components/jcrop/js/jquery.Jcrop",
+        "jquery":                           "bower_components/jquery/dist/jquery",
+        "jquery.browser":                   "bower_components/jquery.browser/dist/jquery.browser",
+        "jquery.anythingslider":            "bower_components/AnythingSlider/js/jquery.anythingslider",
+        "jquery.chosen":                    "bower_components/chosen/chosen/chosen.jquery",
+        "jquery.form":                      "bower_components/jquery-form/jquery.form",
+        "jquery.fullcalendar":              "bower_components/fullcalendar/fullcalendar.min",
+        "jquery.fullcalendar.dnd":          "bower_components/fullcalendar/lib/jquery-ui.custom.min",
+        "jquery.placeholder":               "bower_components/jquery-placeholder/jquery.placeholder.min",
+        "jquery.textchange":                "bower_components/jquery-textchange/jquery.textchange",
+        "logging":                          "bower_components/logging/src/logging",
+        "masonry":                          "bower_components/masonry/dist/masonry.pkgd",
+        "desandro-matches-selector":        "bower_components/desandro-matches-selector/matches-selector",
+        "modernizr":                        "bower_components/modernizr/modernizr",
+        "modernizr-csspositionsticky":      "bower_components/modernizr/feature-detects/css-positionsticky",
+        "outlayer":                         "bower_components/outlayer/outlayer",
+        "parsley":                          "bower_components/parsleyjs/parsley",
+        "parsley.extend":                   "bower_components/parsleyjs/parsley.extend",
+        "patternslib.slides":               "bower_components/slides/src/slides",
+        "photoswipe":                       "bower_components/photoswipe/dist/photoswipe",
+        "photoswipe-ui":                    "bower_components/photoswipe/dist/photoswipe-ui-default",
+        "pikaday":                          "bower_components/pikaday/pikaday",
+        "prefixfree":                       "bower_components/prefixfree/prefixfree.min",
+        "select2":                          "bower_components/select2/select2.min",
+        "showdown":                         "bower_components/showdown/src/showdown",
+        "showdown-github":                  "bower_components/showdown/src/extensions/github",
+        "showdown-prettify":                "bower_components/showdown/src/extensions/prettify",
+        "showdown-table":                   "bower_components/showdown/src/extensions/table",
+        "spectrum":                         "bower_components/spectrum/spectrum",
+        "stickyfill":                       "bower_components/Stickyfill/src/stickyfill",
+        "text":                             "bower_components/requirejs-text/text",
+        "tinymce":                          "bower_components/jquery.tinymce/jscripts/tiny_mce/jquery.tinymce",
+        "underscore":                       "bower_components/underscore/underscore",
+        "validate":                         "bower_components/validate/validate",
+
+        // Calendar pattern
+        "moment": "bower_components/moment/moment",
+        "moment-timezone": "bower_components/moment-timezone/moment-timezone",
+        "moment-timezone-data": "pat/calendar/moment-timezone-with-data-2010-2020",
+
+        // Core
+        "pat-compat":               "core/compat",
+        "pat-base":                 "core/base",
+        "pat-date-picker":          "pat/date-picker/date-picker",
+        "pat-datetime-picker":      "pat/datetime-picker/datetime-picker",
+        "pat-depends_parse":        "lib/depends_parse",
+        "pat-dependshandler":       "lib/dependshandler",
+        "pat-htmlparser":           "lib/htmlparser",
+        "pat-input-change-events":  "lib/input-change-events",
+        "pat-jquery-ext":           "core/jquery-ext",
+        "pat-logger":               "core/logger",
+        "pat-parser":               "core/parser",
+        "pat-mockup-parser":        "core/mockup-parser",
+        "pat-registry":             "core/registry",
+        "pat-remove":               "core/remove",
+        "pat-store":                "core/store",
+        "pat-url":                  "core/url",
+        "pat-utils":                "core/utils",
+
+        // Patterns
+        "pat-ajax":                    "pat/ajax/ajax",
+        "pat-autofocus":               "pat/autofocus/autofocus",
+        "pat-autoscale":               "pat/auto-scale/auto-scale",
+        "pat-auto-scale":               "pat/auto-scale/auto-scale",
+        "pat-autosubmit":              "pat/auto-submit/auto-submit",
+        "pat-auto-submit":              "pat/auto-submit/auto-submit",
+        "pat-autosuggest":             "pat/auto-suggest/auto-suggest",
+        "pat-auto-suggest":             "pat/auto-suggest/auto-suggest",
+        "pat-breadcrumbs":             "pat/breadcrumbs/breadcrumbs",
+        "pat-bumper":                  "pat/bumper/bumper",
+        "pat-calendar":                "pat/calendar/calendar",
+        "pat-carousel":                "pat/carousel/carousel",
+        "pat-checkedflag":             "pat/checked-flag/checked-flag",
+        "pat-checked-flag":             "pat/checked-flag/checked-flag",
+        "pat-checklist":               "pat/checklist/checklist",
+        "pat-chosen":                  "pat/chosen/chosen",
+        "pat-clone":                   "pat/clone/clone",
+        "pat-collapsible":             "pat/collapsible/collapsible",
+        "pat-colour-picker":           "pat/colour-picker/colour-picker",
+        "pat-depends":                 "pat/depends/depends",
+        "pat-edit-tinymce":            "pat/edit-tinymce/edit-tinymce",
+        "pat-equaliser":               "pat/equaliser/equaliser",
+        "pat-expandable":              "pat/expandable-tree/expandable-tree",
+        "pat-expandable-tree":         "pat/expandable-tree/expandable-tree",
+        "pat-focus":                   "pat/focus/focus",
+        "pat-form-state":              "pat/form-state/form-state",
+        "pat-forward":                 "pat/forward/forward",
+        "pat-gallery":                 "pat/gallery/gallery",
+        "pat-gallery-url":             "pat/gallery",
+        "pat-grid":                    "pat/grid/grid",  // Hack, there's no grid jS, but we need for website bundler
+        "pat-syntax-highlight":        "pat/syntax-highlight/syntax-highlight",
+        "pat-image-crop":              "pat/image-crop/image-crop",
+        "pat-inject":                  "pat/inject/inject",
+        "pat-legend":                  "pat/legend/legend",
+        "pat-markdown":                "pat/markdown/markdown",
+        "pat-masonry":                 "pat/masonry/masonry",
+        "pat-menu":                    "pat/menu/menu",
+        "pat-modal":                   "pat/modal/modal",
+        "pat-navigation":              "pat/navigation/navigation",
+        "pat-notification":            "pat/notification/notification",
+        "pat-placeholder":             "pat/placeholder/placeholder",
+        "pat-selectbox":               "pat/selectbox/selectbox",
+        "pat-skeleton":                "pat/skeleton/skeleton",
+        "pat-slides":                  "pat/slides/slides",
+        "pat-slideshow-builder":       "pat/slideshow-builder/slideshow-builder",
+        "pat-sortable":                "pat/sortable/sortable",
+        "pat-stacks":                  "pat/stacks/stacks",
+        "pat-sticky":                  "pat/sticky/sticky",
+        "pat-subform":                 "pat/subform/subform",
+        "pat-switch":                  "pat/switch/switch",
+        "pat-scroll":                  "pat/scroll/scroll",
+        "pat-tabs":                    "pat/tabs/tabs",
+        "pat-toggle":                  "pat/toggle/toggle",
+        "pat-tooltip":                 "pat/tooltip/tooltip",
+        "pat-validation":              "pat/validation/validation",
+        "pat-zoom":                    "pat/zoom/zoom",
+        "patterns":                    "patterns"
+    },
+
+    shim: {
+        "jcrop":                        { deps: ["jquery"] },
+        "jquery":                       { exports: ["jQuery"] },
+        "jquery.anythingslider":        { deps: ["jquery"] },
+        "jquery.chosen":                { deps: ["jquery"] },
+        "jquery.fullcalendar.dnd":      { deps: ["jquery"] },
+        "jquery.placeholder":           { deps: ["jquery"] },
+        "jquery.textchange":            { deps: ["jquery"] },
+        "parsley":                      { deps: ["jquery"] },
+        "parsley.extend":               { deps: ["jquery"] },
+        "select2":                      { deps: ["jquery"] },
+        "spectrum":                     { deps: ["jquery"] },
+        "tinymce":                      { deps: ["jquery"] },
+        "showdown-github":              { deps: ["showdown"] },
+        "showdown-prettify":            { deps: ["showdown", "google-code-prettify"] },
+        "showdown-table":               { deps: ["showdown"] },
+        "modernizr-csspositionsticky":  { deps: ["modernizr"] }
+    },
+});
+
+if (typeof(require) === "function") {
+    require(["patterns"], function(patterns) {});
+}

src/pat/date-picker/_date-picker.scss
@@ -1,203 +1,2 @@
 @charset "UTF-8";
-
-/*!
- * Pikaday
- * Copyright © 2014 David Bushell | BSD & MIT license | http://dbushell.com/
- */
-
-.pika-single {
-    z-index: 9999;
-    display: block;
-    position: relative;
-    color: #333;
-    background: #fff;
-    border: 1px solid #ccc;
-    border-bottom-color: #bbb;
-    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-}
-
-/*
-clear child float (pika-lendar), using the famous micro clearfix hack
-http://nicolasgallagher.com/micro-clearfix-hack/
-*/
-.pika-single:before,
-.pika-single:after {
-    content: " ";
-    display: table;
-}
-.pika-single:after { clear: both }
-.pika-single { *zoom: 1 }
-
-.pika-single.is-hidden {
-    display: none;
-}
-
-.pika-single.is-bound {
-    position: absolute;
-    box-shadow: 0 5px 15px -5px rgba(0,0,0,.5);
-}
-
-.pika-lendar {
-    float: left;
-    width: 240px;
-    margin: 8px;
-}
-
-.pika-title {
-    position: relative;
-    text-align: center;
-}
-
-.pika-label {
-    display: inline-block;
-    *display: inline;
-    position: relative;
-    z-index: 9999;
-    overflow: hidden;
-    margin: 0;
-    padding: 5px 3px;
-    font-size: 14px;
-    line-height: 20px;
-    font-weight: bold;
-    background-color: #fff;
-}
-.pika-title select {
-    cursor: pointer;
-    position: absolute;
-    z-index: 9998;
-    margin: 0;
-    left: 0;
-    top: 5px;
-    filter: alpha(opacity=0);
-    opacity: 0;
-}
-
-.pika-prev,
-.pika-next {
-    display: block;
-    cursor: pointer;
-    position: relative;
-    outline: none;
-    border: 0;
-    padding: 0;
-    width: 20px;
-    height: 30px;
-    /* hide text using text-indent trick, using width value (it's enough) */
-    text-indent: 20px;
-    white-space: nowrap;
-    overflow: hidden;
-    background-color: transparent;
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-size: 75% 75%;
-    opacity: .5;
-    *position: absolute;
-    *top: 0;
-}
-
-.pika-prev:hover,
-.pika-next:hover {
-    opacity: 1;
-}
-
-.pika-prev,
-.is-rtl .pika-next {
-    float: left;
-    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAeCAYAAAAsEj5rAAAAUklEQVR42u3VMQoAIBADQf8Pgj+OD9hG2CtONJB2ymQkKe0HbwAP0xucDiQWARITIDEBEnMgMQ8S8+AqBIl6kKgHiXqQqAeJepBo/z38J/U0uAHlaBkBl9I4GwAAAABJRU5ErkJggg==');
-    *left: 0;
-}
-
-.pika-next,
-.is-rtl .pika-prev {
-    float: right;
-    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAeCAYAAAAsEj5rAAAAU0lEQVR42u3VOwoAMAgE0dwfAnNjU26bYkBCFGwfiL9VVWoO+BJ4Gf3gtsEKKoFBNTCoCAYVwaAiGNQGMUHMkjGbgjk2mIONuXo0nC8XnCf1JXgArVIZAQh5TKYAAAAASUVORK5CYII=');
-    *right: 0;
-}
-
-.pika-prev.is-disabled,
-.pika-next.is-disabled {
-    cursor: default;
-    opacity: .2;
-}
-
-.pika-select {
-    display: inline-block;
-    *display: inline;
-}
-
-.pika-table {
-    width: 100%;
-    border-collapse: collapse;
-    border-spacing: 0;
-    border: 0;
-}
-
-.pika-table th,
-.pika-table td {
-    width: 14.285714285714286%;
-    padding: 0;
-}
-
-.pika-table th {
-    color: #999;
-    font-size: 12px;
-    line-height: 25px;
-    font-weight: bold;
-    text-align: center;
-}
-
-.pika-button {
-    cursor: pointer;
-    display: block;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    outline: none;
-    border: 0;
-    margin: 0;
-    width: 100%;
-    padding: 5px;
-    color: #666;
-    font-size: 12px;
-    line-height: 15px;
-    text-align: right;
-    background: #f5f5f5;
-}
-
-.pika-week {
-    font-size: 11px;
-    color: #999;
-}
-
-.is-today .pika-button {
-    color: #33aaff;
-    font-weight: bold;
-}
-
-.is-selected .pika-button {
-    color: #fff;
-    font-weight: bold;
-    background: #33aaff;
-    box-shadow: inset 0 1px 3px #178fe5;
-    border-radius: 3px;
-}
-
-.is-disabled .pika-button {
-    pointer-events: none;
-    cursor: default;
-    color: #999;
-    opacity: .3;
-}
-
-.pika-button:hover {
-    color: #fff !important;
-    background: #ff8000 !important;
-    box-shadow: none !important;
-    border-radius: 3px !important;
-}
-
-/* styling for abbr */
-.pika-table abbr {
-    border-bottom: none;
-    cursor: help;
-}
-
+@import "../../bower_components/pikaday/scss/pikaday.scss";

src/pat/date-picker/date-picker.css
@@ -1,0 +1,182 @@
+@charset "UTF-8";
+/*!
+ * Pikaday
+ * Copyright © 2014 David Bushell | BSD & MIT license | http://dbushell.com/
+ */
+.pika-single {
+  z-index: 9999;
+  display: block;
+  position: relative;
+  color: #333;
+  background: #fff;
+  border: 1px solid #ccc;
+  border-bottom-color: #bbb;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; }
+  .pika-single.is-hidden {
+    display: none; }
+  .pika-single.is-bound {
+    position: absolute;
+    box-shadow: 0 5px 15px -5px rgba(0, 0, 0, 0.5); }
+
+.pika-single {
+  *zoom: 1; }
+  .pika-single:before, .pika-single:after {
+    content: " ";
+    display: table; }
+  .pika-single:after {
+    clear: both; }
+
+.pika-lendar {
+  float: left;
+  width: 240px;
+  margin: 8px; }
+
+.pika-title {
+  position: relative;
+  text-align: center; }
+  .pika-title select {
+    cursor: pointer;
+    position: absolute;
+    z-index: 9998;
+    margin: 0;
+    left: 0;
+    top: 5px;
+    filter: alpha(opacity=0);
+    opacity: 0; }
+
+.pika-label {
+  display: inline-block;
+  *display: inline;
+  position: relative;
+  z-index: 9999;
+  overflow: hidden;
+  margin: 0;
+  padding: 5px 3px;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: bold;
+  color: #333;
+  background-color: #fff; }
+
+.pika-prev,
+.pika-next {
+  display: block;
+  cursor: pointer;
+  position: relative;
+  outline: none;
+  border: 0;
+  padding: 0;
+  width: 20px;
+  height: 30px;
+  text-indent: 20px;
+  white-space: nowrap;
+  overflow: hidden;
+  background-color: transparent;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: 75% 75%;
+  opacity: .5;
+  *position: absolute;
+  *top: 0; }
+  .pika-prev:hover,
+  .pika-next:hover {
+    opacity: 1; }
+  .pika-prev.is-disabled,
+  .pika-next.is-disabled {
+    cursor: default;
+    opacity: .2; }
+
+.pika-prev,
+.is-rtl .pika-next {
+  float: left;
+  background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAeCAYAAAAsEj5rAAAAUklEQVR42u3VMQoAIBADQf8Pgj+OD9hG2CtONJB2ymQkKe0HbwAP0xucDiQWARITIDEBEnMgMQ8S8+AqBIl6kKgHiXqQqAeJepBo/z38J/U0uAHlaBkBl9I4GwAAAABJRU5ErkJggg==");
+  *left: 0; }
+
+.pika-next,
+.is-rtl .pika-prev {
+  float: right;
+  background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAeCAYAAAAsEj5rAAAAU0lEQVR42u3VOwoAMAgE0dwfAnNjU26bYkBCFGwfiL9VVWoO+BJ4Gf3gtsEKKoFBNTCoCAYVwaAiGNQGMUHMkjGbgjk2mIONuXo0nC8XnCf1JXgArVIZAQh5TKYAAAAASUVORK5CYII=");
+  *right: 0; }
+
+.pika-select {
+  display: inline-block;
+  *display: inline; }
+
+.pika-table {
+  width: 100%;
+  border-collapse: collapse;
+  border-spacing: 0;
+  border: 0; }
+  .pika-table th,
+  .pika-table td {
+    width: 14.285714285714286%;
+    padding: 0; }
+  .pika-table th {
+    color: #999;
+    font-size: 12px;
+    line-height: 25px;
+    font-weight: bold;
+    text-align: center; }
+  .pika-table abbr {
+    border-bottom: none;
+    cursor: help; }
+
+.pika-button {
+  cursor: pointer;
+  display: block;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  outline: none;
+  border: 0;
+  margin: 0;
+  width: 100%;
+  padding: 5px;
+  color: #666;
+  font-size: 12px;
+  line-height: 15px;
+  text-align: right;
+  background: #f5f5f5; }
+  .is-today .pika-button {
+    color: #33aaff;
+    font-weight: bold; }
+  .is-selected .pika-button {
+    color: #fff;
+    font-weight: bold;
+    background: #33aaff;
+    box-shadow: inset 0 1px 3px #178fe5;
+    border-radius: 3px; }
+  .is-disabled .pika-button, .is-outside-current-month .pika-button {
+    color: #999;
+    opacity: .3; }
+  .is-disabled .pika-button {
+    pointer-events: none;
+    cursor: default; }
+  .pika-button:hover {
+    color: #fff;
+    background: #ff8000;
+    box-shadow: none;
+    border-radius: 3px; }
+  .pika-button .is-selection-disabled {
+    pointer-events: none;
+    cursor: default; }
+
+.pika-week {
+  font-size: 11px;
+  color: #999; }
+
+.is-inrange .pika-button {
+  background: #D5E9F7; }
+
+.is-startrange .pika-button {
+  color: #fff;
+  background: #6CB31D;
+  box-shadow: none;
+  border-radius: 3px; }
+
+.is-endrange .pika-button {
+  color: #fff;
+  background: #33aaff;
+  box-shadow: none;
+  border-radius: 3px; }
+
+/*# sourceMappingURL=date-picker.css.map */

commit 4e3f4484184e28ac1ce52b3a309000219e623891

Fixes issue #629
Commit 9c98f5919c7b51cc41a5a1164773bd4af2cf8f20 introduced this bug, because `getJSON` is asynchronous.  This commit reverts a few line changes to handle this case properly.

src/pat/date-picker/date-picker.js
@@ -64,10 +64,12 @@ define(
                 if (this.options.i18n) {
                     $.getJSON(this.options.i18n, function(data) {
                         config.i18n = data;
+                        new Pikaday(config);
                     });
+                } else {
+                    new Pikaday(config);
                 }
 
-                new Pikaday(config);
                 return this.$el;
             },
 