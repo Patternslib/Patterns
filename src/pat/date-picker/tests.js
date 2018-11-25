1921085370fd40634236176ad4e0852b2adde464
Fulvio Casali <fulviocasali@gmail.com>
2018-11-24 18:06:18 -0800

    Add two test cases for i18n, one positive, one negative.

--- src/pat/date-picker/tests.js
+++ src/pat/date-picker/tests.js
@@ -68,5 +68,84 @@ define(['pat-date-picker'], function(pattern) {
 
         });
 
+        describe('Date picker with i18n', function() {
+            var i18n;
+            var url;
+
+            describe('with proper json URL', function() {
+                var deferred;
+
+                beforeEach(function(done) {
+                    url = '/src/pat/date-picker/i18n.json';
+                    i18n = undefined;
+
+                    deferred = new $.Deferred();
+                    $.getJSON(url)
+                    .done(function(data) {
+                        i18n = data;
+                    })
+                    .always(function() {
+                        done();
+                    });
+                });
+
+                it('properly localizes the months and weekdays', function() {
+                    var $pika = $('<input type="date" class="pat-date-picker" value="2018-10-21" data-pat-date-picker="i18n: ' + url + '" />').appendTo(document.body);
+                    spyOn($, "ajax").and.returnValue(deferred);
+                    pattern.init($pika);
+                    deferred.resolve(i18n);
+                    $pika.click();
+
+                    var ajaxargs = $.ajax.calls.mostRecent().args[0];
+                    expect(ajaxargs.url).toEqual(url);
+                    expect(i18n).toBeDefined();
+                    expect(
+                        document.querySelector('.pika-lendar .pika-select-month option[selected="selected"]').textContent
+                    ).toBe('Oktober');
+                });
+
+            });
+
+            describe('with bogus json URL', function() {
+                var deferred;
+                var errjqxhr;
+                var errstatus;
+                var errthrown;
+
+                beforeEach(function(done) {
+                    url = 'this404isExpected';
+                    i18n = undefined;
+                    deferred = new $.Deferred();
+
+                    $.getJSON(url)
+                    .done(function(data) {
+                        i18n = data;
+                    })
+                    .fail(function(jqxhr, status, errorThrown) {
+                        errjqxhr = jqxhr;
+                        errstatus = status;
+                        errthrown = errorThrown;
+                        done();
+                    });
+                });
+
+                it('falls back to default (english) month and weekday labels ', function() {
+                    var $pika = $('<input type="date" class="pat-date-picker" value="2018-10-21" data-pat-date-picker="i18n: ' + url + '" />').appendTo(document.body);
+                    spyOn($, "ajax").and.callThrough().and.returnValue(deferred);
+                    pattern.init($pika);
+                    deferred.reject();
+                    $pika.click();
+
+                    var ajaxargs = $.ajax.calls.mostRecent().args[0];
+                    expect(ajaxargs.url).toEqual(url);
+                    expect(i18n).toBeUndefined();
+                    expect(errstatus).toEqual('error');
+                    expect(errthrown).toEqual('Not Found');
+                    expect(
+                        document.querySelector('.pika-lendar .pika-select-month option[selected="selected"]').textContent
+                    ).toBe('October');
+                });
+            });
+        });
     });
 });
