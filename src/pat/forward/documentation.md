ef67368a51343fcc068919b87dbe9e84c474e31b
ale-rt <alessandro.pisa@gmail.com>
2019-03-13 15:25:43 +0100

    pat-forward: understand the trigger auto option

--- src/pat/forward/documentation.md
+++ src/pat/forward/documentation.md
@@ -10,8 +10,17 @@ This pattern can be used to redirect a click on an element to one or more other 
       <submit id="submit">Submit</submit>
     </form>
 
+If the trigger parameter is set to `auto`, the click event will be triggered
+immediately after the page is loaded.
+
+    <a href="#" class="pat-forward" data-pat-forward="selector: #submit; trigger: auto">Autosubmit form</a>
+    <form>
+      <submit id="submit">Submit</submit>
+    </form>
+
 ### Option reference
 
 | Property | Description | Default | Allowed Values | Type |
 |------|------|-----|------|
-| selector | The element to which the click event should be forwarded. | | | CSS Selector |
+| `selector` | The element to which the click event should be forwarded. | | | CSS Selector |
+| `trigger` | When the forward action should be fired | `click` | `click|auto` | One of the mutually exclusive string values |
--- src/pat/forward/forward.js
+++ src/pat/forward/forward.js
@@ -11,20 +11,24 @@ define([
     var parser = new Parser("forward");
 
     parser.addArgument("selector");
+    parser.addArgument("trigger", "click", ["click", "auto"]);
 
     var _ = {
         name: "forward",
         trigger: ".pat-forward",
 
         init: function($el, opts) {
-            return $el.each(function() {
+            return $el.each(function () {
                 var $el = $(this),
-                    options = parser.parse($el, opts);
+                options = parser.parse($el, opts);
 
                 if (!options.selector)
-                    return;
+                return;
 
                 $el.on("click", null, options.selector, _._onClick);
+                if (options.trigger === "auto") {
+                    $el.trigger("click");
+                }
             });
         },
 
@@ -39,4 +43,3 @@ define([
 });
 
 // vim: sw=4 expandtab
-
