6f433bedf53d965b1d15ea72329ca67c893ffe1d
Fulvio Casali <fulviocasali@gmail.com>
2019-08-12 20:45:56 +0200

    cleanup

--- webpack/karma.config.js
+++ webpack/karma.config.js
@@ -18,10 +18,8 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-            //'tests/specs/*/*.js',
-            'tests/specs/lib/tippy.js',
-//            'src/pat/*/tests.js',
-            'src/pat/tooltip-ng/tests.js',
+            'tests/specs/*/*.js',
+            'src/pat/*/tests.js',
             {
                 pattern: 'src/pat/date-picker/i18n.json',
                 served: true,
