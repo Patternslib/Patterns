d4c3e7d0bf072c666ba3026e3b15ed53ae646bc2
Johannes Raggam <thetetet@gmail.com>
2019-09-26 20:49:07 +0200

    ES6 syntax

--- src/pat/fullscreen/fullscreen.js
+++ src/pat/fullscreen/fullscreen.js
@@ -3,9 +3,9 @@ define([
     "pat-parser",
     "pat-logger",
     "screenful"
-], function(Base, Parser, logging, screenful) {
-    var log = logging.getLogger("fullscreen");
-    var parser = new Parser('fullscreen');
+], (Base, Parser, logging, screenful) => {
+    const log = logging.getLogger("fullscreen");
+    const parser = new Parser('fullscreen');
 
     parser.addArgument('selector', null); // Selector for the fullscreen element.
     parser.addArgument('close-button', 'none', ['none', 'show']); // Inject a fullscreen button.
@@ -14,38 +14,38 @@ define([
         name: "fullscreen",
         trigger: ".pat-fullscreen",
 
-        init: function($el, opts) {
+        init($el, opts) {
             this.options = parser.parse(this.$el, opts);
 
             // setting up the exit button
-            var exit_el = null;
+            let exit_el = null;
             if (this.options.closeButton === 'show') {
-                var exit_el = document.createElement('button');
+                exit_el = document.createElement('button');
                 exit_el.className = 'pat-fullscreen-close-fullscreen';
                 exit_el.title = 'Exit fullscreen';
                 exit_el.appendChild(document.createTextNode('Exit fullscreen'));
-                exit_el.addEventListener('click', function (e) {
+                exit_el.addEventListener('click', e => {
                     e.preventDefault();
                     screenful.exit();
                 });
             }
 
-            var el = this.$el[0];
-            this.$el.on('click', function (e) {
+            const el = this.$el[0];
+            this.$el.on('click', e => {
             //el.addEventListener('click', function (e) {  // TODO: doesn't work in karma for href links
                 e.preventDefault();
                 // querying the fullscreen element fs_el and inside the event
                 // handler instead of outside allows for dynamic injecting
                 // fullscreen elements even after pattern initialization.
-                var fs_el_sel = this.options.selector ? this.options.selector : el.getAttribute('href');
+                let fs_el_sel = this.options.selector ? this.options.selector : el.getAttribute('href');
                 fs_el_sel = fs_el_sel ? fs_el_sel : 'body';
-                var fs_el = document.querySelector(fs_el_sel);
+                const fs_el = document.querySelector(fs_el_sel);
                 if (fs_el) {
                     // setting page to fullscreen
                     screenful.request(fs_el);
                     if (this.options.closeButton === 'show') {
                         fs_el.appendChild(exit_el);
-                        screenful.on('change', function (event) {
+                        screenful.on('change', () => {
                             // Removing exit button.
                             // The button is also removed when pressing the <ESC> button.
                             if (!screenful.isFullscreen) {
@@ -56,7 +56,7 @@ define([
                 } else {
                     log.error('No fullscreen element found.');
                 }
-            }.bind(this));
+            });
         }
     });
 });
