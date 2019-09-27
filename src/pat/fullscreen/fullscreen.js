b08727715463d0764f0ed1a12d51c22132d9e3bc
Johannes Raggam <thetetet@gmail.com>
2019-09-27 10:55:42 +0200

    fullscreen: seperate event callbacks into dedicated methods

--- src/pat/fullscreen/fullscreen.js
+++ src/pat/fullscreen/fullscreen.js
@@ -17,6 +17,32 @@ define([
         init($el, opts) {
             this.options = parser.parse(this.$el, opts);
 
+            const el = this.$el[0];
+            this.$el.on('click', this.fullscreen_handler.bind(this));
+            //el.addEventListener('click', function (e) {  // TODO: doesn't work in karma for href links
+        },
+
+        fullscreen_handler(event) {
+            event.preventDefault();
+            const el = this.$el[0];
+            // querying the fullscreen element fs_el and inside the event
+            // handler instead of outside allows for dynamic injecting
+            // fullscreen elements even after pattern initialization.
+            let fs_el_sel = this.options.selector ? this.options.selector : el.getAttribute('href');
+            fs_el_sel = fs_el_sel ? fs_el_sel : 'body';
+            const fs_el = document.querySelector(fs_el_sel);
+            if (fs_el) {
+                // setting page to fullscreen
+                screenful.request(fs_el);
+                if (this.options.closeButton === 'show') {
+                    this.setup_exit_button(fs_el);
+                }
+            } else {
+                log.error('No fullscreen element found.');
+            }
+        },
+
+        setup_exit_button(fs_el) {
             // setting up the exit button
             let exit_el = null;
             if (this.options.closeButton === 'show') {
@@ -24,40 +50,21 @@ define([
                 exit_el.className = 'pat-fullscreen-close-fullscreen';
                 exit_el.title = 'Exit fullscreen';
                 exit_el.appendChild(document.createTextNode('Exit fullscreen'));
-                exit_el.addEventListener('click', e => {
+                exit_el.addEventListener('click', (e) => {
                     e.preventDefault();
                     screenful.exit();
                 });
             }
-
-            const el = this.$el[0];
-            this.$el.on('click', e => {
-            //el.addEventListener('click', function (e) {  // TODO: doesn't work in karma for href links
-                e.preventDefault();
-                // querying the fullscreen element fs_el and inside the event
-                // handler instead of outside allows for dynamic injecting
-                // fullscreen elements even after pattern initialization.
-                let fs_el_sel = this.options.selector ? this.options.selector : el.getAttribute('href');
-                fs_el_sel = fs_el_sel ? fs_el_sel : 'body';
-                const fs_el = document.querySelector(fs_el_sel);
-                if (fs_el) {
-                    // setting page to fullscreen
-                    screenful.request(fs_el);
-                    if (this.options.closeButton === 'show') {
-                        fs_el.appendChild(exit_el);
-                        screenful.on('change', () => {
-                            // Removing exit button.
-                            // The button is also removed when pressing the <ESC> button.
-                            if (!screenful.isFullscreen) {
-                                fs_el.removeChild(exit_el);
-                            }
-                        });
-                    }
-                } else {
-                    log.error('No fullscreen element found.');
+            fs_el.appendChild(exit_el);
+            screenful.on('change', () => {
+                // Removing exit button.
+                // The button is also removed when pressing the <ESC> button.
+                if (!screenful.isFullscreen) {
+                    fs_el.removeChild(exit_el);
                 }
             });
         }
+
     });
 });
 
