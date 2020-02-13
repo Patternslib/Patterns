1152a986e7cc83389599112eb612b8c765ea1341
Johannes Raggam <thetetet@gmail.com>
2020-02-13 17:04:30 +0100

    add new timeout function. used for waiting in tests.

--- src/core/utils.js
+++ src/core/utils.js
@@ -542,6 +542,10 @@ function checkInputSupport(type, invalid_value) {
     return support;
 }
 
+const timeout = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 var utils = {
     // pattern pimping - own module?
     jqueryPlugin: jqueryPlugin,
@@ -562,7 +566,8 @@ var utils = {
     parseTime: parseTime,
     findRelatives: findRelatives,
     getCSSValue: getCSSValue,
-    checkInputSupport: checkInputSupport
+    checkInputSupport: checkInputSupport,
+    timeout: timeout
 };
 
 export default utils;
