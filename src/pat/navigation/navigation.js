1112bba6c4e1d3eafd7f0608e7302d6078760cd3
Johannes Raggam <thetetet@gmail.com>
2020-03-12 16:14:42 +0100

    no need for each here.

--- src/pat/navigation/navigation.js
+++ src/pat/navigation/navigation.js
@@ -9,57 +9,54 @@ define([
         name: "navigation",
         trigger: "nav, .navigation, .pat-navigation",
         init: function($el) {
-            return $el.each(function() {
-                var $el = $(this);
-                var curpath = window.location.pathname;
-                log.debug("current path:", curpath);
+            var curpath = window.location.pathname;
+            log.debug("current path:", curpath);
 
-                // check whether to load
-                if ($el.hasClass("navigation-load-current")) {
-                    $el.find("a.current, .current a").click();
-                    // check for current elements injected here
-                    $el.on("patterns-injected-scanned", function(ev) {
-                        var $target = $(ev.target);
-                        if ($target.is("a.current"))
-                            $target.click();
-                        if ($target.is(".current"))
-                            $target.find("a").click();
-                        _._updatenavpath($el);
-                    });
-                }
-
-                // An anchor within this navigation triggered injection
-                $el.on("patterns-inject-triggered", "a", function(ev) {
+            // check whether to load
+            if ($el.hasClass("navigation-load-current")) {
+                $el.find("a.current, .current a").click();
+                // check for current elements injected here
+                $el.on("patterns-injected-scanned", function(ev) {
                     var $target = $(ev.target);
-                    // remove all set current classes
-                    $el.find(".current").removeClass("current");
-                    // set .current on target
-                    $target.addClass("current");
-                    // If target's parent is an LI, also set current there
-                    $target.parent("li").addClass("current");
+                    if ($target.is("a.current"))
+                        $target.click();
+                    if ($target.is(".current"))
+                        $target.find("a").click();
                     _._updatenavpath($el);
                 });
+            }
 
-                // set current class if it is not set
-                if ($el.find(".current").length === 0) {
-                    $el.find("li a").each(function() {
-                        var $a = $(this),
-                            $li = $a.parents("li:first"),
-                            url = $a.attr("href"),
-                            path;
-                        if (typeof url === "undefined") {
-                            return;
-                        }
-                        path = _._pathfromurl(url);
-                        log.debug("checking url:", url, "extracted path:", path);
-                        if (_._match(curpath, path)) {
-                            log.debug("found match", $li);
-                            $li.addClass("current");
-                        }
-                    });
-                }
+            // An anchor within this navigation triggered injection
+            $el.on("patterns-inject-triggered", "a", function(ev) {
+                var $target = $(ev.target);
+                // remove all set current classes
+                $el.find(".current").removeClass("current");
+                // set .current on target
+                $target.addClass("current");
+                // If target's parent is an LI, also set current there
+                $target.parent("li").addClass("current");
                 _._updatenavpath($el);
             });
+
+            // set current class if it is not set
+            if ($el.find(".current").length === 0) {
+                $el.find("li a").each(function() {
+                    var $a = $(this),
+                        $li = $a.parents("li:first"),
+                        url = $a.attr("href"),
+                        path;
+                    if (typeof url === "undefined") {
+                        return;
+                    }
+                    path = _._pathfromurl(url);
+                    log.debug("checking url:", url, "extracted path:", path);
+                    if (_._match(curpath, path)) {
+                        log.debug("found match", $li);
+                        $li.addClass("current");
+                    }
+                });
+            }
+            _._updatenavpath($el);
         },
         _updatenavpath: function($el) {
             $el.find(".navigation-in-path").removeClass("navigation-in-path");
