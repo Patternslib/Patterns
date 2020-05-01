74ca713f9571575b107e9573a732d6d209abbd53
Johannes Raggam <thetetet@gmail.com>
2020-05-01 11:23:28 +0200

    Fix pat-subform tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,9 +4,14 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 
+// pat-fullscreen
 document.requestFullscreen = jest.fn();
 document.exitFullscreen = jest.fn();
 document.fullscreenElement = jest.fn();
 document.fullscreenEnabled = jest.fn();
 document.fullscreenchange = jest.fn();
 document.fullscreenerror = jest.fn();
+
+// pat-subform
+// See https://github.com/jsdom/jsdom/issues/1937#issuecomment-461810980
+window.HTMLFormElement.prototype.submit = () => {};
