3a3b43eb93f96317d803e9e5a44c68b285df57a9
Johannes Raggam <thetetet@gmail.com>
2020-05-01 11:29:10 +0200

    fix pat-auto-suggest tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import jquery from "jquery";
+
+window.jQuery = jquery;
 
 // pat-fullscreen
 document.requestFullscreen = jest.fn();
