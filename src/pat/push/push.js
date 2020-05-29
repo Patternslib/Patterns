a9bff4da40e639f8d82c9490e3c3c961d88de0c3
Johannes Raggam <thetetet@gmail.com>
2020-05-29 14:44:57 +0200

    es6ify pat-push

--- src/pat/push/push.js
+++ src/pat/push/push.js
@@ -1,43 +1,41 @@
-define(
-  ["jquery", "pat-base", "pat-parser", "pat-logger"],
-  function ($, Base, Parser, logging) {
+import $ from "jquery";
+import Base from "../../core/base";
+import logging from "../../core/logging";
+import Parser from "../../core/parser";
 
-  var logger = logging.getLogger("push");
-  var parser = new Parser("push");
+const logger = logging.getLogger("push");
+const parser = new Parser("push");
+parser.addArgument("url", null);
+parser.addArgument("push-id", null);
 
-  parser.addArgument("url", null);
-  parser.addArgument("push-id", null);
 
-  return Base.extend({
-    name: "push",
-    trigger: ".pat-push",
+export default Base.extend({
+  name: "push",
+  trigger: ".pat-push",
 
-    init: function ($el, opts) {
-      this.options = parser.parse($el, opts);
-      $(document.body).on("push", function (e, data) {
-        logger.debug('received push marker');
-        if (data === this.options.pushId) {
-          this.perform_inject();
-        }
-      }.bind(this));
-    },
+  init: function ($el, opts) {
+    this.options = parser.parse($el, opts);
+    $(document.body).on("push", function (e, data) {
+      logger.debug('received push marker');
+      if (data === this.options.pushId) {
+        this.perform_inject();
+      }
+    }.bind(this));
+  },
 
-    perform_inject: function() {
-      let el = this.$el[0];
-      $.ajax({
-        cache: false,
-        dataType: "html",
-        url: this.options.url,
+  perform_inject: function() {
+    let el = this.$el[0];
+    $.ajax({
+      cache: false,
+      dataType: "html",
+      url: this.options.url,
+    })
+      .done((data) => {
+        el.innerHTML = data;
       })
-        .done((data) => {
-          el.innerHTML = data;
-        })
-        .fail(() => {
-          logger.error("Could not fetch from " + this.options.url + " on push-id " + this.options.pushId);
-        });
-    },
+      .fail(() => {
+        logger.error("Could not fetch from " + this.options.url + " on push-id " + this.options.pushId);
+      });
+  },
 
-  });
 });
-
-
