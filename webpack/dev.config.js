c8c39ddfc3058092cb7df45e6ffd13a0363544a7
Johannes Raggam <thetetet@gmail.com>
2020-06-21 22:30:59 +0200

    Use env in webpack config

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,15 +3,17 @@ const BundleVisualizer = require('webpack-visualizer-plugin');
 const merge = require('webpack-merge');
 
 
-module.exports = merge(baseConfig, {
-    mode: 'development',
-    devServer: {
-    	inline: true,
-        contentBase: './',
-        port: '3001',
-        host: '0.0.0.0'
-    },
-    plugins: [
-        new BundleVisualizer(),
-    ]
-});
+module.exports = env => {
+    return merge(baseConfig(env), {
+        mode: 'development',
+        devServer: {
+            inline: true,
+            contentBase: './',
+            port: '3001',
+            host: '0.0.0.0'
+        },
+        plugins: [
+            new BundleVisualizer(),
+        ]
+    });
+};
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -2,12 +2,15 @@ const merge = require('webpack-merge');
 const baseConfig = require('./base.config.js');
 const path = require('path');
 
-module.exports = merge(baseConfig, {
-  mode: 'production',
-  entry: {
-    "bundle": path.resolve(__dirname, "../src/patterns.js"),
-    "bundle.min": path.resolve(__dirname, "../src/patterns.js"),
-    "bundle-polyfills": path.resolve(__dirname, "../src/polyfills.js"),
-    "bundle-polyfills.min": path.resolve(__dirname, "../src/polyfills.js"),
-  }
-});
+
+module.exports = env => {
+    return merge(baseConfig(env), {
+      mode: 'production',
+      entry: {
+        "bundle": path.resolve(__dirname, "../src/patterns.js"),
+        "bundle.min": path.resolve(__dirname, "../src/patterns.js"),
+        "bundle-polyfills": path.resolve(__dirname, "../src/polyfills.js"),
+        "bundle-polyfills.min": path.resolve(__dirname, "../src/polyfills.js"),
+      }
+    });
+};
