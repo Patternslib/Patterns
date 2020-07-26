ac9d51addb9c8d2418da0d09d1e7208783d5e6ad
Johannes Raggam <thetetet@gmail.com>
2020-07-26 15:25:36 +0200

    pat forward

--- src/pat/forward/forward.js
+++ src/pat/forward/forward.js
@@ -1,45 +1,41 @@
 /**
- * Patterns forward - Forward click events
- *
- * Copyright 2013 Simplon B.V. - Wichert Akkerman
- */
-define([
-    "jquery",
-    "pat-parser",
-    "pat-registry"
-], function($, Parser, registry) {
-    var parser = new Parser("forward");
+* Patterns forward - Forward click events
+*
+* Copyright 2013 Simplon B.V. - Wichert Akkerman
+*/
 
-    parser.addArgument("selector");
-    parser.addArgument("trigger", "click", ["click", "auto"]);
+import $ from "jquery";
+import Parser from "../../core/parser";
+import registry from "../../core/registry";
 
-    var _ = {
-        name: "forward",
-        trigger: ".pat-forward",
+var parser = new Parser("forward");
 
-        init: function($el, opts) {
-            return $el.each(function () {
-                var $el = $(this),
+parser.addArgument("selector");
+parser.addArgument("trigger", "click", ["click", "auto"]);
+
+var _ = {
+    name: "forward",
+    trigger: ".pat-forward",
+
+    init: function($el, opts) {
+        return $el.each(function() {
+            var $el = $(this),
                 options = parser.parse($el, opts);
 
-                if (!options.selector)
-                return;
+            if (!options.selector) return;
 
-                $el.on("click", null, options.selector, _._onClick);
-                if (options.trigger === "auto") {
-                    $el.trigger("click");
-                }
-            });
-        },
+            $el.on("click", null, options.selector, _._onClick);
+            if (options.trigger === "auto") {
+                $el.trigger("click");
+            }
+        });
+    },
 
-        _onClick: function(event) {
-            $(event.data).click();
-            event.preventDefault();
-            event.stopPropagation();
-        }
-    };
-    registry.register(_);
-    return _;
-});
-
-// vim: sw=4 expandtab
+    _onClick: function(event) {
+        $(event.data).click();
+        event.preventDefault();
+        event.stopPropagation();
+    }
+};
+registry.register(_);
+export default _;
