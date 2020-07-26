8177a0c5267a39687d1b8d37697a2a8012f01b44
Johannes Raggam <thetetet@gmail.com>
2020-07-26 15:28:31 +0200

    pat subform

--- src/pat/subform/subform.js
+++ src/pat/subform/subform.js
@@ -3,123 +3,120 @@
  *
  * Copyright 2013 Marko Durkovic
  */
-define([
-    "jquery",
-    "pat-registry",
-    "pat-logger",
-    "pat-parser",
-    "pat-ajax",
-    "pat-inject"
-], function($, registry, logging, Parser, ajax, inject) {
-    var log = logging.getLogger("subform");
+import $ from "jquery";
+import ajax from "../ajax/ajax";
+import Base from "../../core/base";
+import inject from "../inject/inject";
+import logging from "../../core/logging";
 
-    var _ = {
-        name: "subform",
-        trigger: ".pat-subform",
+var log = logging.getLogger("subform");
 
-        init: function($el) {
-            return $el.each(function() {
-                var $el = $(this);
-                $el.submit(_.submit);
-                $el.find('input').on('keyup keypress keydown', _.keyboard_handler);
-                $el.find("button[type=submit]").on("click", _.submitClicked);
-                return $el;
+export default Base.extend({
+    name: "subform",
+    trigger: ".pat-subform",
+
+    init($el) {
+        $el.submit(this.submit.bind(this));
+        $el.find("input").on(
+            "keyup keypress keydown",
+            this.keyboard_handler.bind(this)
+        );
+        $el.find("button[type=submit]").on(
+            "click",
+            this.submitClicked.bind(this)
+        );
+    },
+
+    destroy($el) {
+        $el.off("submit");
+    },
+
+    scopedSubmit($el) {
+        var $form = $el.parents("form"),
+            $exclude = $form.find(":input").filter(function () {
+                return !$(this).is($el.find("*"));
             });
-        },
-
-        destroy: function($el) {
-            $el.off("submit");
-        },
-
-        scopedSubmit: function($el) {
-            var $form = $el.parents("form"),
-                $exclude = $form
-                            .find(":input")
-                            .filter(function() {
-                                return !$(this).is($el.find("*"));
-                            });
-            // make other controls "unsuccessful"
-            log.debug("Hiding unwanted elements from submission.");
-            var names = $exclude.map(function() {
-                var name = $(this).attr("name");
-                return name ? name : 0;
-            });
-            $exclude.each(function() {
-                $(this).attr("name", "");
-            });
-            if ($el.is(".pat-inject") || $el.is(".pat-modal")) {
-                inject.submitSubform($el);
-            } else {
-                // use the native handler, since there could be event handlers
-                // redirecting to inject/ajax.
-                $form[0].submit();
-            }
-            // reenable everything
-            log.debug("Restoring previous state.");
-            $exclude.each(function(i) {
-                if (names[i]) {
-                    $(this).attr("name", names[i]);
-                }
-            });
-        },
-
-        submit: function(ev) {
-            ev.stopPropagation();
-
-            var $this = $(this),
-                $button = $this.find("button[type=submit][formaction]").first();
-            if ($button.length) {
-                $button.trigger("click");
-            } else {
-                _.scopedSubmit($this);
-            }
-        },
-
-        keyboard_handler: function (ev) {
-            // If the user presses the enter key and
-            // we have an autosubmit form trigger the subform submission
-            if (ev.keyCode != 13) {
-                return;
-            }
-            var $subform = $(this).parents('.pat-subform');
-            if (!$subform.is('.pat-autosubmit')) {
-                return;
-            }
-            return $subform.submit();
-        },
-
-        submitClicked: function(ev) {
-            ev.preventDefault();
-            ev.stopPropagation();
-            ajax.onClickSubmit(ev); // make sure the submitting button is sent with the form
-
-            var $button = $(ev.target),
-                $sub = $button.parents(".pat-subform").first(),
-                formaction = $button.attr("formaction");
-
-            if (formaction) {
-                // override the default action and restore afterwards
-                if ($sub.is(".pat-inject")) {
-                    var previousValue = $sub.data("pat-inject");
-                    $sub.data("pat-inject", inject.extractConfig($sub, {url: formaction}));
-                    _.scopedSubmit($sub);
-                    $sub.data("pat-inject", previousValue);
-                } else if ($sub.is(".pat-modal")) {
-                    $sub.data("pat-inject", [$.extend($sub.data("pat-inject")[0], {url: formaction})]);
-                    _.scopedSubmit($sub);
-                } else {
-                    $sub.parents("form").attr("action", formaction);
-                    _.scopedSubmit($sub);
-                }
-            } else {
-                _.scopedSubmit($sub);
-            }
+        // make other controls "unsuccessful"
+        log.debug("Hiding unwanted elements from submission.");
+        var names = $exclude.map(function () {
+            var name = $(this).attr("name");
+            return name ? name : 0;
+        });
+        $exclude.each(function () {
+            $(this).attr("name", "");
+        });
+        if ($el.is(".pat-inject") || $el.is(".pat-modal")) {
+            inject.submitSubform($el);
+        } else {
+            // use the native handler, since there could be event handlers
+            // redirecting to inject/ajax.
+            $form[0].submit();
         }
-    };
+        // reenable everything
+        log.debug("Restoring previous state.");
+        $exclude.each(function (i) {
+            if (names[i]) {
+                $(this).attr("name", names[i]);
+            }
+        });
+    },
 
-    registry.register(_);
-    return _;
+    submit(ev) {
+        ev.stopPropagation();
+        var $this = $(ev.target),
+            $button = $this.find("button[type=submit][formaction]").first();
+        if ($button.length) {
+            $button.trigger("click");
+        } else {
+            this.scopedSubmit($this);
+        }
+    },
+
+    keyboard_handler(ev) {
+        // If the user presses the enter key and
+        // we have an autosubmit form trigger the subform submission
+        if (ev.keyCode != 13) {
+            return;
+        }
+        var $subform = $(ev.target).parents(".pat-subform");
+        if (!$subform.is(".pat-autosubmit")) {
+            return;
+        }
+        return $subform.submit();
+    },
+
+    submitClicked(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        ajax.onClickSubmit(ev); // make sure the submitting button is sent with the form
+
+        var $button = $(ev.target),
+            $sub = $button.parents(".pat-subform").first(),
+            formaction = $button.attr("formaction");
+
+        if (formaction) {
+            // override the default action and restore afterwards
+            if ($sub.is(".pat-inject")) {
+                var previousValue = $sub.data("pat-inject");
+                $sub.data(
+                    "pat-inject",
+                    inject.extractConfig($sub, { url: formaction })
+                );
+                this.scopedSubmit($sub);
+                $sub.data("pat-inject", previousValue);
+            } else if ($sub.is(".pat-modal")) {
+                $sub.data("pat-inject", [
+                    $.extend($sub.data("pat-inject")[0], {
+                        url: formaction,
+                    }),
+                ]);
+                this.scopedSubmit($sub);
+            } else {
+                $sub.parents("form").attr("action", formaction);
+                this.scopedSubmit($sub);
+            }
+        } else {
+            this.scopedSubmit($sub);
+        }
+    },
 });
-
-// jshint indent: 4, browser: true, jquery: true, quotmark: double
-// vim: sw=4 expandtab
---
+++ src/pat/subform/subform.test.js
@@ -0,0 +1,84 @@
+import registry from "../../core/registry";
+import $ from "jquery";
+import pattern from "./subform";
+
+
+describe("subform base tests", function() {
+    describe("Triggering of the pattern", function() {
+        it("happens when a fieldset has the pat-subform class", function() {
+            var $form = $(
+                "<form>" +
+                    '  <fieldset class="pat-subform">' +
+                    '    <input type="text" name="q" placeholder="Search query"/>' +
+                    "  </fieldset>" +
+                    "</form>"
+            );
+            var spy_init = spyOn(pattern, "init");
+            registry.scan($form);
+            expect(spy_init).toHaveBeenCalled();
+        });
+    });
+
+    describe("Entering a return", function() {
+        it("does nothing if the pat-autosubmit class is missing", function() {
+            var $form = $(
+                "<form>" +
+                    '  <fieldset class="pat-subform">' +
+                    '    <input type="text" name="q" placeholder="Search query"/>' +
+                    "  </fieldset>" +
+                    "</form>"
+            );
+            const pat = new pattern($('.pat-subform', $form));
+            const spy_keyboard_handler = spyOn(pat.__proto__, "keyboard_handler").and.callThrough();
+            const spy_submit = spyOn(pat.__proto__, "submit");
+            registry.scan($form);
+            $form.find("[name=q]").trigger({
+                type: "keydown",
+                keyCode: 13
+            });
+            expect(spy_keyboard_handler).toHaveBeenCalled();
+            expect(spy_submit).not.toHaveBeenCalled();
+        });
+        it("submits the subform when the pat-autosubmit class is present", function() {
+            var $form = $(
+                "<form>" +
+                    '  <fieldset class="pat-subform pat-autosubmit">' +
+                    '    <input type="text" name="q" placeholder="Search query"/>' +
+                    "  </fieldset>" +
+                    "</form>"
+            );
+            const pat = new pattern($('.pat-subform', $form));
+            const spy_keyboard_handler = spyOn(pat.__proto__, "keyboard_handler").and.callThrough();
+            const spy_submit = spyOn(pat.__proto__, "submit");
+            registry.scan($form);
+            $form.find("[name=q]").trigger({
+                type: "keydown",
+                keyCode: 13
+            });
+            expect(spy_keyboard_handler).toHaveBeenCalled();
+            expect(spy_submit).toHaveBeenCalled();
+        });
+        it("does not submit the parent autosubmit form when the pat-autosubmit class is present on both", function() {
+            var $form = $(
+                '<form class="pat-autosubmit">' +
+                    '  <fieldset class="pat-subform pat-autosubmit">' +
+                    '    <input type="text" name="q" placeholder="Search query"/>' +
+                    "  </fieldset>" +
+                    '  <button type="submit" name="submit">Submit</button>' +
+                    "</form>"
+            );
+            const pat = new pattern($('.pat-subform', $form));
+            const spy_keyboard_handler = spyOn(pat.__proto__, "keyboard_handler").and.callThrough();
+            const spy_submit = spyOn(pat.__proto__, "submit");
+            const spy_formsubmit = spyOn($form, "submit");
+            registry.scan($form);
+            $form.find("[name=q]").trigger({
+                type: "keydown",
+                keyCode: 13
+            });
+            expect(spy_keyboard_handler).toHaveBeenCalled();
+            expect(spy_submit).toHaveBeenCalled();
+            expect(spy_formsubmit).not.toHaveBeenCalled();
+        });
+    });
+});
