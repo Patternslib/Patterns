fcfa7b600479fc5cce4f03e1425c480d33b214a8
Johannes Raggam <thetetet@gmail.com>
2020-07-27 12:30:32 +0200

    core pliggable

--- src/core/pluggable.js
+++ src/core/pluggable.js
@@ -1,25 +1,24 @@
-define([
-    "jquery",
-    "pat-utils"
-], function($, utils) {
-    var pluggable = {
+import $ from "jquery";
+import utils from "../../core/utils";
 
-        extend: function (attrs) {
-            return utils.extend(this, attrs);
-        },
+var pluggable = {
+    extend: function(attrs) {
+        return utils.extend(this, attrs);
+    },
 
-        registerPlugin: function (name, callback) {
-            this.plugins[name] = callback;
-        },
+    registerPlugin: function(name, callback) {
+        this.plugins[name] = callback;
+    },
 
-        initializePlugins: function () {
-            var i, keys = _.keys(this.plugins);
-            var args = arguments;
-            for (i=0; i<keys.length; i++) {
-                $.proxy(this.plugins[keys[i]], this).apply(this, args);
-            }
-            return args;
+    initializePlugins: function() {
+        var i,
+            keys = _.keys(this.plugins);
+        var args = arguments;
+        for (i = 0; i < keys.length; i++) {
+            $.proxy(this.plugins[keys[i]], this).apply(this, args);
         }
-    };
-    return pluggable;
-});
+        return args;
+    }
+};
+
+export default pluggable;
