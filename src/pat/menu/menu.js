38c367e1f7653818a66c09094951d34d1396e06a
Johannes Raggam <thetetet@gmail.com>
2020-07-27 12:42:34 +0200

    pat menu

--- src/pat/menu/menu.js
+++ src/pat/menu/menu.js
@@ -1,60 +1,57 @@
-define([
-    "pat-base",
-    "pat-parser",
-    "jquery",
-], function(Base, Parser, $) {
+import $ from "jquery";
+import Base from "../../core/base";
+import Parser from "../../core/parser";
 
-    var parser = new Parser('menu');
+const parser = new Parser("fullscreen");
 
-    parser.addArgument('nav-item-selector', 'li'); // CSS Selector for navigation items.
+parser.addArgument('nav-item-selector', 'li'); // CSS Selector for navigation items.
 
-    return Base.extend({
-        name: "menu",
-        trigger: ".pat-menu",
 
-        timer: null,
+export default Base.extend({
+    name: "menu",
+    trigger: ".pat-menu",
 
-        init: function($el, opts) {
-            this.options = parser.parse(this.$el, opts);
-            var self = this;
-            $el.find(this.options.navItemSelector).each(function() {
-                var $it = $(this);
-                $it.addClass("closed")
-                    .on("mouseover", self.mouseOverHandler.bind(self))
-                    .on("mouseout", self.mouseOutHandler.bind(self));
-                if($it.children(self.options.navItemSelector).length > 0) {
-                    $it.addClass("hasChildren");
-                };
-            });
-        },
+    timer: null,
 
-        openMenu: function(it) {
-            if (this.timer) {
-                clearTimeout(this.timer);
-                this.timer = null;
-            }
-            it = $(it);
-            if (!it.hasClass("open")) {
-                it.siblings(".open").each(function() { this.closeMenu(this.$el);}.bind(this));
-                it.addClass("open").removeClass("closed");
-            }
-        },
+    init: function($el, opts) {
+        this.options = parser.parse(this.$el, opts);
+        var self = this;
+        $el.find(this.options.navItemSelector).each(function() {
+            var $it = $(this);
+            $it.addClass("closed")
+                .on("mouseover", self.mouseOverHandler.bind(self))
+                .on("mouseout", self.mouseOutHandler.bind(self));
+            if($it.children(self.options.navItemSelector).length > 0) {
+                $it.addClass("hasChildren");
+            };
+        });
+    },
 
-        closeMenu: function(it) {
-            $(it).find(".open").addBack().removeClass("open").addClass("closed");
-        },
-
-        mouseOverHandler: function(ev) {
-            this.openMenu(ev.target);
-        },
-
-        mouseOutHandler: function(ev) {
-            if (this.timer) {
-                clearTimeout(this.timer);
-                this.timer = null;
-            }
-            this.timer = setTimeout(function() { this.closeMenu(ev.target); }.bind(this), 1000);
+    openMenu: function(it) {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
         }
+        it = $(it);
+        if (!it.hasClass("open")) {
+            it.siblings(".open").each(function() { this.closeMenu(this.$el);}.bind(this));
+            it.addClass("open").removeClass("closed");
+        }
+    },
 
-    });
+    closeMenu: function(it) {
+        $(it).find(".open").addBack().removeClass("open").addClass("closed");
+    },
+
+    mouseOverHandler: function(ev) {
+        this.openMenu(ev.target);
+    },
+
+    mouseOutHandler: function(ev) {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
+        this.timer = setTimeout(function() { this.closeMenu(ev.target); }.bind(this), 1000);
+    }
 });
