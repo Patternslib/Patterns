15e5c5a53ddbd31de87c2c20fd22d1b47e9fd035
Johannes Raggam <thetetet@gmail.com>
2020-07-27 12:44:38 +0200

    pat sticky

--- src/pat/sticky/sticky.js
+++ src/pat/sticky/sticky.js
@@ -1,49 +1,45 @@
 /* pat-sticky - A pattern for a sticky polyfill */
-define([
-    "underscore",
-    "pat-parser",
-    "pat-registry",
-    "pat-base",
-    "pat-logger",
-    "pat-utils",
-    "stickyfilljs"
-], function(_, Parser, registry, Base, logger, utils, Stickyfill) {
-    "use strict";
-    var parser = new Parser("sticky");
-    var log = logger.getLogger("sticky")
-    parser.addArgument("selector", "");
+import _ from "underscore";
+import Parser from "../../core/parser";
+import Base from "../../core/base";
+import logging from "../../core/logging";
+import utils from "../../core/utils";
+import Stickyfill from "stickyfilljs";
+"use strict";
+var parser = new Parser("sticky");
+var log = logging.getLogger("sticky");
+parser.addArgument("selector", "");
 
-    return Base.extend({
-        name: "sticky",
-        trigger: ".pat-sticky",
-        init: function() {
-            this.options = parser.parse(this.$el);
-            this.makeSticky();
-            $('body').on('pat-update', utils.debounce(this.onPatternUpdate.bind(this), 500));
-            
-            /* recalc if the DOM changes. Should fix positioning issues when parts of the page get injected */
+export default Base.extend({
+    name: "sticky",
+    trigger: ".pat-sticky",
+    init: function() {
+        this.options = parser.parse(this.$el);
+        this.makeSticky();
+        $("body").on(
+            "pat-update",
+            utils.debounce(this.onPatternUpdate.bind(this), 500)
+        );
 
-            return this.$el;
-        },
-        onPatternUpdate: function (ev, data) {
-            /* Handler which gets called when pat-update is triggered within
-             * the .pat-sticky element.
-             */
-            Stickyfill.refreshAll()
-            return true;
-        },
-        makeSticky: function() {
-            if (this.options.selector === "") {
-                this.$stickies = this.$el;
-            } else {
-                this.$stickies = this.$el.find(this.options.selector);
-            }
-            this.$stickies.each(function (idx, elem) {
-                Stickyfill.add(elem);
-            });
+        /* recalc if the DOM changes. Should fix positioning issues when parts of the page get injected */
+
+        return this.$el;
+    },
+    onPatternUpdate: function(ev, data) {
+        /* Handler which gets called when pat-update is triggered within
+         * the .pat-sticky element.
+         */
+        Stickyfill.refreshAll();
+        return true;
+    },
+    makeSticky: function() {
+        if (this.options.selector === "") {
+            this.$stickies = this.$el;
+        } else {
+            this.$stickies = this.$el.find(this.options.selector);
         }
-    });
+        this.$stickies.each(function(idx, elem) {
+            Stickyfill.add(elem);
+        });
+    }
 });
-
-// jshint indent: 4, browser: true, jquery: true, quotmark: double
-// vim: sw=4 expandtab
