038c897888a5f0f518f7f61f39061cfaf0025e34
Johannes Raggam <thetetet@gmail.com>
2020-08-03 14:35:25 +0200

    Cleanup

--- src/wrap-end.js
+++
@@ -1,9 +0,0 @@
-// (function(root) {
-//   root.patterns=require("patterns");
-//   root.patterns.Parser=require("pat-parser");
-//   root.patterns.logging=require("pat-logger");
-//   root.patterns.utils=require("pat-utils");
-//   root.patterns.logger=require("pat-logger");
-//   root.patterns.Base=require("pat-base");
-// })(window);
-// window.define=undefined;
