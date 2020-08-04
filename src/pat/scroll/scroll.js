42d2667fcf1542f8ffeed10281d65c65aa703e36
Alexander Pilz <pilz@syslab.com>
2020-08-04 15:02:36 +0200

    revert only the scroll js to the state of 412abb4bb6d77962dedc934367701bf477eebb62 before the latest thet fixes, so that Cornelis gets his old behavior back for horizonscan until thet can look at the issue

--- src/pat/scroll/scroll.js
+++ src/pat/scroll/scroll.js
@@ -120,21 +120,6 @@ define([
             }
         },
 
-        findScrollContainer: function(el) {
-            var direction = this.options.direction;
-            var scrollable = $(el).parents().filter(function() {
-                return (
-                    ['auto', 'scroll'].indexOf($(this).css('overflow')) > -1 ||
-                    (direction === 'top' && ['auto', 'scroll'].indexOf($(this).css('overflow-y')) > -1) ||
-                    (direction === 'left' && ['auto', 'scroll'].indexOf($(this).css('overflow-x')) > -1)
-                );
-            }).first();
-            if ( typeof scrollable[0] === 'undefined' ) {
-                scrollable = $('html, body');
-            }
-            return scrollable;
-        },
-
         smoothScroll: function() {
             var href, fragment;
             var scroll = this.options.direction == "top" ? 'scrollTop' : 'scrollLeft',
@@ -143,10 +128,6 @@ define([
                 // apply scroll options directly
                 scrollable = this.options.selector ? $(this.options.selector) : this.$el;
                 options[scroll] = this.options.offset;
-            } else if (this.options.selector === "top") {
-                // Just scroll up, period.
-                scrollable = this.findScrollContainer(this.$el);
-                options['scrollTop'] = 0;
             } else {
                 // Get the first element with overflow (the scroll container)
                 // starting from the *target*
@@ -156,16 +137,27 @@ define([
                     fragment = this.options.selector;
                 } else {
                     href = this.$el.attr('href');
-                    fragment = href.indexOf('#') !== -1 ? '#' + href.split('#').pop() : undefined;
+                    fragment = href.indexOf('#') !== -1 && '#' + href.split('#').pop() || undefined;                    
                 }
                 var target = $(fragment);
                 if (target.length === 0) {
                     return;
                 }
 
-                scrollable = this.findScrollContainer(target);
+                scrollable = $(target.parents().filter(function() {
+                    return ( $(this).css('overflow') === 'auto' ||
+                             $(this).css('overflow') === 'scroll' );
+                }).first())
 
-                if ( scroll === "scrollTop" ) {
+                if ( typeof scrollable[0] === 'undefined' ) {
+                    scrollable = $('html, body');
+                    // positioning context is document
+                    if ( scroll === "scrollTop" ) {
+                        options[scroll] = Math.floor(target.safeOffset().top);
+                    } else {
+                        options[scroll] = Math.floor(target.safeOffset().left);
+                    }
+                } else if ( scroll === "scrollTop" ) {
                     // difference between target top and scrollable top becomes 0
                     options[scroll] = Math.floor(scrollable.scrollTop()
                                                  + target.safeOffset().top
