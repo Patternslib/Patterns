7ecf08f31d0ef023d42f6d42d354eb6cce395e48
Johannes Raggam <thetetet@gmail.com>
2020-08-10 11:51:34 +0200

    Test setup

---
+++ src/setupTests.js
@@ -0,0 +1,29 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect";
+
+// need this for async/await in tests
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import jquery from "jquery";
+window.jQuery = jquery;
+jQuery.expr.pseudos.visible = function( elem ) {
+    // Fix jQuery ":visible" selector always returns false in JSDOM.
+    // https://github.com/jsdom/jsdom/issues/1048#issuecomment-401599392
+    return true;
+};
+
+// pat-fullscreen
+document.requestFullscreen = jest.fn();
+document.exitFullscreen = jest.fn();
+document.fullscreenElement = jest.fn();
+document.fullscreenEnabled = jest.fn();
+document.fullscreenchange = jest.fn();
+document.fullscreenerror = jest.fn();
+
+// pat-subform
+// See https://github.com/jsdom/jsdom/issues/1937#issuecomment-461810980
+window.HTMLFormElement.prototype.submit = () => {};
