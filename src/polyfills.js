c5f16b772b5742dd2b66818200ea6bd75197408a
Johannes Raggam <thetetet@gmail.com>
2020-09-24 13:27:51 +0200

    IE11 support: Simplify browserslist target, Fix promise polyfill import, Import all of core-js features

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,7 +1,15 @@
 // Polyfills for older browsers, most notably IE11.
 // Usage: Import this module
 //      import "patternslib/src/polyfills";
+
+// Core JS features
+// You can also import individual core-js features:
+//import "core-js/stable/object/assign";
+// But we're importing them all:
+import "core-js/stable";
+
+// Web APIs
 import "intersection-observer";
-import "promise-polyfill";
+import "promise-polyfill/src/polyfill";
 import "url-polyfill";
 import "whatwg-fetch";
