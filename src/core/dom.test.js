142b1106f7351c10a31dc4e196a45532ada179bc
Johannes Raggam <thetetet@gmail.com>
2020-10-22 17:32:13 +0200

    core dom: Add ``jqToNode`` to return a DOM node if a jQuery node was passed.

--- src/core/dom.test.js
+++ src/core/dom.test.js
@@ -1,5 +1,18 @@
+import $ from "jquery";
 import dom from "./dom";
 
 describe("core.dom tests", () => {
     // Tests from the core.dom module
+
+    describe("jqToNode tests", () => {
+        it("always returns a bare DOM node no matter if a jQuery or bare DOM node was passed.", (done) => {
+            const el = document.createElement("div");
+            const $el = $(el);
+
+            expect(dom.jqToNode($el)).toBe(el);
+            expect(dom.jqToNode(el)).toBe(el);
+
+            done();
+        });
+    });
 });
