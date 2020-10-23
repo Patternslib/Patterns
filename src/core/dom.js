894f5630d219410534a30012e4b6260eb363bf20
Johannes Raggam <thetetet@gmail.com>
2020-10-23 10:40:39 +0200

    core dom: Add ``querySelectorAllAndMe`` to do a querySelectorAll including the starter element.

--- src/core/dom.js
+++ src/core/dom.js
@@ -8,8 +8,22 @@ const jqToNode = (el) => {
     return el;
 };
 
+const querySelectorAllAndMe = (el, selector) => {
+    // Like querySelectorAll but including the element where it starts from.
+    // Returns an Array, not a NodeList
+
+    el = jqToNode(el); // Ensure real DOM node.
+
+    const all = [...el.querySelectorAll(selector)];
+    if (el.matches(selector)) {
+        all.unshift(el); // start element should be first.
+    }
+    return all;
+};
+
 const dom = {
     jqToNode: jqToNode,
+    querySelectorAllAndMe: querySelectorAllAndMe,
 };
 
 export default dom;
--- src/core/dom.test.js
+++ src/core/dom.test.js
@@ -15,4 +15,31 @@ describe("core.dom tests", () => {
             done();
         });
     });
+
+    describe("querySelectorAllAndMe tests", () => {
+        it("return also starting node if query matches.", (done) => {
+            const el = document.createElement("div");
+            el.setAttribute("class", "node1");
+            el.innerHTML = `<span class="node2">hello.</span>`;
+
+            const res1 = dom.querySelectorAllAndMe(el, ".node1");
+            expect(res1.length).toBe(1);
+            expect(res1[0].outerHTML).toBe(
+                `<div class="node1"><span class="node2">hello.</span></div>`
+            );
+
+            const res2 = dom.querySelectorAllAndMe(el, ".node2");
+            expect(res2.length).toBe(1);
+            expect(res2[0].outerHTML).toBe(`<span class="node2">hello.</span>`);
+
+            const res3 = dom.querySelectorAllAndMe(el, "div, span");
+            expect(res3.length).toBe(2);
+            expect(res3[0].outerHTML).toBe(
+                `<div class="node1"><span class="node2">hello.</span></div>`
+            );
+            expect(res3[1].outerHTML).toBe(`<span class="node2">hello.</span>`);
+
+            done();
+        });
+    });
 });
