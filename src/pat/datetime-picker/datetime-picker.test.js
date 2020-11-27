db92888093d92032ed7491173a92c96f12f78a43
Johannes Raggam <thetetet@gmail.com>
2020-11-27 15:50:37 +0100

    pat datetime picker: - Change CSS selectors for better namespacing and remove implicit dependency on glyphicons. - Remove dependency on MomentJS. - After updating the original input, let the ``change`` event bubble up. - Support ``native`` behavior.

--- src/pat/datetime-picker/datetime-picker.test.js
+++ src/pat/datetime-picker/datetime-picker.test.js
@@ -16,18 +16,19 @@ describe("pat-datetime-picker", function () {
     });
 
     it("Default datetime picker.", async function () {
-        var $el = $(
-            '<input type="datetime-local" class="pat-datetime-picker"/>'
-        ).appendTo(document.body);
-        pattern.init($el);
+        document.body.innerHTML = `
+            <input
+                type="datetime-local"
+                class="pat-datetime-picker"/>
+        `;
+        pattern.init(document.querySelector("input"));
         await utils.timeout(1); // wait a tick for async to settle.
+        document.querySelector("input[type=text]").click();
 
-        $("input.date", $el.parent()).click();
-
-        var date = new Date();
-        var day = date.getDate().toString();
-        var month = date.getMonth().toString(); // remember, month-count starts from 0
-        var year = date.getFullYear().toString();
+        const date = new Date();
+        const day = date.getDate().toString();
+        const month = date.getMonth().toString(); // remember, month-count starts from 0
+        const year = date.getFullYear().toString();
 
         expect(
             document.querySelector(
@@ -54,13 +55,15 @@ describe("pat-datetime-picker", function () {
     });
 
     it("Date/Time picker starts at Monday.", async function () {
-        var $el = $(
-            '<input type="date" class="pat-datetime-picker" data-pat-datetime-picker="first-day: 1" />'
-        ).appendTo(document.body);
-        pattern.init($el);
+        document.body.innerHTML = `
+            <input
+                type="datetime-local"
+                class="pat-datetime-picker"
+                data-pat-datetime-picker="first-day: 1" />
+        `;
+        pattern.init(document.querySelector("input"));
         await utils.timeout(1); // wait a tick for async to settle.
-
-        $("input.date", $el.parent()).click();
+        document.querySelector("input[type=text]").click();
 
         expect(
             document.querySelector(".pika-lendar th:first-child abbr")
@@ -69,13 +72,15 @@ describe("pat-datetime-picker", function () {
     });
 
     it("Date/Time picker with pre-set value.", async function () {
-        var $el = $(
-            '<input type="datetime-local" class="pat-datetime-picker" value="1900-01-01T00:00"/>'
-        ).appendTo(document.body);
-        pattern.init($el);
+        document.body.innerHTML = `
+            <input
+                type="datetime-local"
+                class="pat-datetime-picker"
+                value="1900-01-01T00:00"/>
+        `;
+        pattern.init(document.querySelector("input"));
         await utils.timeout(1); // wait a tick for async to settle.
-
-        $("input.date", $el.parent()).click();
+        document.querySelector("input[type=text]").click();
 
         expect(
             document.querySelector(
@@ -97,55 +102,43 @@ describe("pat-datetime-picker", function () {
     });
 
     it("Date/Time picker with week numbers.", async function () {
-        var $el = $(
-            '<input type="datetime-local" class="pat-datetime-picker" data-pat-datetime-picker="week-numbers: show;" value="2017-09-18T23:42"/>'
-        ).appendTo(document.body);
-        pattern.init($el);
+        document.body.innerHTML = `
+            <input
+                type="datetime-local"
+                class="pat-datetime-picker"
+                data-pat-datetime-picker="week-numbers: show;" value="2017-09-18T23:42"/>
+        `;
+        pattern.init(document.querySelector("input"));
         await utils.timeout(1); // wait a tick for async to settle.
-
-        $("input.date", $el.parent()).click();
+        document.querySelector("input[type=text]").click();
 
         expect(
             document.querySelectorAll(".pika-lendar .pika-week")[0].textContent
         ).toBe("35");
     });
 
-    /* TODO: fix and properly mock with jasmine.Clock().mockTime in jasmine 2.x
-    it('Set Date/Time to today.', async function () {
+    it("Test today and clear buttons.", async function () {
+        Date.prototype.toISOString = jest.fn(() => "2021-05-01T10:10:10");
+        Date.prototype.toTimeString = jest.fn(() => "10:10:10 GMT+0200");
 
-        var baseDate = new Date('2010-10-10T12:34:00.000Z');
+        document.body.innerHTML = `
+            <input type="datetime-local"/>
+        `;
 
-        spyOn(window, 'Date').and.callFake(function() {
-            return {
-                toTimeString: function () {
-                    return baseDate.toTimeString();
-                },
-                toISOString: function () {
-                    return baseDate.toISOString();
-                },
-                getTime: function () {
-                    return baseDate.getTime();
-                }
-            };
-        });
-
-        var $el = $('<input type="datetime-local"/>').appendTo(document.body);
-        pattern.init($el);
+        pattern.init(document.querySelector("input"));
         await utils.timeout(1); // wait a tick for async to settle.
-        var $wrapper = $el.next();
-        var $date = $('input.date', $wrapper);
-        var $time = $('input.time', $wrapper);
 
-        expect($el.val()).toBe('');
-        expect($date.val()).toBe('');
-        expect($time.val()).toBe('');
+        expect(document.querySelector("input[type=text]").value).toBe("");
+        expect(document.querySelector("input[type=time]").value).toBe("");
+        expect(document.querySelector("input[type=datetime-local]").value).toBe(""); // prettier-ignore
 
-        $('button.today', $wrapper).click();
+        document.querySelector("._btn._now").click();
 
-        expect($el.val()).toBe('2010-10-10T12:34');
-        expect($date.val()).toBe('2010-10-10');
-        expect($time.val()).toBe('12:34');
+        expect(document.querySelector("input[type=text]").value).toBe("2021-05-01"); // prettier-ignore
+        expect(document.querySelector("input[type=time]").value).toBe("10:10"); // prettier-ignore
+        expect(document.querySelector("input[type=datetime-local]").value).toBe("2021-05-01T10:10"); // prettier-ignore
 
+        Date.prototype.toISOString.mockRestore();
+        Date.prototype.toTimeString.mockRestore();
     });
-    */
 });
