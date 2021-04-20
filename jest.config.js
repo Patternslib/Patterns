db49fd85c9c697b930e055cc02b8a690f3ea916c
Johannes Raggam <thetetet@gmail.com>
2021-04-20 20:55:07 +0200

    Code formating.

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,12 @@
 module.exports = {
     roots: ["./src", "./webpack"],
     setupFilesAfterEnv: ["./src/setupTests.js"],
-    watchPlugins: [
-        "jest-watch-typeahead/filename",
-        "jest-watch-typeahead/testname",
-    ],
+    watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
     transform: {
         "^.+\\.[t|j]sx?$": "babel-jest",
     },
     moduleNameMapper: {
         "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     },
-    transformIgnorePatterns: [
-        "/node_modules/(?!@fullcalendar/*).+\\.[t|j]sx?$",
-    ],
+    transformIgnorePatterns: ["/node_modules/(?!@fullcalendar/*).+\\.[t|j]sx?$"],
 };
