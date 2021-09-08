57febfd18c5a5d015ab837114ad999f9c14ab2d7
Johannes Raggam <thetetet@gmail.com>
2021-09-08 13:38:10 +0200

    feat(core dom): add_event_listener and remove_event_listener methods to register event handlers for DOM nodes which can be unregistered by an id. Event handlers with the same id on the same node won't be registered twice.

--- src/core/dom.test.js
+++ src/core/dom.test.js
@@ -321,4 +321,51 @@ describe("core.dom tests", () => {
             done();
         });
     });
+
+    describe("add / remove event listener", () => {
+        const _el = {
+            event_list: [],
+            addEventListener(event_type, cb) {
+                this.event_list.push([event_type, cb]);
+            },
+            removeEventListener(event_type, cb) {
+                const idx = this.event_list.indexOf([event_type, cb]);
+                this.event_list.splice(idx, 1);
+            },
+        };
+
+        it("Registers events only once and unregisters events.", (done) => {
+            const cb1 = () => {};
+            const cb2 = () => {};
+
+            // register one event handler
+            dom.add_event_listener(_el, "click", "test_click", cb1);
+            expect(_el.event_list.length).toBe(1);
+            expect(_el.event_list[0][1]).toBe(cb1);
+
+            // register another event hander under the same id
+            dom.add_event_listener(_el, "click", "test_click", cb2);
+            expect(_el.event_list.length).toBe(1);
+            expect(_el.event_list[0][1]).toBe(cb2);
+
+            // register two more event handlers with unique ids
+            dom.add_event_listener(_el, "click", "test_click_2", () => {});
+            dom.add_event_listener(_el, "click", "test_click_3", () => {});
+            expect(_el.event_list.length).toBe(3);
+
+            // remove one specific event handler
+            dom.remove_event_listener(_el, "test_click_2");
+            expect(_el.event_list.length).toBe(2);
+
+            // try to remove an unregistered event handler
+            dom.remove_event_listener(_el, "test_click_4");
+            expect(_el.event_list.length).toBe(2);
+
+            // remove all registered event handlers on that element
+            dom.remove_event_listener(_el);
+            expect(_el.event_list.length).toBe(0);
+
+            done();
+        });
+    });
 });
