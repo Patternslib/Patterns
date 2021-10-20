93fe9762515a682f937abeb6749a714c038cfd24
Johannes Raggam <thetetet@gmail.com>
2021-10-20 17:25:50 +0200

    maint(pat scroll): Restructure and modernize code for better readability.

--- src/pat/scroll-box/scroll-box.js
+++ src/pat/scroll-box/scroll-box.js
@@ -1,4 +1,3 @@
-import $ from "jquery";
 import Base from "../../core/base";
 
 export default Base.extend({
@@ -10,65 +9,35 @@ export default Base.extend({
     timeout: 200,
     timeout_in_use: null,
 
-    init: function ($el) {
-        const el = $el[0];
-        let scroll_listener = el;
-        if (scroll_listener === document.body) {
-            scroll_listener = window;
-        }
+    scroll_listener: null,
+    last_known_scroll_position: 0,
+    timeout_id: null,
+    timeout_id__scroll_stop: null,
+
+    init() {
+        const el = this.el;
+        this.scroll_listener = el === document.body ? window : el;
 
         // If scolling is not possible, exit.
         if (
-            !scroll_listener == window &&
+            !this.scroll_listener === window &&
             (["auto", "scroll"].indexOf(getComputedStyle(el).overflow) === -1 ||
                 ["auto", "scroll"].indexOf(getComputedStyle(el).overflowY) === -1)
         ) {
             return;
         }
 
-        let last_known_scroll_position = 0;
-        let scroll_y = 0;
-        let timeout_id = null;
-        let timeout_id__scroll_stop = null;
-
-        let set_scroll_classes = (scroll_pos) => {
-            el.classList.remove("scroll-up");
-            el.classList.remove("scroll-down");
-            el.classList.remove("scroll-position-top");
-            el.classList.remove("scroll-position-bottom");
-
-            if (scroll_pos < last_known_scroll_position) {
-                el.classList.add("scroll-up");
-            } else if (last_known_scroll_position < scroll_pos) {
-                el.classList.add("scroll-down");
-            }
-
-            if (scroll_pos === 0) {
-                el.classList.add("scroll-position-top");
-            } else if (
-                scroll_listener === window &&
-                window.innerHeight + scroll_pos >= el.scrollHeight
-            ) {
-                el.classList.add("scroll-position-bottom");
-            } else if (
-                scroll_listener !== window &&
-                el.clientHeight + scroll_pos >= el.scrollHeight
-            ) {
-                el.classList.add("scroll-position-bottom");
-            }
-        };
-
-        scroll_listener.addEventListener("scroll", () => {
-            if (timeout_id === null) {
+        this.scroll_listener.addEventListener("scroll", () => {
+            if (this.timeout_id === null) {
                 // Run first callback quite soon to immediately set correct classes.
                 this.timeout_in_use = 10;
             }
 
-            window.clearTimeout(timeout_id);
-            timeout_id = window.setTimeout(() => {
-                scroll_y = this.get_scroll_y(scroll_listener);
-                set_scroll_classes(scroll_y);
-                last_known_scroll_position = scroll_y;
+            window.clearTimeout(this.timeout_id);
+            this.timeout_id = window.setTimeout(() => {
+                const scroll_y = this.get_scroll_y();
+                this.set_scroll_classes(scroll_y);
+                this.last_known_scroll_position = scroll_y;
                 this.timeout_in_use = this.timeout; // Set to normal timeout after first run
             }, this.timeout_in_use || this.timeout); // If not set for any reason, fallback to this.timeout
 
@@ -76,22 +45,50 @@ export default Base.extend({
             // has stopped for sure.
             // Then, when scrolling again the scroll classes are set
             // immediately and at the end of a series of scroll events.
-            window.clearTimeout(timeout_id__scroll_stop);
-            timeout_id__scroll_stop = window.setTimeout(() => {
-                timeout_id = null;
+            window.clearTimeout(this.timeout_id__scroll_stop);
+            this.timeout_id__scroll_stop = window.setTimeout(() => {
+                this.timeout_id = null;
             }, Math.max(500, this.timeout * 3)); // Note: should be more than the timespan in which scroll events are thrown.
         });
 
         // Set initial state
-        $().ready(() => set_scroll_classes(this.get_scroll_y(scroll_listener)));
+        this.set_scroll_classes(this.get_scroll_y());
     },
 
-    get_scroll_y: (el) => {
-        if (el === window) {
+    set_scroll_classes(scroll_pos) {
+        const el = this.el;
+        el.classList.remove("scroll-up");
+        el.classList.remove("scroll-down");
+        el.classList.remove("scroll-position-top");
+        el.classList.remove("scroll-position-bottom");
+
+        if (scroll_pos < this.last_known_scroll_position) {
+            el.classList.add("scroll-up");
+        } else if (this.last_known_scroll_position < scroll_pos) {
+            el.classList.add("scroll-down");
+        }
+
+        if (scroll_pos === 0) {
+            el.classList.add("scroll-position-top");
+        } else if (
+            this.scroll_listener === window &&
+            window.innerHeight + scroll_pos >= el.scrollHeight
+        ) {
+            el.classList.add("scroll-position-bottom");
+        } else if (
+            this.scroll_listener !== window &&
+            el.clientHeight + scroll_pos >= el.scrollHeight
+        ) {
+            el.classList.add("scroll-position-bottom");
+        }
+    },
+
+    get_scroll_y() {
+        if (this.scroll_listener === window) {
             // scrolling the window
             return window.scrollY !== undefined ? window.scrollY : window.pageYOffset; // pageYOffset for IE
         }
         // scrolling a DOM element
-        return el.scrollTop;
+        return this.scroll_listener.scrollTop;
     },
 });
