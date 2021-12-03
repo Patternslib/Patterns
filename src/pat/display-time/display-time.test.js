b02950e318a915ce91edb39d933e94066c5e0458
Johannes Raggam <thetetet@gmail.com>
2021-12-03 22:46:48 +0100

    maint(pat display time): Document preference of setting the language as lang attribute.

--- src/pat/display-time/display-time.test.js
+++ src/pat/display-time/display-time.test.js
@@ -59,6 +59,23 @@ describe("pat-display-time tests", () => {
         expect(el.textContent).toBe("April Donnerstag 22. 2021, 4:00:00");
     });
 
+    it("Example setting the output format explicitly in German using the lang attribute", async () => {
+        document.body.innerHTML = `
+          <time
+              class="pat-display-time"
+              datetime="2021-04-22T23:00Z"
+              lang="de"
+              data-pat-display-time="output-format: MMMM dddd Do YYYY, h:mm:ss">
+          </time>
+        `;
+        const el = document.querySelector(".pat-display-time");
+
+        Pattern.init(el);
+        await utils.timeout(1); // wait a tick for async to settle.
+
+        expect(el.textContent).toBe("April Donnerstag 22. 2021, 4:00:00");
+    });
+
     it("Example setting the output format in German due containment in German container", async () => {
         document.body.innerHTML = `
           <section lang="de">
@@ -129,6 +146,23 @@ describe("pat-display-time tests", () => {
               class="pat-display-time"
               datetime="2000-04-22T10:00Z"
               data-pat-display-time="from-now: true; no-suffix: true; locale: de">
+          </time>
+        `;
+        const el = document.querySelector(".pat-display-time");
+
+        Pattern.init(el);
+        await utils.timeout(1); // wait a tick for async to settle.
+
+        expect(el.textContent.indexOf("Jahre") >= 0).toBe(true);
+    });
+
+    it("Output formatted as 'from now' with no suffix in german using the lang attribute", async () => {
+        document.body.innerHTML = `
+          <time
+              class="pat-display-time"
+              datetime="2000-04-22T10:00Z"
+              lang="de"
+              data-pat-display-time="from-now: true; no-suffix: true">
           </time>
         `;
         const el = document.querySelector(".pat-display-time");
