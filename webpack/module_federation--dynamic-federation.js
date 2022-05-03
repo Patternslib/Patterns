1c9d5552d197eaffc62e7696d4e99ad17d727a7f
Johannes Raggam <thetetet@gmail.com>
2022-05-04 01:32:43 +0200

    Module Federation: Add approach from @manfredsteyer. Adapt to style guides.

--- webpack/module_federation--dynamic-federation.js
+++ webpack/module_federation--dynamic-federation.js
@@ -1,36 +1,40 @@
-// Original from: https://github.com/manfredsteyer/plugin-demo.git
-// Ref: https://github.com/thet/module-federation-minimaltest.git
+// Author: Manfred Steyer <manfred.steyer@gmx.net>
+// Author: Johannes Raggam <thetetet@gmail.com>
 
-// Helper functions for dynamic federation
-// Wrapper around webpack runtime api
+// From:
+//   https://github.com/manfredsteyer/plugin-demo.git
+//   https://github.com/thet/module-federation-minimaltest.git
 
-const containerMap = {};
-let isDefaultScopeInitialized = false;
+/**
+ * Load remote module / bundle.
+ *
+ * Wrapper around webpack runtime API
+ *
+ * Usage: get_container("bundle-name-xyz")
+ *
+ * @param {string} remote - the remote global name
+ * @returns {Promise<object>} - Federated Module Container
+ */
+const container_map = {};
+let is_default_scope_initialized = false;
 
-async function getContainer(key) {
-    const container = window[key];
+export default async function get_container(remote) {
+    const container = window[remote];
 
     // Do we still need to initialize the remote?
-    if (containerMap[key]) {
+    if (container_map[remote]) {
         return container;
     }
 
-    // Do we still need to initialize the share scope?
-    if (!isDefaultScopeInitialized) {
-        await __webpack_init_sharing__("default");
-        isDefaultScopeInitialized = true;
+    // Do we still need to initialize the shared scope?
+    if (!is_default_scope_initialized) {
+        await __webpack_init_sharing__("default"); // eslint-disable-line no-undef
+        is_default_scope_initialized = true;
     }
 
-    await container.init(__webpack_share_scopes__.default);
+    await container.init(__webpack_share_scopes__.default); // eslint-disable-line no-undef
 
-    // remember that container has been initialized
-    containerMap[key] = true;
+    // Remember that the container has been initialized.
+    container_map[remote] = true;
     return container;
-}
-
-export async function loadRemoteModule({ remoteName, exposedModule }) {
-    const container = await getContainer(remoteName);
-    const factory = await container.get(exposedModule);
-    const module = factory();
-    return module;
 }
