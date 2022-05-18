7eba043bf5f9d021c81d0ef65831226f16d025c5
Johannes Raggam <thetetet@gmail.com>
2022-05-18 18:18:33 +0200

    maint(Bundle): Module Federation: Allow to pass the shared dependencies explicitly. When passed package.json dependencies are not automatically added.

--- webpack/webpack.mf.js
+++ webpack/webpack.mf.js
@@ -6,6 +6,13 @@ const local_package_json = require("../package.json");
 // NOTE: This is also defined in ``module_federation.js``.
 const MF_NAME_PREFIX = "__patternslib_mf__";
 
+/**
+ * Get dependencies and versions for the module federation plugin from
+ * package.json dependency lists.
+ *
+ * @param {Array} - List of package.json dependencies fields.
+ * @returns {Object} - Object with dependencies for the module federation plugin.
+ */
 function shared_from_dependencies(...dependencies) {
     const shared = {};
     for (const deps of dependencies) {
@@ -26,13 +33,40 @@ function shared_from_dependencies(...dependencies) {
     return shared;
 }
 
-function config({ name, filename = "remote.min.js", package_json, remote_entry }) {
+/**
+ * Webpack module federation config factory.
+ *
+ * Use this to extend your webpack configuration for module federation support.
+ *
+ * @param {String} name - Bundle/remote name. If not given, the package.json name is used.
+ * @param {String} filename - Name of the generated remote entry file. Default ``remote.min.js``.
+ * @param {Object} package_json - Your imported project's package.json file. This is to automatically set the shared dependencies.
+ * @param {String} remote_entry - Path to which the new remote entry file is written to.
+ * @param {Object} shared - Object with dependency name - version specifier pairs. Can be used instead of the package_json parameter.
+ * @returns {Object} - Webpack config partial with instantiated module federation plugins.
+ */
+function config({
+    name,
+    filename = "remote.min.js",
+    package_json,
+    remote_entry,
+    shared = {},
+}) {
     // If no name is given, use the package name.
     name = name || package_json?.name || local_package_json.name;
 
     // Create a JS-variable compatible name and add a prefix.
     const normalized_bundle_name =
         MF_NAME_PREFIX + name.match(/([_$A-Za-z0-9])/g).join("");
+
+    shared = {
+        ...(!Object.keys(shared).length && // only include package.json depenencies, if shared is empty.
+            shared_from_dependencies(
+                local_package_json.dependencies,
+                package_json?.dependencies
+            )),
+        ...shared,
+    };
 
     return new ModuleFederationPlugin({
         name: normalized_bundle_name,
@@ -42,12 +76,7 @@ function config({ name, filename = "remote.min.js", package_json, remote_entry }
                 "./main": remote_entry,
             },
         }),
-        shared: {
-            ...shared_from_dependencies(
-                local_package_json.dependencies,
-                package_json?.dependencies
-            ),
-        },
+        shared: shared,
     });
 }
 
