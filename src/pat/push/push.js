2dbe44f8eeb2e32becf4efd5394bd3a4522dc442
Johannes Raggam <thetetet@gmail.com>
2022-06-08 16:56:22 +0200

    maint(pat push): Restructure code.
    Restructure code and narrow down the error handling for fetching to only the fetch block.

--- src/pat/push/push.js
+++ src/pat/push/push.js
@@ -33,87 +33,97 @@ export default Base.extend({
     },
 
     async perform_inject() {
+        let data = null;
+
         try {
             const response = await fetch(this.options.url);
-            const data = await response.text();
-            if (this.options.mode === "append") {
-                this.el.insertAdjacentHTML("beforeend", data);
-            } else {
-                this.el.innerHTML = data;
-            }
-            registry.scan(this.el);
+            data = await response.text();
         } catch (e) {
             logger.error(
                 `Could not fetch from ${this.options.url} on push-id ${this.options.pushId}.`
             );
         }
+
+        if (data === null) {
+            return;
+        }
+
+        if (this.options.mode === "append") {
+            this.el.insertAdjacentHTML("beforeend", data);
+        } else {
+            this.el.innerHTML = data;
+        }
+        registry.scan(this.el);
     },
 
     async desktop_notification() {
+        // Let's check if the browser supports notifications
+        if (!("Notification" in window)) {
+            logger.error("This browser does not support notifications.");
+            return;
+        }
+
+        // Notifications need to be granted.
+        // Note: Current browsers don't allow an automatic request for
+        //       permission but need an interaction to allow it.
+        //       The following code won't work out of the box in such cases.
+        if (!(Notification.permission in ["denied", "granted"])) {
+            Notification.requestPermission((permission) => {
+                // Whatever the user answers, we make sure Chrome stores the information
+                if (!("permission" in Notification)) {
+                    Notification.permission = permission;
+                }
+            });
+        }
+
+        // Let's check if the user is okay to get some notification
+        if (Notification.permission !== "granted") {
+            return;
+        }
+
+        let data = [];
         try {
-            // Let's check if the browser supports notifications
-            if (!("Notification" in window)) {
-                logger.error("This browser does not support notifications.");
-                return;
-            }
-
-            // Notifications need to be granted.
-            // Note: Current browsers don't allow an automatic request for
-            //       permission but need an interaction to allow it.
-            //       The following code won't work out of the box in such cases.
-            if (!(Notification.permission in ["denied", "granted"])) {
-                Notification.requestPermission((permission) => {
-                    // Whatever the user answers, we make sure Chrome stores the information
-                    if (!("permission" in Notification)) {
-                        Notification.permission = permission;
-                    }
-                });
-            }
-
-            // Let's check if the user is okay to get some notification
-            if (Notification.permission === "granted") {
-                const response = await fetch(this.options.url);
-                const data = await response.json();
-
-                if (data.length === 0) {
-                    return;
-                }
-
-                for (const message of data) {
-                    const notification = new Notification(message.title, message);
-                    if (message.data?.url) {
-                        // If ``message.data`` contains an URL, open it.
-                        // If not, the browser will simply get the focus when
-                        // clicking on the desktop notification.
-                        notification.addEventListener("click", () => {
-                            window.open(message.data.url, "_self");
-
-                            // Opening a URL in a new tab from JavaScript is
-                            // equivalent to opening a popup window which is
-                            // blocked by default.
-                            // Therefore, we open the URL in the same tab and
-                            // rely on other mechanisms that the user's work
-                            // isn't lost (auto-save, form unlod protection.)
-
-                            // Don't focus the originating tab.
-                            //e.preventDefault();
-                            // Open in new window.
-                            //const new_window = window.open(message.data.url, "_blank");
-                            //try {
-                            //    new_window.focus();
-                            //} catch (e) {
-                            //    // Opening of new window (aka "popup") might
-                            //    // have been blocked.
-                            //    window.focus();
-                            //}
-                        });
-                    }
-                }
-            }
+            const response = await fetch(this.options.url);
+            data = await response.json();
         } catch (e) {
             logger.error(
                 `Could not fetch from ${this.options.url} on push-id ${this.options.pushId}.`
             );
+        }
+
+        if (!data?.length) {
+            return;
+        }
+
+        for (const message of data) {
+            const notification = new Notification(message.title, message);
+            if (message.data?.url) {
+                // If ``message.data`` contains an URL, open it.
+                // If not, the browser will simply get the focus when
+                // clicking on the desktop notification.
+                notification.addEventListener("click", () => {
+                    window.open(message.data.url, "_self");
+
+                    // Opening a URL in a new tab from JavaScript is
+                    // equivalent to opening a popup window which is
+                    // blocked by default.
+                    // Therefore, we open the URL in the same tab and
+                    // rely on other mechanisms that the user's work
+                    // isn't lost (auto-save, form unload protection.)
+
+                    // Don't focus the originating tab.
+                    //e.preventDefault();
+                    // Open in new window.
+                    //const new_window = window.open(message.data.url, "_blank");
+                    //try {
+                    //    new_window.focus();
+                    //} catch (e) {
+                    //    // Opening of new window (aka "popup") might
+                    //    // have been blocked.
+                    //    window.focus();
+                    //}
+                });
+            }
         }
     },
 });
