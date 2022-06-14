125a4f757a0b98dd72ec7ccca4f707ccb6e45fa0
Johannes Raggam <thetetet@gmail.com>
2022-06-14 18:45:33 +0200

    breaking: Extend jest config from @patternslib/dev.

---
+++ src/setup-tests.js
@@ -0,0 +1,16 @@
+// Extra test setup.
+
+import jquery from "jquery";
+global.$ = global.jQuery = jquery;
+
+// Do not output error messages
+import logging from "./core/logging";
+logging.setLevel(50);
+// level: FATAL
+
+// patch dom.is_visible to not rely on jest-unavailable offsetWidth/Height but
+// simply on el.hidden.
+import dom from "./core/dom";
+dom.is_visible = (el) => {
+    return !el.hidden;
+};
