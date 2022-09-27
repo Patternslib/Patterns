6fa0b4668532532a26b84593aad361c55f59e0bf
Johannes Raggam <thetetet@gmail.com>
2022-09-27 20:51:18 +0200

    feat: Make this a monorepo and provide @patternslib/core.

---
+++ src/core.js
@@ -0,0 +1,18 @@
+// @patternslib/core exports
+
+export * as base from "./core/base";
+export * as dom from "./core/dom";
+export * as events from "./core/events";
+export * as i18n from "./core/i18n";
+export * as jquery_ext from "./core/jquery-ext";
+export * as logging from "./core/logging";
+export * as mockup_parser from "./core/mockup-parser";
+export * as parser from "./core/parser";
+export * as polyfills from "./core/polyfills";
+export * as push_kit from "./core/push_kit";
+export * as registry from "./core/registry";
+export * as remove from "./core/remove";
+export * as store from "./core/store";
+export * as url from "./core/url";
+export * as utils from "./core/utils";
+export { PATTERN_REGISTRY } from "./core/registry";
