b8ecca487c6c4cb4a9e4fcdfc1e89cc2c99cefd5
Johannes Raggam <thetetet@gmail.com>
2022-10-04 00:14:02 +0200

    pat-syntax-highlight: Switch to Prism syntax highlighting.

--- src/pat/syntax-highlight/syntax-highlight.js
+++ src/pat/syntax-highlight/syntax-highlight.js
@@ -1,13 +1,62 @@
 import Base from "../../core/base";
-import utils from "../../core/utils";
+import Parser from "../../core/parser";
+
+export const parser = new Parser("syntax-highlight");
+parser.addArgument("language", "html");
+parser.addArgument("theme", "dark", ["dark", "light"]);
+parser.addArgument("features", null, ["line-highlight", "line-numbers"], true);
 
 export default Base.extend({
     name: "syntax-highlight",
     trigger: ".pat-syntax-highlight",
 
     async init() {
-        const Prettify = (await import("google-code-prettify/src/prettify")).default; // prettier-ignore
-        this.$el.addClass("prettyprint");
-        utils.debounce(Prettify.prettyPrint, 50)();
+        this.options = parser.parse(this.el, this.options);
+
+        let theme;
+        if (this.options.theme === "light") {
+            theme = "";
+        } else if (this.options.theme === "dark") {
+            theme = "okaidia";
+        } else {
+            theme = this.options.theme;
+        }
+
+        import(`prismjs/themes/prism${theme ? "-" + theme : ""}.css`);
+        const Prism = (await import("prismjs")).default;
+
+        if (this.options.features.includes("line-highlight")) {
+            import("prismjs/plugins/line-highlight/prism-line-highlight.css");
+            await import("prismjs/plugins/line-highlight/prism-line-highlight.js");
+        }
+
+        if (this.options.features.includes("line-numbers")) {
+            import("prismjs/plugins/line-numbers/prism-line-numbers.css");
+            await import("prismjs/plugins/line-numbers/prism-line-numbers.js");
+            this.el.classList.add("line-numbers");
+        }
+
+        Prism.manual = true;
+
+        let _el = this.el;
+        const code_el = [...this.el.children].filter((it) => it.tagName === "CODE")?.[0];
+        if (code_el) {
+            _el = code_el;
+        }
+
+        // Get the language
+        let language = [..._el.classList, ...this.el.classList]
+            .filter((it) => {
+                return it.startsWith("language-") || it.startsWith("lang-");
+            })?.[0]
+            ?.replace("language-", "")
+            ?.replace("lang-", "");
+        // CSS class language always win.
+        language = language || this.options.language || "html";
+        // Set the language on the code element (ignored if already set)
+        this.el.classList.add(`language-${language}`);
+        _el.classList.add(`language-${language}`);
+
+        Prism.highlightElement(_el);
     },
 });
