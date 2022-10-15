37520b72bd2c8c407659db5613b9ce79013bb0ff
Johannes Raggam <thetetet@gmail.com>
2022-10-15 14:29:12 +0200

    maint(Tests): Use real path for @patternslib/patternslib module mapping. When extending this config elsewhere the path would not be right.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const config = require("@patternslib/dev/jest.config.js");
 
 config.setupFilesAfterEnv.push(path.resolve(__dirname, "./src/setup-tests.js"));
-config.moduleNameMapper["@patternslib/patternslib/(.*)"] = "<rootDir>/$1";
+config.moduleNameMapper["@patternslib/patternslib/(.*)"] =
+    path.resolve(__dirname) + "/$1";
 
 module.exports = config;
