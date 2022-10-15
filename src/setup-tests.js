7e8ccf45733780b07d3e0985c5067454248b8312
Johannes Raggam <thetetet@gmail.com>
2022-10-15 14:52:38 +0200

    feat(Tests): Add $(":visible") pseudo selector for jQuery.

--- src/setup-tests.js
+++ src/setup-tests.js
@@ -3,6 +3,12 @@
 import jquery from "jquery";
 global.$ = global.jQuery = jquery;
 
+jquery.expr.pseudos.visible = function () {
+    // Fix jQuery ":visible" selector always returns false in JSDOM.
+    // https://github.com/jsdom/jsdom/issues/1048#issuecomment-401599392
+    return true;
+};
+
 // Do not output error messages
 import logging from "./core/logging";
 logging.setLevel(50);
