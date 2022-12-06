f6d8124a84d744f217a8eec2d5b7b184d210439d
Johannes Raggam <thetetet@gmail.com>
2022-12-06 01:16:55 +0100

    maint: Add deprecation note for public_path.

--- src/public_path.js
+++ src/public_path.js
@@ -1,3 +1,5 @@
+// DEPRECATED. Webpack handles the public path by iteself.
+// TODO: REMOVE with next major release
 // NOTE: Import this file before any other files
 // Overwrite path to load resources or use default one.
 __webpack_public_path__ = window.__patternslib_public_path__; // eslint-disable-line no-undef
