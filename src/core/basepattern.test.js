ce962d2d09b41ab39b746d0f9f432df81cc96e61
Johannes Raggam <thetetet@gmail.com>
2022-12-10 16:18:59 +0100

    maint(core basepattern): Use identity instead equality comaprison instead in tests for stricter testing.

--- src/core/basepattern.test.js
+++ src/core/basepattern.test.js
@@ -25,16 +25,16 @@ describe("Basepattern class tests", function () {
         // trigger and name are static and available on the class itself
         expect(Pat.name).toBe("example");
         expect(Pat.trigger).toBe(".example");
-        expect(typeof Pat.parser.parse).toEqual("function");
+        expect(typeof Pat.parser.parse).toBe("function");
 
         const el = document.createElement("div");
 
         const pat = new Pat(el);
         await utils.timeout(1);
 
-        expect(pat.name).toEqual("example");
-        expect(pat.trigger).toEqual(".example");
-        expect(typeof pat.parser.parse).toEqual("function");
+        expect(pat.name).toBe("example");
+        expect(pat.trigger).toBe(".example");
+        expect(typeof pat.parser.parse).toBe("function");
     });
 
     it("2 - Base pattern is class based and does inheritance, polymorphism, encapsulation, ... pt1", async function () {
@@ -61,13 +61,13 @@ describe("Basepattern class tests", function () {
 
         expect(pat1 instanceof Pat1).toBe(true);
         expect(pat1 instanceof Pat2).toBe(false);
-        expect(pat1.el).toEqual(el1);
-        expect(pat1.some).toEqual("thing");
+        expect(pat1.el).toBe(el1);
+        expect(pat1.some).toBe("thing");
 
         expect(pat2 instanceof Pat1).toBe(true);
         expect(pat2 instanceof Pat2).toBe(true);
-        expect(pat2.el).toEqual(el2);
-        expect(pat2.some).toEqual("other");
+        expect(pat2.el).toBe(el2);
+        expect(pat2.some).toBe("other");
     });
 
     it("3 - can be extended multiple times", async function () {
@@ -95,23 +95,23 @@ describe("Basepattern class tests", function () {
         const pat3 = new Pat3(el);
         await utils.timeout(1);
 
-        expect(pat1.name).toEqual("example1");
-        expect(pat1.something).toEqual("else");
-        expect(pat1.some).toEqual(undefined);
+        expect(pat1.name).toBe("example1");
+        expect(pat1.something).toBe("else");
+        expect(pat1.some).toBe(undefined);
         expect(pat1 instanceof BasePattern).toBeTruthy();
         expect(pat1 instanceof Pat2).toBeFalsy();
         expect(pat1 instanceof Pat3).toBeFalsy();
 
-        expect(pat2.name).toEqual("example2");
-        expect(pat2.something).toEqual("else");
-        expect(pat2.some).toEqual("thing2");
+        expect(pat2.name).toBe("example2");
+        expect(pat2.something).toBe("else");
+        expect(pat2.some).toBe("thing2");
         expect(pat2 instanceof BasePattern).toBeTruthy();
         expect(pat2 instanceof Pat1).toBeTruthy();
         expect(pat2 instanceof Pat3).toBeFalsy();
 
-        expect(pat3.name).toEqual("example3");
-        expect(pat3.something).toEqual("else");
-        expect(pat3.some).toEqual("thing3");
+        expect(pat3.name).toBe("example3");
+        expect(pat3.something).toBe("else");
+        expect(pat3.some).toBe("thing3");
         expect(pat3 instanceof BasePattern).toBeTruthy();
         expect(pat3 instanceof Pat1).toBeTruthy();
         expect(pat3 instanceof Pat2).toBeTruthy();
@@ -126,7 +126,7 @@ describe("Basepattern class tests", function () {
         const pat = new Pat(el);
         await utils.timeout(1);
 
-        expect(el["pattern-example"]).toEqual(pat);
+        expect(el["pattern-example"]).toBe(pat);
     });
 
     it("5 - Registers with the registry.", async function () {
