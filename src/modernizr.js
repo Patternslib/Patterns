56e5d86aa8ec49195cd35f1358e09e17fc39159e
Johannes Raggam <thetetet@gmail.com>
2022-12-19 11:58:14 +0100

    breaking(Build): Separate modernizr into a own bundle.
    Since Patternslib 9.0.0-alpha.0 where we introduced webpack module
    federation for our bundles, Modernizr is loaded asynchronously and
    applying it's CSS classes a tick too late. For example, the change from
    the `no-js` to the `js` class was done while the tiles have already been
    drawn and visible on the screen, resulting in screen flickering. There
    are a number of projects which depend on Modernizr being applied early.
    
    This change now fixes the problem by separating the Modernizr build from
    the Patternslib bundle.
    
    If you depend on Modernzir, please include the new `modernizr.min.js` bundle in
    a script tag, preferably before the Patternslib bundle `bundle.min.js`.

---
+++ src/modernizr.js
@@ -0,0 +1,5 @@
+// Include modernizr for a modernizr bundle.
+// To use it in your project, include the `modernizr.min.js` bundle in a script
+// tag. This is for projects where styles depend on specific modernizr features
+// like the change from a `no-js` class to a `js` class.
+import("modernizr");
