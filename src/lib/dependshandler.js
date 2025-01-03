0030a56ca65144a1965aed2b6e9b57327b24602d
Johannes Raggam <thetetet@gmail.com>
2025-01-03 11:06:00 +0100

    fix(lib dependshandler): Do not return the value from an disabled input. Return null instead.

--- src/lib/dependshandler.js
+++ src/lib/dependshandler.js
@@ -32,6 +32,9 @@ class DependsHandler {
             if (input.type === "checkbox" && input.checked === false) {
                 return false;
             }
+            if (input.disabled) {
+                    return false;
+            }
             return true;
         })
 
