Patternslib/Patterns

commit b0f94fb058a7067e19bc9c3101a11dfa49ff8dd2

maint(pat inject): Modernize some parts of pat inject.

src/pat/inject/inject.js
@@ -347,7 +347,6 @@ const inject = {
                 return false;
             }
             cfg.$target = this.createTarget(cfg.target);
-            cfg.$injected = cfg.$target;
         }
         return true;
     },
@@ -455,23 +454,36 @@ const inject = {
         /* Called after the XHR has succeeded and we have a new $source
          * element to inject.
          */
-        if (cfg.sourceMod === "content") {
-            $source = $source.contents();
-        }
-        const $src = $source.safeClone();
-        for (const img of dom.querySelectorAllAndMe($src[0], "img")) {
-            $(img).on("load", (e) => {
-                $(e.currentTarget).trigger("pat-inject-content-loaded");
-            });
+        const wrapper = document.createElement("div");
+        if ($source.length > 0) {
+            if (cfg.sourceMod === "content") {
+                wrapper.innerHTML = $source[0].innerHTML;
+            } else {
+                wrapper.innerHTML = $source[0].outerHTML;
+            }
+
+            for (const img of wrapper.querySelectorAll("img")) {
+                events.add_event_listener(
+                    img,
+                    "load",
+                    "inject_img_load",
+                    (e) => {
+                        $(e.currentTarget).trigger("pat-inject-content-loaded");
+                    },
+                    { once: true }
+                );
+            }
         }
 
-        const $injected = cfg.$injected || $src;
+        // Copy, because after insertion wrapper.children is empty.
+        const source_nodes = [...wrapper.childNodes];
+
         // Now the injection actually happens.
-        if (this._inject(trigger, $src, $(target), cfg)) {
+        if (this._inject(trigger, source_nodes, target, cfg)) {
             // Update history
             this._update_history(cfg, trigger, title);
             // Post-injection
-            this._afterInjection($el, $injected, cfg);
+            this._afterInjection($el, $(source_nodes), cfg);
         }
     },
 
@@ -488,9 +500,12 @@ const inject = {
         history.pushState({ url: url }, "", url);
         // Also inject title element if we have one
         if (title) {
-            this._inject(trigger, title, $("title"), {
-                action: "element",
-            });
+            const title_el = document.querySelector("title");
+            if (title_el) {
+                this._inject(trigger, title, title_el, {
+                    action: "element",
+                });
+            }
         }
     },
 
@@ -761,45 +776,45 @@ const inject = {
         }
     },
 
-    _inject(trigger, $source, $target, cfg) {
-        // action to jquery method mapping, except for "content"
-        // and "element"
-        const method = {
-            contentbefore: "prepend",
-            contentafter: "append",
-            elementbefore: "before",
-            elementafter: "after",
-        }[cfg.action];
-
+    _inject(trigger, source, target, cfg) {
         if (cfg.source === "none") {
-            $target.replaceWith("");
+            // Special case. Clear the target after ajax call.
+            target.replaceWith("");
             return true;
         }
-        if ($source.length === 0) {
-            log.warn("Aborting injection, source not found:", $source);
+        if (source.length === 0) {
+            log.warn("Aborting injection, source not found:", source);
             $(trigger).trigger("pat-inject-missingSource", {
                 url: cfg.url,
                 selector: cfg.source,
             });
             return false;
         }
-        if (cfg.target === "none")
+        if (cfg.target === "none") {
             // Special case. Don't do anything, we don't want any result
             return true;
-        if ($target.length === 0) {
-            log.warn("Aborting injection, target not found:", $target);
+        }
+        if (!target) {
+            log.warn("Aborting injection, target not found:", target);
             $(trigger).trigger("pat-inject-missingTarget", {
                 selector: cfg.target,
             });
             return false;
         }
-        if (cfg.action === "content") {
-            $target.empty().append($source);
-        } else if (cfg.action === "element") {
-            $target.replaceWith($source);
-        } else {
-            $target[method]($source);
-        }
+
+        // cfg.action to DOM method mapping
+        const method = {
+            content: "replaceChildren",
+            contentafter: "append",
+            contentbefore: "prepend",
+            element: "replaceWith",
+            elementafter: "after",
+            elementbefore: "before",
+        }[cfg.action];
+
+        // Inject the content HERE!
+        target[method](...source);
+
         return true;
     },
 

commit 2e70a130ee5d5c1a31064fb9100eba74dc8374f6

fix(pat-inject): Fix problem with inserting table rows.
There was a problem with injecting table rows introduced in: b0f94fb058a7067e19bc9c3101a11dfa49ff8dd2
The problem occured when setting a table row as content of a temporary
<div> wrapper. But a <tr> is not a valid child node of a <div>. This
caused visually destroyed tables.

Using a <template> tag as wrapper instead of a div solved the problem.

src/pat/inject/inject.js
@@ -459,7 +459,7 @@ const inject = {
         /* Called after the XHR has succeeded and we have a new $sources
          * element to inject.
          */
-        const wrapper = document.createElement("div");
+        const wrapper = document.createElement("template");
         if ($sources.length > 0) {
             const method = cfg.sourceMod === "content" ? "innerHTML" : "outerHTML";
             // There might be multiple sources, so we need to loop over them.
@@ -467,7 +467,7 @@ const inject = {
             const sources_string = [...$sources].map(source => source[method]).join("\n");
             wrapper.innerHTML = sources_string;
 
-            for (const img of wrapper.querySelectorAll("img")) {
+            for (const img of wrapper.content.querySelectorAll("img")) {
                 events.add_event_listener(
                     img,
                     "load",
@@ -481,7 +481,7 @@ const inject = {
         }
 
         // Copy, because after insertion wrapper.children is empty.
-        const source_nodes = [...wrapper.childNodes];
+        const source_nodes = [...wrapper.content.childNodes];
 
         // Now the injection actually happens.
         if (this._inject(trigger, source_nodes, target, cfg)) {

src/pat/inject/inject.test.js
@@ -1868,6 +1868,61 @@ describe("pat-inject", function () {
             });
         });
 
+        describe("9.6 - injecting tables.", function () {
+            let spy_ajax;
+
+            beforeEach(function () {
+                spy_ajax = jest.spyOn($, "ajax").mockImplementation(() => deferred);
+            });
+
+            afterEach(function () {
+                spy_ajax.mockRestore();
+            });
+
+            it("9.6.1 - Correctly injects table rows", async function () {
+                // Table rows <tr> can only be childs of <tbodys> or <thead> elements.
+                // If they are childs of <table> elements, the browser implicitly adds a <tbody> element.
+                // Table rows cannot be childs of <div> elements or others.
+                // There is one exception though - they can be directly added to <template> elements.
+                // This test checks this problem which popped up during the 9.10 release cycle and was fixed in:
+                // https://github.com/Patternslib/Patterns/pull/1238
+
+                document.body.innerHTML = `
+                    <a class="pat-inject"
+                       href="test.html"
+                       data-pat-inject="
+                        source: tr::element;
+                        target: table;
+                    ">link</a>
+                    <table>
+                    </table>
+                `;
+
+                answer(`
+                    <html>
+                        <body>
+                            <table>
+                                <tr><td>wohoo</td></tr>
+                            </table>
+                        </body>
+                    </html>
+                `);
+
+                const inject = document.querySelector(".pat-inject");
+
+                pattern.init($(inject));
+                await utils.timeout(1); // wait a tick for async to settle.
+
+                inject.click();
+                await utils.timeout(1); // wait a tick for async to settle.
+
+
+                console.log(document.body.outerHTML);
+
+                const injected = document.querySelectorAll("table tr");
+                expect(injected.length).toBe(1);
+            });
+        });
     });
 
     describe("10 - Error handling", () => {