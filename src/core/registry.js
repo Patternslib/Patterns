c3d9c6fea159ddc082babe2ca5f8a06631611d66
Johannes Raggam <thetetet@gmail.com>
2025-09-03 17:39:48 +0200

    DRAFT: fix(core registry): Re-scan the document after module federation bundle initialization for any new Patterns.
    HEADS UP: remove, as this is not needed.
    Registering a bundle already scans the DOM for that one bundle.

--- src/core/registry.js
+++ src/core/registry.js
@@ -66,6 +66,14 @@ const registry = {
                 // Do not reinitialize a already initialized registry.
                 return;
             }
+            events.add_event_listener(
+                document,
+                "patternslib__mf--loaded",
+                "registry_rescan_mf",
+                () => {
+                    registry.scan(document.body);
+                }
+            );
             window.__patternslib_registry_initialized = true;
             log.debug("Loaded: " + Object.keys(registry.patterns).sort().join(", "));
             registry.scan(document.body);
