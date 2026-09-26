6135014c7e229a6bae5f59a1656143ac6f384e4f
Johannes Raggam <thetetet@gmail.com>
2026-09-26 15:09:48 +0200

    maint: Remove support for Modernizr.
    Remove Modernizr, as announced in 9.10.1 that it will be done for an
    upcoming minor version.

--- src/core/feature-detection.js
+++ src/core/feature-detection.js
@@ -1,33 +1,13 @@
 (function () {
-    // Add JavaScript feature as class to the <html> element, just like
-    // Modernizr does. This is needed for accessibility reasons, to support
-    // browsers and situations where JavaScript is not available or disabled.
-    // The HTML root tag needs to have the `no-js` class set which is then
-    // replaced by `js`.
+    // JavaScript feature detection.
+    // If JavaScript is available, `js` will be added to the HTML class list.
+    // This is needed for accessibility reasons, to support browsers and
+    // situations where JavaScript is not available or disabled.
+    // NOTE: The HTML root tag needs to have the `no-js` class set which is
+    // then replaced by `js`.
     const html = document.getElementsByTagName("html")[0];
     if (html.classList.contains("no-js")) {
         html.classList.remove("no-js");
         html.classList.add("js");
     }
-
-    // NOTE: Modernizr will be removed in an upcoming minor release.
-
-    // Do not load modernizr if disabled. It's enabled by default.
-    // You might want to disable it for your project by setting:
-    // window.__patternslib_disable_modernizr = true;
-    if (window.__patternslib_disable_modernizr) {
-        return;
-    }
-
-    // Get the current script tag's URL.
-    const script_url = document.currentScript.src;
-    // Get the base URL of the current script tag's URL.
-    let base_url = script_url.substring(0, script_url.lastIndexOf("/")) + "/";
-    // The modernizr script is located outside the chunks directory.
-    base_url = base_url.replace("chunks/", "");
-
-    // Inject a new one with the modernizr bundle.
-    const script_tag = document.createElement("script");
-    script_tag.src = base_url + "modernizr.min.js";
-    document.getElementsByTagName("head")[0].appendChild(script_tag);
 })();
